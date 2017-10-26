ee8808ae9221176f57ab5f0cfc9c6f895c0f6641
Manuel Martin <manuel@elasticbox.com>
2017-10-26 12:10:48 +0200

    Changes Chi API

--- scripts/cli.js
+++ scripts/cli.js
@@ -3,54 +3,47 @@
 import * as chi from './chi';
 import yargs from 'yargs';
 
-function getCommand(input) {
-  if (input.length !== 1) {
-    throw Error('Select one command');
-  }
-
-  const command = input[0];
-
-  if (['all', 'css', 'assets'].indexOf(command) === -1) {
-    throw Error('Invalid command');
-  }
-
-  return command;
-}
-
 const argv = yargs
-  .usage('Usage: $0 command -c <component-list> -o <output-dir>')
-  .example('$0 css -c grid buttons avatars -o dist/assets')
+  .usage('Usage: $0 <component-list> -c command -o <output-dir> -p <assets_path>')
+  .example('$0 grid buttons avatars -o dist/assets -p /assets/')
   .command('all', 'generates the stylesheet and copies the assets of the selected components')
   .command('css', 'generates the stylesheet with the selected components')
   .command('assets', 'copy the selected components assets')
   .options({
     'c': {
-      alias: 'components',
+      alias: 'command',
       demandOption: false,
       default: 'all',
-      describe: 'list of components',
-      type: 'array'
+      describe: 'command',
+      choices: ['all', 'css', 'assets']
     },
     'o': {
       alias: 'output',
       demandOption: true,
       describe: 'output folder',
       type: 'string'
+    },
+    'p': {
+      alias: 'path',
+      demandOption: false,
+      default: '/',
+      describe: 'assets path',
+      type: 'string'
     }
   })
   .argv;
 
 try {
-  switch (getCommand(argv._)) {
+  switch (argv.c) {
     case 'all':
-      chi.buildCss({names: argv.c, dest: argv.o});
-      chi.copyAssets({names: argv.c, dest: argv.o});
+      chi.buildCss({names: argv._, dest: argv.o, assetsPath: argv.p});
+      chi.copyAssets({names: argv._, dest: argv.o});
       break;
     case 'css':
-      chi.buildCss({names: argv.c, dest: argv.o});
+      chi.buildCss({names: argv._, dest: argv.o, assetsPath: argv.p});
       break;
     case 'assets':
-      chi.copyAssets({names: argv.c, dest: argv.o});
+      chi.copyAssets({names: argv._, dest: argv.o});
       break;
   }
 } catch(e) {
