21ba6c8a733ef409dadd24f38334bec4e071c67e
Manuel Martin <manuel@elasticbox.com>
2017-12-04 17:11:18 +0100

    Doc for Chi v0.4.5
    Added:
     - input-texts
     - checkboxes
     - toggle switches
     - radio buttons
     - range sliders
    
    Updated:
     - buttons
     - button groups

--- tasks/build-extrenal-assets.js
+++ tasks/build-extrenal-assets.js
@@ -8,6 +8,8 @@ const chiAssetsFolder = path.join(Folders.dist.STYLES, 'chi');
 const chiAssetsPath = chiAssetsFolder.substring(Folders.DIST.length) + '/';
 
 gulp.task('build-external-assets', () => {
-  chi.buildCss({ dest: Folders.dist.STYLES, assetsPath: chiAssetsPath });
-  chi.copyAssets({ dest: chiAssetsFolder });
+  const names = ['all'];
+
+  chi.buildCss({ names, dest: Folders.dist.STYLES, assetsPath: chiAssetsPath });
+  chi.copyAssets({ names, dest: chiAssetsFolder });
 });
