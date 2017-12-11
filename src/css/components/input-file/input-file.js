9f9653459db0b36c91db0234336c7e434b742abe
Manuel Martin <manuel@elasticbox.com>
2017-12-11 19:12:44 +0100

    Adds input file component

---
+++ src/css/components/input-file/input-file.js
@@ -0,0 +1,19 @@
+var inputFiles = document.querySelectorAll('input[type="file"].c-input');
+
+Array.prototype.forEach.call(inputFiles, function(input) {
+  var label = input.nextElementSibling;
+
+  input.addEventListener('change', function(e) {
+    var fileName = '';
+
+    if (this.files && this.files.length > 1) {
+      fileName = this.files.length + ' files selected';
+    } else {
+      fileName = e.target.value.split('\\').pop();
+    }
+
+    if (fileName) {
+      label.innerHTML = fileName;
+    }
+  });
+});
