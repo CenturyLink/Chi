5e19e234be6974b862b41cf352bbc5553ac51a98
Justin Mecham <justin@mecham.me>
2018-05-15 16:43:51 -0400

    Updated naming to remove CenturyLink Human Interface in favor of Chi.

--- tasks/serve.js
+++ tasks/serve.js
@@ -6,7 +6,7 @@ const plugins = require('gulp-load-plugins')();
 
 gulp.task('serve', ['test-build'], () => {
   server = plugins.connect.server({
-      name: 'CenturyLink Human Interface',
+      name: 'Chi',
       root: ['public'],
       port: 8000,
       livereload: true
