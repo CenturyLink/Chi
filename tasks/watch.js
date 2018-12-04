a6c505b0986fed045673e2c17d028602663f0995
Daniel Peña Gil <daniel.pena@centurylink.com>
2018-12-04 14:54:12 +0100

    Changed gulp task. Now on scss changes only compiles sassc files.

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp';
 
 gulp.task('watch', [ 'serve' ], () => {
-  gulp.watch('src/chi/**/*.scss', [ 'lint:css', 'build:chi' ]);
+  gulp.watch('src/chi/**/*.scss', [ 'lint:css', 'build:chi:styles' ]);
   gulp.watch('src/chi/**/*.js', ['build:chi:scripts', 'build:chi:scriptsES6', 'build:chi:scriptsAMD']);
   gulp.watch('src/website/assets/images/**/*', [ 'build:website:images' ]);
   gulp.watch('src/website/assets/styles/**/*', [ 'build:website:styles' ]);
