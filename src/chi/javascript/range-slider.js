63b1c5dc78d355f4d38b5207ad04cb93b6153a70
Daniel Peña Gil <daniel.pena@centurylink.com>
2018-12-18 12:26:55 +0100

    [CHI-157] Add javascript range slider component Added JavaScript to examples. Removed unnecessary m-formGroup classes. Changed track labels font size and line height. Set opacity 0 to thumb when an alternative cross-browser element is used.

--- src/chi/javascript/range-slider.js
+++ src/chi/javascript/range-slider.js
@@ -2,6 +2,7 @@ import {Util} from "./util.js";
 import {chi} from "./chi.js";
 
 const COMPONENT_TYPE = "rangeSlider";
+const COMPONENT_OWN_THUMB_CLASS = 'a-rangeSlider__thumbOverwritten';
 
 class RangeSlider {
 
@@ -17,17 +18,29 @@ class RangeSlider {
 
     Util.registerComponent(COMPONENT_TYPE, this._elem, this);
 
-    const thumbs = this._elem.parentElement.getElementsByClassName(
-      'a-input__thumb'
-    );
-    const progress = this._elem.parentElement.getElementsByClassName(
-      'a-input__progress'
-    );
-    if (thumbs.length) {
-      this._thumbElem = thumbs[0];
+
+    if (this._config.thumb) {
+      this._thumbElem = thumb;
+      Util.addClass(this._elem, COMPONENT_OWN_THUMB_CLASS);
+    } else {
+      const thumbs = this._elem.parentElement.getElementsByClassName(
+        'a-input__thumb'
+      );
+      if (thumbs.length) {
+        this._thumbElem = thumbs[0];
+        Util.addClass(this._elem, COMPONENT_OWN_THUMB_CLASS);
+      }
     }
-    if (progress.length) {
-      this._progressElem = progress[0];
+
+    if (this._config.progress) {
+      this._progressElem = this._config.progress;
+    } else {
+      const progress = this._elem.parentElement.getElementsByClassName(
+        'a-input__progress'
+      );
+      if (progress.length) {
+        this._progressElem = progress[0];
+      }
     }
 
     this._minValue = this._elem.getAttribute('min') || this._minValue;
@@ -48,10 +61,14 @@ class RangeSlider {
   updateView () {
     const x =
       (this._currentValue - this._minValue) / (this._maxValue - this._minValue);
-    this._progressElem.style.width =
-      'calc(' + x*100 + '% - ' + (x-0.5)*1.25 + 'rem)';
-    this._thumbElem.style.left =
-      'calc(' + x*100 + '% - ' + (x-0.5)*1.25 + 'rem)';
+    if (this._progressElem) {
+      this._progressElem.style.width =
+        'calc(' + x*100 + '% - ' + (x-0.5)*1.25 + 'rem)';
+    }
+    if (this._thumbElem) {
+      this._thumbElem.style.left =
+        'calc(' + x*100 + '% - ' + (x-0.5)*1.25 + 'rem)';
+    }
   }
 
   dispose() {
@@ -62,6 +79,7 @@ class RangeSlider {
     this._thumbElem = null;
     this._progressElem = null;
     Util.unregisterComponent(COMPONENT_TYPE, this._elem);
+    Util.removeClass(this._elem, COMPONENT_OWN_THUMB_CLASS);
     this._elem = null;
   }
 
