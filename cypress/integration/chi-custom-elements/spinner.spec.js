9e9deebc296a7411a681ce03636b90fe1402e102
Srinath Koneru <srinath.koneru@centurylink.com>
2019-06-26 10:03:40 -0500

    refactoring e2e tests

--- cypress/integration/chi-custom-elements/spinner.spec.js
+++ cypress/integration/chi-custom-elements/spinner.spec.js
@@ -1,47 +1,46 @@
 describe('Spinner', () => {
-  it('Base spinner should be included inside the custom element', () => {
-    cy.visit('tests/custom-elements/spinner.html');
+  const selectors = ['spinner-md', 'base-backdrop-spinner', 'inverse-backdrop-spinner', 'spinner-secondary', 'spinner-xxl'];
 
-    cy.get('[data-cy="spinner-md"]', { timeout: 5000 })
-      .should('have.class', 'hydrated')
-      .children()
-      .first()
-      .should('match', 'svg.a-spinner');
+  const assertion = (el, value) => {
+    cy.get(el)
+      .should('match', value);
+  };
+
+  before(() => {
+    cy.visit('tests/custom-elements/spinner.html');
+  });
+
+  beforeEach(() => {
+    selectors.forEach((s) => {
+      cy.get(`[data-cy=${s}]`, { timeout: 5000 })
+        .should('have.class', 'hydrated')
+        .children()
+        .first()
+        .as(s);
+    });
+  });
+
+  it('Base spinner should be included inside the custom element', () => {
+    assertion('@spinner-md', 'svg.a-spinner');
   });
 
   it('Base backdrop spinner should have appropriate class', () => {
-    cy.get('[data-cy="base-backdrop-spinner"]', { timeout: 5000 })
-      .should('have.class', 'hydrated')
-      .children()
-      .first()
+    cy.get('@base-backdrop-spinner')
       .should('match', 'div.a-backdrop')
       .and('not.have.class', '-inverse');
   });
 
   it('Inverse backdrop spinner should have appropriate class', () => {
-    cy.get('[data-cy="inverse-backdrop-spinner"]', { timeout: 5000 })
-      .should('have.class', 'hydrated')
-      .children()
-      .first()
+    cy.get('@inverse-backdrop-spinner')
       .should('match', 'div.a-backdrop')
       .and('have.class', '-inverse');
   });
 
   it('Colored spinner should have appropriate class', () => {
-    cy.get('[data-cy="spinner-secondary"]', { timeout: 5000 })
-      .should('have.class', 'hydrated')
-      .children()
-      .first()
-      .should('match', 'svg.a-spinner.-text--secondary')
-      .and('not.have.class', 'secondary');
+    assertion('@spinner-secondary', 'svg.a-spinner.-text--secondary');
   });
 
   it('Sized spinner should have appropriate class', () => {
-    cy.get('[data-cy="spinner-xxl"]', { timeout: 5000 })
-      .should('have.class', 'hydrated')
-      .children()
-      .first()
-      .should('match', 'svg.a-spinner.-xxl')
-      .and('not.have.class', 'xxl');
+    assertion('@spinner-xxl', 'svg.a-spinner.-xxl');
   });
 });
