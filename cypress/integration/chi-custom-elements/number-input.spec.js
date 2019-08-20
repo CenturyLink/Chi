b063022e9ced5aa660521344ceffd79e5b4ca458
David Ojeda <david.ojeda@centurylink.com>
2019-08-20 11:20:14 +0200

    [CHI-XXX] Fix bug number input text

--- cypress/integration/chi-custom-elements/number-input.spec.js
+++ cypress/integration/chi-custom-elements/number-input.spec.js
@@ -2,62 +2,53 @@
 
 describe('Number input', function() {
 
-  const spyCheck = function(element, button, value, type=false, typeElement=null, typevalue=undefined) {
-    
-    const spy = cy.spy();
-      
-      cy.get(element)
-        .then((el) => {
-          el.on('chiChange', spy);
-        });
+  const valueCheck = function(button, value, input, type=false, typevalue=undefined) {
 
-      if (type) {
-        cy.get(typeElement)
+    if (type) {
+      cy.get(input)
         .clear()
         .type(typevalue);
-      }
+    }
 
-      cy.get(button)
-        .click()
-        .then(() => {
-          expect(spy).to.be.called;
-          expect(spy.getCall(0).args[0].detail).to.equal(value);
-        });
+    cy.get(button)
+      .click();
+
+    cy.get(input).should('have.value', value);
   };
 
   const checkComponent = function(element, classToCheck) {
 
     cy.get(element)
-        .should('have.class', 'hydrated')
-        .children()
-        .first()
-        .should('have.class', classToCheck);
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('have.class', classToCheck);
   };
 
   const getComponentAliases = function(component, baseAlias, wrapperAlias, inputAlias, minusButtonAlias, plusButtonAlias) {
 
     cy.get(component)
-        .first()
-        .as(baseAlias);
+      .first()
+      .as(baseAlias);
 
-      cy.get(`@${baseAlias}`)
-        .children()
-        .first()
-        .as(wrapperAlias);
+    cy.get(`@${baseAlias}`)
+      .children()
+      .first()
+      .as(wrapperAlias);
 
-      cy.get(`@${wrapperAlias}`)
-        .children()
-        .first()
-        .as(inputAlias);
-
-      cy.get(`@${inputAlias}`)
-        .next()
-        .as(minusButtonAlias);
-    
     cy.get(`@${wrapperAlias}`)
-        .children()
-        .last()
-        .as(plusButtonAlias);
+      .children()
+      .first()
+      .as(inputAlias);
+
+    cy.get(`@${inputAlias}`)
+      .next()
+      .as(minusButtonAlias);
+
+    cy.get(`@${wrapperAlias}`)
+      .children()
+      .last()
+      .as(plusButtonAlias);
   };
 
   describe('Number input base', function() {
@@ -70,23 +61,23 @@ describe('Number input', function() {
     });
 
     it('Number input custom element should initiate a Chi number-input component. ', function() {
-      
+
       checkComponent('@base_number_input', 'a-inputWrapper');
     });
 
     it('Clicking arrow-up button sends a value increment event. ', function() {
 
-      spyCheck('@base_number_input', '@plus_button', '1');
+      valueCheck('@plus_button', '1', '@input');
     });
 
     it('Clicking arrow-down button sends a value decrement event. ', function() {
 
-      spyCheck('@base_number_input', '@minus_button', '-1');
+      valueCheck('@minus_button', '-1', '@input');
     });
 
     it('Setting a value manually in the input must be allowed as a base number. ', function() {
 
-      spyCheck('@base_number_input', '@plus_button', '56', true, '@input', '55');
+      valueCheck('@plus_button', '56', '@input', true, '55');
     });
   });
 
@@ -100,23 +91,23 @@ describe('Number input', function() {
     });
 
     it('Number input custom element should initiate an expanded Chi number-input component. ', function() {
-      
+
       checkComponent('@expanded_number_input', 'm-inputNumber');
     });
 
     it('Clicking arrow-up button sends a value increment event. ', function() {
 
-      spyCheck('@expanded_number_input', '@expanded_plus_button', '1');
+      valueCheck('@expanded_plus_button', '1', '@expanded_input');
     });
 
     it('Clicking arrow-down button sends a value decrement event. ', function() {
 
-      spyCheck('@expanded_number_input', '@expanded_minus_button', '-1');
+      valueCheck('@expanded_minus_button', '-1', '@expanded_input');
     });
 
     it('Setting a value manually in the input must be allowed as a base number. ', function() {
 
-      spyCheck('@expanded_number_input', '@expanded_minus_button', '3', true, '@expanded_input', '3.3');
+      valueCheck('@expanded_minus_button', '3', '@expanded_input', true, '3.3');
     });
   });
 });
