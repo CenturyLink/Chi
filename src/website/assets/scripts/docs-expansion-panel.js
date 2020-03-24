5158241cb121a67a835c1398b8322dc2eabf00ec
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-03-24 17:29:11 +0100

    [PE-4536] Migrate Expansion Panel interaction examples

--- src/website/assets/scripts/docs-expansion-panel.js
+++ src/website/assets/scripts/docs-expansion-panel.js
@@ -1,35 +1,5 @@
-
 document.addEventListener('DOMContentLoaded',
   function() {
-
-    const epDom = document.getElementById('example1');
-    const ePanel = chi.expansionPanel(epDom);
-
-    const checkBoxes = document.querySelectorAll('input[name="example1"]');
-
-    Array.prototype.forEach.call(
-      checkBoxes,
-      function(input) {
-        input.addEventListener('change', function() {
-          if (this.checked) {
-            ePanel.setState(this.value);
-          }
-        }, false);
-      }
-    );
-
-    epDom.addEventListener('chi.epanel.change', function() {
-      const state = ePanel.getStateName();
-      Array.prototype.forEach.call(
-        checkBoxes,
-        function(input) {
-          if (input.value === state) {
-            input.checked = true;
-          }
-        }
-      );
-    });
-
     chi.expansionPanel(document.getElementById('example2'));
     chi.expansionPanel(document.querySelectorAll('[data-chi-epanel-group="example3"]'));
     chi.expansionPanel(document.querySelectorAll('[data-chi-epanel-group="example4"]'), {mode: 'accordion'});
