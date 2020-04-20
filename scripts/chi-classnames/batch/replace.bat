cd7fed31b6386060eb04b4362b04f24f7c7eaa2d
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-04-20 17:55:22 +0200

    [PE-4555] Updated Button and Avatar classname replacement

--- scripts/chi-classnames/batch/replace.bat
+++ scripts/chi-classnames/batch/replace.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enableDelayedExpansion
 set "PROJECT_SRC=path/to/project/folder"
+REM Please make sure that you provide correct path to your project folder.
+REM Wrong path might affect other folders / project
 
 for /f "tokens=1,2 delims=:" %%i in (classes.txt) do (
    for /f "delims=" %%G in ('findstr /M /nrc:"[^a-zA-Z0-9]%%i[^a-zA-Z0-9]" /M /nrc:"[^a-zA-Z0-9]%%i$" /S %PROJECT_SRC%/*') do (
