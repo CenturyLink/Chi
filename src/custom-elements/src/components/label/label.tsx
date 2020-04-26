241ff5d6178f30f91ca059bb16eacf9d12821461
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-04-26 19:52:39 +0200

    [PE-4819] Added size and required attrs to Label

--- src/custom-elements/src/components/label/label.tsx
+++ src/custom-elements/src/components/label/label.tsx
@@ -1,24 +1,48 @@
-import { Component, Element, Prop, h } from '@stencil/core';
+import { Component, Element, Prop, h, Watch } from '@stencil/core';
+
+const VALID_SIZES = ['xs', 'sm', 'md', 'lg', 'xl'];
 
 @Component({
   tag: 'chi-label',
   styleUrl: 'label.scss',
   scoped: true
 })
+
 export class Label {
   @Element() el: HTMLLabelElement;
   /**
-   *  to set for attribute.
+   *  To indicate ID of target form control web component.
    */
   @Prop({ reflect: true }) for: string;
+  /**
+   * OPTIONAL. Size of the label. {xs,sm, md, lg, xl}.
+   */
+  @Prop({ reflect: true }) size = 'md';
+  /**
+   * To indicate which form field is required.
+   */
+  @Prop({ reflect: true }) required = false;
+
+  @Watch('size')
+  validateSizeAttribute(newValue: string) {
+    if (newValue && VALID_SIZES.indexOf(newValue) === -1) {
+      throw new Error('Not valid size (' + newValue + '). Valid values are xs, sm, md, lg or xl. ');
+    }
+  }
 
   render() {
+    const requiredAsterisk = <chi-icon class="-ml--1" color="danger" icon="asterisk" size="xs"></chi-icon>;
+
     return (
       <label
-        class="chi-label"
+        class={`
+          chi-label
+          ${this.size ? `-${this.size}` : ``}
+          `}
         htmlFor={`${this.for}-control`}
       >
-          <slot></slot>
+        <slot></slot>
+        {this.required ? requiredAsterisk : ''}
       </label>
     );
   }
