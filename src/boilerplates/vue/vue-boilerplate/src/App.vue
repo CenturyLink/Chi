147fcedbbc8a8d0345955487bdbea0c681ac1599
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-10-12 22:31:31 +0200

    Implemented Vue & Stencil boilerplates & updated documentation

---
+++ src/boilerplates/vue/vue-boilerplate/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
