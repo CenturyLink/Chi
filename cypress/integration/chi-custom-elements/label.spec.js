19f77ad282646e3c4dcb8261b4c005d37e48c082
Sabah Ahmed <sabah.ahmed@centurylink.com>
2020-10-12 11:10:24 -0600

    Updated cypress test for Label component

--- cypress/integration/chi-custom-elements/label.spec.js
+++ cypress/integration/chi-custom-elements/label.spec.js
@@ -17,93 +17,30 @@ describe('Label', function() {
       .contains('(optional)');
   });
 
-  it('Class xs is present in base. ', function() {
-    cy.get('[data-cy="test-label-base-xs"]')
-      .find('label.chi-label')
-      .should('have.class', '-xs');
-  });
+  const labelSizes = [
+    'test-label-base-xs',
+    'test-label-base-sm',
+    'test-label-base-md',
+    'test-label-base-lg',
+    'test-label-base-xl',
+    'test-label-required-xs',
+    'test-label-required-sm',
+    'test-label-required-md',
+    'test-label-required-lg',
+    'test-label-required-xl',
+    'test-label-optional-xs',
+    'test-label-optional-sm',
+    'test-label-optional-md',
+    'test-label-optional-lg',
+    'test-label-optional-xl'
+  ];
 
-  it('Class sm is present in base. ', function() {
-    cy.get('[data-cy="test-label-base-sm"]')
-      .find('label.chi-label')
-      .should('have.class', '-sm');
-  });
-
-  it('Class md is present in base. ', function() {
-    cy.get('[data-cy="test-label-base-md"]')
-      .find('label.chi-label')
-      .should('have.class', '-md');
-  });
-
-  it('Class lg is present in base. ', function() {
-    cy.get('[data-cy="test-label-base-lg"]')
-      .find('label.chi-label')
-      .should('have.class', '-lg');
-  });
-
-  it('Class xl is present in base. ', function() {
-    cy.get('[data-cy="test-label-base-xl"]')
-      .find('label.chi-label')
-      .should('have.class', '-xl');
-  });
-
-  it('Class xs is present in required. ', function() {
-    cy.get('[data-cy="test-label-required-xs"]')
-      .find('label.chi-label')
-      .should('have.class', '-xs');
-  });
-
-  it('Class sm is present in required. ', function() {
-    cy.get('[data-cy="test-label-required-sm"]')
-      .find('label.chi-label')
-      .should('have.class', '-sm');
-  });
-
-  it('Class md is present in required. ', function() {
-    cy.get('[data-cy="test-label-required-md"]')
-      .find('label.chi-label')
-      .should('have.class', '-md');
-  });
-
-  it('Class lg is present in required. ', function() {
-    cy.get('[data-cy="test-label-required-lg"]')
-      .find('label.chi-label')
-      .should('have.class', '-lg');
-  });
-
-  it('Class xl is present in required. ', function() {
-    cy.get('[data-cy="test-label-required-xl"]')
-      .find('label.chi-label')
-      .should('have.class', '-xl');
-  });
-
-  it('Class xs is present in optional. ', function() {
-    cy.get('[data-cy="test-label-optional-xs"]')
-      .find('label.chi-label')
-      .should('have.class', '-xs');
-  });
-
-  it('Class sm is present in optional. ', function() {
-    cy.get('[data-cy="test-label-optional-sm"]')
-      .find('label.chi-label')
-      .should('have.class', '-sm');
-  });
-
-  it('Class md is present in optional. ', function() {
-    cy.get('[data-cy="test-label-optional-md"]')
-      .find('label.chi-label')
-      .should('have.class', '-md');
-  });
-
-  it('Class lg is present in optional. ', function() {
-    cy.get('[data-cy="test-label-optional-lg"]')
-      .find('label.chi-label')
-      .should('have.class', '-lg');
-  });
-
-  it('Class xl is present in optional. ', function() {
-    cy.get('[data-cy="test-label-optional-xl"]')
-      .find('label.chi-label')
-      .should('have.class', '-xl');
-  });
+    beforeEach(() => {
+      labelSizes.forEach(s => {
+        cy.get(`[data-cy=${s}]`, { timeout: 5000 })
+          .find('label.chi-label')
+          .should('have.class', 'chi-label')
+          .as(s);
+      });
+    });
 });
