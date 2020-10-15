1233fce6413aa6cd559a4f362c9fbd29d283c108
Sebastian <Joshua.Sebastian@centurylink.com>
2020-10-15 15:19:03 +0530

    [FSPE-6062] E2E test for Chi Drawer Review component Fix

--- cypress/integration/chi-js/chi-drawer.spec.js
+++ cypress/integration/chi-js/chi-drawer.spec.js
@@ -1,172 +1,114 @@
 describe('chi-drawer', function() {
   before(() => {
-    cy.visit('http://localhost:8002/tests/js/chi-drawer.html');
-  });
-
-  describe('open left drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-1').click()
-      ;
-    });
+    cy.visit('http://localhost:8004/tests/js/chi-drawer.html');
   });
 
   describe('check that click on "X" in left menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-1')
+      cy.get('#drawer-trigger-1')
+        .click()
+        .get('#drawer-1')
         .should('have.class', '-active')
         .should('have.class', '-left')
         .should('be.visible')
         .find('.-close')
         .click()
-        .should('not.be.visible')
         .parent('.chi-drawer')
+        .should('not.be.visible')
+        .get('#drawer-1')
         .should('not.have.class', '-active')
-      ;
-    });
-  });
-
-  describe('open right drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-5').click()
       ;
     });
   });
 
   describe('check that click on "X" in right menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-5')
-        .should('have.class', '-active')
+      cy.get('#drawer-trigger-5').click()
+        .get('#drawer-5')
+        .should('have.class', '-animated')
         .should('have.class', '-right')
         .should('be.visible')
         .find('.-close')
         .click()
+        .parent('.chi-drawer')       
         .should('not.be.visible')
-        .parent('.chi-drawer')
-        .should('not.have.class', '-active')
       ;
     });
   });
 
-  describe('open top drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-7').click()
-      ;
-    });
-  });
 
   describe('check that click on "X" in top menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-7')
-        .should('have.class', '-active')
+      cy.get('#drawer-trigger-7').click()
+        .get('#drawer-7')
         .should('have.class', '-top')
         .should('be.visible')
         .find('.-close')
         .click()
-        .should('not.be.visible')
         .parent('.chi-drawer')
-        .should('not.have.class', '-active')
-      ;
-    });
-  });
-
-
-  describe('open bottom drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-9').click()
+        .should('not.be.visible')
       ;
     });
   });
 
   describe('check that click on "X" in bottom menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-9')
-        .should('have.class', '-active')
+      cy.get('#drawer-trigger-9').click()
+        .get('#drawer-9')
         .should('have.class', '-bottom')
         .should('be.visible')
         .find('.-close')
         .click()
-        .should('not.be.visible')
         .parent('.chi-drawer')
-        .should('not.have.class', '-active')
+        .should('not.be.visible')
       ;
     });
   });
-
-  describe('open drawer with backdrop on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-11').click()
-      ;
-    });
-  });
-
 
   describe('check that click on "X" in drawer with backdrop, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-11')
+      cy.get('#drawer-trigger-11').click() 
+        .get('#drawer-11')
         .parents('div')
         .should('have.class', 'chi-backdrop')
-      ;
-  
-      cy.get('#drawer-11')
-        .should('have.class', '-active')
+        .get('#drawer-11')
         .should('have.class', '-bottom')
         .should('be.visible')
         .find('.-close')
         .click()
-        .should('not.be.visible')
         .parent('.chi-drawer')
-        .should('not.have.class', '-active')
+        .should('not.be.visible')
       ;
     });
   });
-
-
-  describe('open titled drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-13').click()
-      ;
-    });
-  });
-
 
   describe('check that click on "X" in titled menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-13')
-        .should('have.class', '-active')
-        .should('have.class', '-left')
+      cy.get('#drawer-trigger-13').click()
+        .get('#drawer-13')
+        .find('.chi-drawer__header')
         .should('be.visible')
         .find('.-close')
         .click()
-        .should('not.be.visible')
         .parent('.chi-drawer')
-        .should('not.have.class', '-active')
+        .should('not.be.visible')
       ;
     });
   });
  
-  describe('open portal drawer on click', () => {
-    it('click drawer', () => {
-      cy.get('#drawer-trigger-23').click()
-      ;
-    });
-  });
-
-  describe('check that click on "X" in left menu drawer, closes drawer', () => {
+  describe('check that click on "X" in left menu portal drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-23')
+      cy.get('#drawer-trigger-23').click() 
+        .get('#drawer-23')
         .parents('div')
-        .should('have.class', 'chi-backdrop')
-      ;
-  
-      cy.get('#drawer-23')
-        .should('have.class', '-active')
-        .should('have.class', '-bottom')
+        .should('have.class', 'chi-backdrop') 
+        .get('#drawer-23')
         .should('have.class', '-portal')
         .should('be.visible')
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.have.class', '-active')
+        .should('not.be.visible')
       ;
     });
   });
