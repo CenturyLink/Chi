15342248fcee3be604d1e4a777d3866dcc9f1d5d
Sebastian <Joshua.Sebastian@centurylink.com>
2020-10-15 16:29:08 +0530

    [FSPE-6062] lint error fix to avoid build failure

--- cypress/integration/chi-js/chi-drawer.spec.js
+++ cypress/integration/chi-js/chi-drawer.spec.js
@@ -16,58 +16,57 @@ describe('chi-drawer', function() {
         .parent('.chi-drawer')
         .should('not.be.visible')
         .get('#drawer-1')
-        .should('not.have.class', '-active')
-      ;
+        .should('not.have.class', '-active');
     });
   });
 
   describe('check that click on "X" in right menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-5').click()
+      cy.get('#drawer-trigger-5')
+        .click()
         .get('#drawer-5')
         .should('have.class', '-animated')
         .should('have.class', '-right')
         .should('be.visible')
         .find('.-close')
         .click()
-        .parent('.chi-drawer')       
-        .should('not.be.visible')
-      ;
+        .parent('.chi-drawer')
+        .should('not.be.visible');
     });
   });
 
-
   describe('check that click on "X" in top menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-7').click()
+      cy.get('#drawer-trigger-7')
+        .click()
         .get('#drawer-7')
         .should('have.class', '-top')
         .should('be.visible')
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.be.visible')
-      ;
+        .should('not.be.visible');
     });
   });
 
   describe('check that click on "X" in bottom menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-9').click()
+      cy.get('#drawer-trigger-9')
+        .click()
         .get('#drawer-9')
         .should('have.class', '-bottom')
         .should('be.visible')
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.be.visible')
-      ;
+        .should('not.be.visible');
     });
   });
 
   describe('check that click on "X" in drawer with backdrop, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-11').click() 
+      cy.get('#drawer-trigger-11')
+        .click()
         .get('#drawer-11')
         .parents('div')
         .should('have.class', 'chi-backdrop')
@@ -77,39 +76,38 @@ describe('chi-drawer', function() {
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.be.visible')
-      ;
+        .should('not.be.visible');
     });
   });
 
   describe('check that click on "X" in titled menu drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-13').click()
+      cy.get('#drawer-trigger-13')
+        .click()
         .get('#drawer-13')
         .find('.chi-drawer__header')
         .should('be.visible')
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.be.visible')
-      ;
+        .should('not.be.visible');
     });
   });
- 
+
   describe('check that click on "X" in left menu portal drawer, closes drawer', () => {
     it('drawer closes on clicking X', () => {
-      cy.get('#drawer-trigger-23').click() 
+      cy.get('#drawer-trigger-23')
+        .click()
         .get('#drawer-23')
         .parents('div')
-        .should('have.class', 'chi-backdrop') 
+        .should('have.class', 'chi-backdrop')
         .get('#drawer-23')
         .should('have.class', '-portal')
         .should('be.visible')
         .find('.-close')
         .click()
         .parent('.chi-drawer')
-        .should('not.be.visible')
-      ;
+        .should('not.be.visible');
     });
   });
 });
