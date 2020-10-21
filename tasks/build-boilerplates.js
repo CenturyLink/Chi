0f5862206ecf5c123e7c980066f5fc3dd9f99d64
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-10-21 18:36:07 +0200

    Added prefix chi- to Boilerplate Zip files

--- tasks/build-boilerplates.js
+++ tasks/build-boilerplates.js
@@ -6,7 +6,7 @@ const debug = require('gulp-debug');
 
 const buildVueBoilerplate = () => {
   return gulp.src(['src/boilerplates/vue/**/*', '!src/boilerplates/vue/vue-boilerplate/node_modules/**/*'])
-    .pipe(zip('dist/boilerplates/vue-boilerplate.zip'))
+    .pipe(zip('dist/boilerplates/chi-vue-boilerplate.zip'))
     .pipe(gulp.dest('./'));
 };
 
@@ -14,7 +14,7 @@ buildVueBoilerplate.description = 'Zips Vue boilerplate';
 
 const buildStencilBoilerplate = () => {
   return gulp.src(['src/boilerplates/stencil/**/*', '!src/boilerplates/stencil/stencil-boilerplate/node_modules/**/*'])
-    .pipe(zip('dist/boilerplates/stencil-boilerplate.zip'))
+    .pipe(zip('dist/boilerplates/chi-stencil-boilerplate.zip'))
     .pipe(gulp.dest('./'));
 };
 
@@ -22,7 +22,7 @@ buildStencilBoilerplate.description = 'Zips Stencil boilerplate';
 
 const buildReactBoilerplate = () => {
   return gulp.src(['src/boilerplates/react/**/*', '!src/boilerplates/react/react-boilerplate/node_modules/**/*'])
-    .pipe(zip('dist/boilerplates/react-boilerplate.zip'))
+    .pipe(zip('dist/boilerplates/chi-react-boilerplate.zip'))
     .pipe(gulp.dest('./'));
 };
 
@@ -30,7 +30,7 @@ buildReactBoilerplate.description = 'Zips React boilerplate';
 
 const buildAngularBoilerplate = () => {
   return gulp.src(['src/boilerplates/angular/**/*', '!src/boilerplates/angular/angular-boilerplate/node_modules/**/*'])
-    .pipe(zip('dist/boilerplates/angular-boilerplate.zip'))
+    .pipe(zip('dist/boilerplates/chi-angular-boilerplate.zip'))
     .pipe(gulp.dest('./'));
 };
 
