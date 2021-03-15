df35071a3ca493507f5af25b9d890f132563ce17
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2021-03-15 11:23:38 +0100

    Changed build:chi task order

--- tasks/build.js
+++ tasks/build.js
@@ -6,7 +6,7 @@ gulp.task('build', gulp.series(
   'build:test',
   'sri:chi:assets',
   'build:website',
-  'copy:chi:ce-scripts',
   'update:boilerplate:assets',
-  'build:boilerplates'
+  'build:boilerplates',
+  'copy:chi:ce-scripts'
 ));
