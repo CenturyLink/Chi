682df5b99ba5486ccd2b8baacbd79b6dfddfdd7f
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2021-03-17 00:27:30 +0100

    [FSPE-5109] Added support of SRI to Chi assets

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -63,7 +63,8 @@ build() {
     unbuffer npm run build 2>&1 | addheader_custom_elements
     cd /chi/src/chi-vue
     unbuffer npm run build:component 2>&1 | sed "s/^[[:space:]]*..*\$/${PREFIX_VUE}&/"
-
+    cd /chi
+    unbuffer npm run sri 2>&1 | addheader_chi
 }
 
 publish_chi_vue() {
@@ -94,6 +95,7 @@ test() {
 
     cd /chi
     npm run test
+    unbuffer npm run sri 2>&1 | addheader_chi
 }
 
 OPTION=$1
---
+++ tasks/update-boilerplate-assets.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const fs = require('fs');
+const currentVersion = JSON.parse(fs.readFileSync('./package.json')).version;
+
+gulp.task('update:boilerplate:assets', function(done) {
+  const sriJson = JSON.parse(fs.readFileSync('./sri.json'));
+  const cssSRI = sriJson['dist/chi.css'];
+  const jsSRI = sriJson['dist/js/chi.js'];
+  const favSVGSRI = sriJson['src/website/assets/images/favicon.svg'];
+  const favICOSRI = sriJson['src/website/assets/images/favicon.ico'];
+  const ceModuleSRI = sriJson['dist/js/ce/ux-chi-ce/ux-chi-ce.esm.js'];
+  const ceNoModuleSRI = sriJson['dist/js/ce/ux-chi-ce.js'];
+
+  const newCSS = `<link rel="stylesheet" href="https://assets.ctl.io/chi/${currentVersion}/chi.css" integrity="${cssSRI}" crossorigin="anonymous">`;
+  const newJS = `<script src="https://assets.ctl.io/chi/${currentVersion}/js/chi.js" integrity="${jsSRI}" crossorigin="anonymous"></script>`;
+  const newFavSVG = `<link rel="icon" type="image/svg+xml" href="https://assets.ctl.io/chi/${currentVersion}/assets/images/favicon.svg" integrity="${favSVGSRI}" crossorigin="anonymous">`;
+  const newFavICO = `<link rel="alternate icon" href="https://assets.ctl.io/chi/${currentVersion}/assets/images/favicon.ico" integrity="${favICOSRI}" crossorigin="anonymous">`;
+  const newCEModule = `<script type="module" src="https://assets.ctl.io/chi/${currentVersion}/js/ce/ux-chi-ce/ux-chi-ce.esm.js" integrity="${ceModuleSRI}" crossorigin="anonymous"></script>`;
+  const newCENoModule = `<script nomodule="" src="https://assets.ctl.io/chi/${currentVersion}/js/ce/ux-chi-ce/ux-chi-ce.js" integrity="${ceNoModuleSRI}" crossorigin="anonymous"></script>`;
+
+  function updateIndexFile(path) {
+    gulp.src(`${path}/index.html`)
+      .pipe(replace(/<link rel="stylesheet" href="https:\/\/assets\.ctl\.io\/chi.*\/chi.css.*>/g,
+        newCSS))
+      .pipe(replace(/<script src="https:\/\/assets.ctl.io\/chi\/.*\/chi.js.*><\/script>/g,
+        newJS))
+      .pipe(replace(/<link rel="icon" type="image\/svg.xml" href="https:\/\/assets.ctl.io\/chi\/.*favicon.svg.*>/g,
+        newFavSVG))
+      .pipe(replace(/<link rel="alternate icon" href="https:\/\/assets.ctl.io\/chi\/.*favicon.ico.*>/g,
+        newFavICO))
+      .pipe(replace(/<script type="module" src="https:\/\/assets.ctl.io\/chi\/.*ux-chi-ce.esm.js.*><\/script>/g,
+        newCEModule))
+      .pipe(replace(/<script nomodule="" src="https:\/\/assets.ctl.io\/chi\/.*ux-chi-ce.js.*><\/script>/g,
+        newCENoModule))
+      .pipe(gulp.dest(path));
+    done();
+  }
+
+  [
+    'src/boilerplates/angular/chi-angular-boilerplate/src',
+    'src/boilerplates/es6/chi-vue-es6-boilerplate/public',
+    'src/boilerplates/react/chi-react-boilerplate/public',
+    'src/boilerplates/stencil/chi-stencil-boilerplate/src',
+    'src/boilerplates/vue/chi-vue-boilerplate/public'
+  ].forEach(path => updateIndexFile(path));
+});
