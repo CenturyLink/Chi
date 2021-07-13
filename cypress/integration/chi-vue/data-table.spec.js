21300f47cf40b3d61c577c3cd70a042b68543d91
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2021-07-13 18:45:12 +0200

    [FSDE-1106][FSDE-1123] Added support of Server Side Pagination feature to Chi Vue Data Table component (#100)
    * Implemented Server Side Pagination
    
    * [FSDE-1106] Added support of automatic selection of nested rows
    
    * [FSDE-1106] Added support of optional results config to Data Table
    
    add server-side e2e tests
    
    add e2e tests for server-side-portal
    
    fix sorting and refactor code
    
    minor refactor and bug fix
    
    improve naming of variables
    
    move server-side tests into data-table.spec

--- cypress/integration/chi-vue/data-table.spec.js
+++ cypress/integration/chi-vue/data-table.spec.js
@@ -65,6 +65,30 @@ const hasClassAssertion = (el, value) => {
   cy.get(el).should('have.class', value);
 };
 
+const checkStatusSorting = statuses => {
+  statuses.forEach((status, index) => {
+    cy.get('@rows')
+      .eq(index)
+      .should('contain', status);
+  });
+};
+
+const checkDateSorting = dates => {
+  dates.forEach((date, index) => {
+    cy.get('@rows')
+      .eq(index)
+      .should('contain', date);
+  });
+};
+
+const isSelected = elements => {
+  elements.forEach(el => expect(el.selected).to.be.true);
+};
+
+const isNotSelected = elements => {
+  elements.forEach(el => expect(el.selected).to.be.false);
+};
+
 describe('Data Table', () => {
   before(() => {
     cy.visit('tests/chi-vue/data-table.html');
@@ -651,22 +675,8 @@ describe('Data Table', () => {
           });
       });
 
-      function checkStatusSorting(statuses) {
-        statuses.forEach((status, index) => {
-          if (index === 2) {
-            cy.get('@rows')
-              .eq(index)
-              .should('contain', status);
-          } else {
-            cy.get('@rows')
-              .eq(index)
-              .should('contain', status);
-          }
-        });
-      }
-
       it(`should sort by status using a custom template`, () => {
-        const statuses = ['active', 'inactive', 'active'];
+        const statuses = ['active', 'inact', 'active'];
 
         checkStatusSorting(statuses);
         cy.get(`[data-cy='data-table-sorting'] .${DATA_TABLE_CLASSES.ROW}`)
@@ -683,7 +693,7 @@ describe('Data Table', () => {
         cy.get('@idCell')
           .click()
           .then(() => {
-            const inactives = ['inactive', 'inactive', 'inactive'];
+            const inactives = ['inact', 'inact', 'inact'];
 
             checkStatusSorting(inactives);
           });
@@ -693,14 +703,6 @@ describe('Data Table', () => {
             checkStatusSorting(statuses);
           });
       });
-
-      function checkDateSorting(dates) {
-        dates.forEach((date, index) => {
-          cy.get('@rows')
-            .eq(index)
-            .should('contain', date);
-        });
-      }
 
       it(`should sort by date`, () => {
         const dates = ['6 Jan 2018', '5 Jul 2018', '5 Apr 2019'];
@@ -1124,6 +1126,502 @@ describe('Data Table Portal', () => {
     it('should have 2-level collapsed content', () => {
       cy.get(`@childRows`).should('contain', 'Child Name 1');
       cy.get(`@grandchildRows`).should('contain', 'Grand-child Name 1');
+    });
+  });
+});
+
+describe('Server Side Data Table', () => {
+  before(() => {
+    cy.visit('tests/chi-vue/server-side.html');
+  });
+
+  describe('pagination', () => {
+    beforeEach(() => {
+      cy.get(`[data-cy='data-table']`)
+        .children()
+        .eq(1)
+        .as('body')
+        .children()
+        .first()
+        .as('row');
+      cy.get(`[data-cy='data-table'] .${PAGINATION_CLASSES.PAGINATION}`)
+        .as('pagination')
+        .find(`.${ICON_BUTTON}`)
+        .as('paginationIcons');
+    });
+
+    it('should render new data when going to page two', () => {
+      cy.get('@paginationIcons')
+        .eq(2)
+        .click()
+        .then(() => {
+          cy.get('@row').should('contain', 'Name 4');
+        });
+      cy.get('@paginationIcons')
+        .eq(1)
+        .click()
+        .then(() => {
+          cy.get(`@row`).should('contain', 'Name 1');
+        });
+    });
+
+    it(`should trigger the ${PAGINATION_EVENTS.PAGE_CHANGE} event`, () => {
+      cy.window()
+        .its('dataTableExample')
+        .then(dataTableExample => {
+          const spy = cy.spy();
+          const dataTableRef = dataTableExample.$refs.dataTableRef;
+
+          dataTableRef.$on(`${PAGINATION_EVENTS.PAGE_CHANGE}`, spy);
+          cy.get('@paginationIcons')
+            .eq(2)
+            .click()
+            .then(() => {
+              expect(spy).to.be.calledWith({ page: 2 });
+            });
+          cy.get('@paginationIcons')
+            .eq(1)
+            .click()
+            .then(() => {
+              expect(spy).to.be.calledWith({ page: 1 });
+            });
+        });
+    });
+  });
+
+  describe('selection', () => {
+    beforeEach(() => {
+      cy.get(`[data-cy='data-table']`)
+        .find(`.${DATA_TABLE_CLASSES.SELECTABLE}`)
+        .as('selectables');
+    });
+
+    it(`should trigger the ${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE} event`, () => {
+      cy.window()
+        .its('dataTableExample')
+        .then(dataTableExample => {
+          const spy = cy.spy();
+          const dataTableRef = dataTableExample.$refs.dataTableRef;
+
+          dataTableRef.$on(`${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE}`, spy);
+          cy.get('@selectables')
+            .eq(1)
+            .click()
+            .then(() => {
+              isSelected([dataTableExample.table.body[0]]);
+              expect(spy).to.be.calledWith([
+                {
+                  active: false,
+                  data: [
+                    'Name 1',
+                    {
+                      payload: {
+                        status: 'active'
+                      },
+                      template: 'status'
+                    },
+                    'Dec 18, 2020 3:26 PM'
+                  ],
+                  id: 'name-1',
+                  nestedContent: {
+                    value: 'Accordion content'
+                  },
+                  rowId: 'row-dt-0-name-1',
+                  rowNumber: '0'
+                }
+              ]);
+            });
+          cy.get('@selectables')
+            .eq(1)
+            .click()
+            .then(() => {
+              isNotSelected([dataTableExample.table.body[0]]);
+            });
+        });
+    });
+
+    it('should select and deselect a row', () => {
+      cy.get('@selectables')
+        .eq(1)
+        .click()
+        .then(() => {
+          cy.get(
+            `[data-cy='data-table'] .${DATA_TABLE_CLASSES.BODY} .${DATA_TABLE_CLASSES.ROW}`
+          )
+            .eq(0)
+            .as('firstRow');
+          hasClassAssertion('@firstRow', ACTIVE_CLASS);
+        });
+      cy.get('@selectables')
+        .eq(1)
+        .click()
+        .then(() => {
+          cy.get('@firstRow').should('not.have.class', ACTIVE_CLASS);
+        });
+    });
+  });
+
+  describe('sorting', () => {
+    it('should sort by status in asc and desc', () => {
+      const statuses = ['active', 'inact', 'active'];
+
+      cy.get(`[data-cy='data-table'] .${DATA_TABLE_CLASSES.BODY}`)
+        .find(`.${DATA_TABLE_CLASSES.ROW}`)
+        .as('rows');
+      checkStatusSorting(statuses);
+      cy.get(`[data-cy='data-table'] .${DATA_TABLE_CLASSES.ROW}`)
+        .eq(0)
+        .find(`.${DATA_TABLE_CLASSES.CELL}`)
+        .eq(3)
+        .as('idCell')
+        .click()
+        .then(() => {
+          const actives = ['active', 'active', 'active'];
+
+          checkStatusSorting(actives);
+        });
+      cy.get('@idCell')
+        .click()
+        .then(() => {
+          const inactives = ['inact', 'inact', 'inact'];
+
+          checkStatusSorting(inactives);
+        });
+      cy.get(`[data-cy='data-table'] .${PAGINATION_CLASSES.PAGINATION}`)
+        .as('pagination')
+        .find(`.${ICON_BUTTON}`)
+        .as('paginationIcons')
+        .eq(2)
+        .click()
+        .then(() => {
+          const actives = ['active', 'active', 'active'];
+
+          checkStatusSorting(actives);
+        });
+      cy.get('@paginationIcons')
+        .eq(1)
+        .click()
+        .then(() => {
+          const inactives = ['inact', 'inact', 'inact'];
+
+          checkStatusSorting(inactives);
+        });
+      cy.get('@idCell')
+        .click()
+        .then(() => {
+          checkStatusSorting(statuses);
+        });
+    });
+
+    it(`should trigger the ${DATA_TABLE_EVENTS.DATA_SORTING} event`, () => {
+      it('should sort by status in asc and desc', () => {
+        cy.window()
+          .its('dataTableExample')
+          .then(dataTableExample => {
+            const spy = cy.spy();
+            const dataTableRef = dataTableExample.$refs.dataTableRef;
+
+            dataTableRef.$on(`${DATA_TABLE_EVENTS.DATA_SORTING}`, spy);
+            cy.get(`[data-cy='data-table'] .${DATA_TABLE_CLASSES.ROW}`)
+              .eq(0)
+              .find(`.${DATA_TABLE_CLASSES.CELL}`)
+              .eq(3)
+              .as('idCell')
+              .click()
+              .then(() => {
+                expect(spy).to.be.calledOnceWith({
+                  column: 'id',
+                  direction: 'ascending'
+                });
+              });
+            cy.get('@idCell')
+              .click()
+              .then(() => {
+                expect(spy).to.be.calledWith({
+                  column: 'id',
+                  direction: 'descending'
+                });
+              });
+          });
+      });
+    });
+  });
+
+  describe('portal', () => {
+    before(() => {
+      cy.visit('tests/chi-vue/server-side-portal.html');
+    });
+
+    describe('pagination', () => {
+      beforeEach(() => {
+        cy.get(`[data-cy='data-table-portal']`)
+          .children()
+          .eq(1)
+          .as('body')
+          .children()
+          .first()
+          .as('row');
+        cy.get(
+          `[data-cy='data-table-portal'] .${PAGINATION_CLASSES.PAGINATION}`
+        )
+          .as('pagination')
+          .find(`.${ICON_BUTTON}`)
+          .as('paginationIcons');
+      });
+
+      it('should render new data when going to page two', () => {
+        cy.get('@paginationIcons')
+          .eq(2)
+          .click()
+          .then(() => {
+            cy.get('@row').should('contain', 'Name 4');
+          });
+        cy.get('@paginationIcons')
+          .eq(1)
+          .click()
+          .then(() => {
+            cy.get(`@row`).should('contain', 'Name 1');
+          });
+      });
+
+      it(`should trigger the ${PAGINATION_EVENTS.PAGE_CHANGE} event`, () => {
+        cy.window()
+          .its('dataTablePortalExample')
+          .then(dataTablePortalExample => {
+            const spy = cy.spy();
+            const dataTableRef =
+              dataTablePortalExample.$refs.dataTablePortalRef;
+
+            dataTableRef.$on(`${PAGINATION_EVENTS.PAGE_CHANGE}`, spy);
+            cy.get('@paginationIcons')
+              .eq(2)
+              .click()
+              .then(() => {
+                expect(spy).to.be.calledWith({ page: 2 });
+              });
+            cy.get('@paginationIcons')
+              .eq(1)
+              .click()
+              .then(() => {
+                expect(spy).to.be.calledWith({ page: 1 });
+              });
+          });
+      });
+    });
+
+    describe('selection', () => {
+      beforeEach(() => {
+        cy.get(`[data-cy='data-table-portal']`)
+          .find(`.${DATA_TABLE_CLASSES.SELECTABLE}`)
+          .as('selectables');
+      });
+
+      it(`should trigger the ${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE} event`, () => {
+        cy.window()
+          .its('dataTablePortalExample')
+          .then(dataTablePortalExample => {
+            const spy = cy.spy();
+            const dataTableRef =
+              dataTablePortalExample.$refs.dataTablePortalRef;
+
+            dataTableRef.$on(`${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE}`, spy);
+            cy.get('@selectables')
+              .eq(1)
+              .click()
+              .then(() => {
+                isSelected([dataTablePortalExample.table.body[0]]);
+                expect(spy).to.be.calledWith([
+                  {
+                    active: false,
+                    data: [
+                      'Name 1',
+                      {
+                        payload: {
+                          status: 'active'
+                        },
+                        template: 'status'
+                      },
+                      'Dec 18, 2020 3:26 PM'
+                    ],
+                    id: 'name-1',
+                    nestedContent: {
+                      value: 'Accordion content'
+                    },
+                    rowId: 'row-dt-0-name-1',
+                    rowNumber: '0'
+                  }
+                ]);
+              });
+            cy.get('@selectables')
+              .eq(1)
+              .click()
+              .then(() => {
+                isNotSelected([dataTablePortalExample.table.body[0]]);
+              });
+          });
+      });
+
+      it('should select and deselect a row', () => {
+        cy.get('@selectables')
+          .eq(1)
+          .click()
+          .then(() => {
+            cy.get(
+              `[data-cy='data-table-portal'] .${DATA_TABLE_CLASSES.BODY} .${DATA_TABLE_CLASSES.ROW}`
+            )
+              .eq(0)
+              .as('firstRow');
+            hasClassAssertion('@firstRow', ACTIVE_CLASS);
+          });
+        cy.get('@selectables')
+          .eq(1)
+          .click()
+          .then(() => {
+            cy.get('@firstRow').should('not.have.class', ACTIVE_CLASS);
+          });
+      });
+
+      it('should automatically select nested rows', () => {
+        cy.window()
+          .its('dataTablePortalExample')
+          .then(dataTablePortalExample => {
+            const spy = cy.spy();
+            const dataTableRef =
+              dataTablePortalExample.$refs.dataTablePortalRef;
+            const parent = dataTablePortalExample.table.body[2];
+            const child = parent.nestedContent.table.data[0];
+            const grandchild = child.nestedContent.table.data[0];
+
+            dataTableRef.$on(`${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE}`, spy);
+            cy.get('@selectables')
+              .eq(3)
+              .click()
+              .then(() => {
+                expect(spy).to.be.called;
+                isSelected([parent, child, grandchild]);
+              });
+            cy.get('@selectables')
+              .eq(3)
+              .click()
+              .then(() => {
+                expect(spy).to.be.called;
+              });
+          });
+      });
+
+      it('should automatically deselect nested rows', () => {
+        cy.get('@selectables')
+          .eq(3)
+          .click();
+        cy.window()
+          .its('dataTablePortalExample')
+          .then(dataTablePortalExample => {
+            const spy = cy.spy();
+            const dataTableRef =
+              dataTablePortalExample.$refs.dataTablePortalRef;
+            const parent = dataTablePortalExample.table.body[2];
+            const child = parent.nestedContent.table.data[0];
+            const grandchild = child.nestedContent.table.data[0];
+
+            dataTableRef.$on(`${DATA_TABLE_EVENTS.SELECTED_ROWS_CHANGE}`, spy);
+            cy.get('@selectables')
+              .eq(3)
+              .click()
+              .then(() => {
+                expect(spy).to.be.called;
+                isNotSelected([parent, child, grandchild]);
+              });
+          });
+      });
+    });
+
+    describe('sorting', () => {
+      it('should sort by status in asc and desc', () => {
+        const statuses = ['active', 'inact', 'active'];
+
+        cy.get(`[data-cy='data-table-portal'] .${DATA_TABLE_CLASSES.BODY}`)
+          .find(`.${DATA_TABLE_CLASSES.ROW}`)
+          .as('rows');
+        checkStatusSorting(statuses);
+        cy.get(`[data-cy='data-table-portal'] .${DATA_TABLE_CLASSES.ROW}`)
+          .eq(0)
+          .find(`.${DATA_TABLE_CLASSES.CELL}`)
+          .eq(3)
+          .as('idCell')
+          .click()
+          .then(() => {
+            const actives = ['active', 'active', 'active'];
+
+            checkStatusSorting(actives);
+          });
+        cy.get('@idCell')
+          .click()
+          .then(() => {
+            const inactives = ['inact', 'inact', 'inact'];
+
+            checkStatusSorting(inactives);
+          });
+        cy.get(
+          `[data-cy='data-table-portal'] .${PAGINATION_CLASSES.PAGINATION}`
+        )
+          .as('pagination')
+          .find(`.${ICON_BUTTON}`)
+          .as('paginationIcons')
+          .eq(2)
+          .click()
+          .then(() => {
+            const actives = ['active', 'active', 'active'];
+
+            checkStatusSorting(actives);
+          });
+        cy.get('@paginationIcons')
+          .eq(1)
+          .click()
+          .then(() => {
+            const inactives = ['inact', 'inact', 'inact'];
+
+            checkStatusSorting(inactives);
+          });
+        cy.get('@idCell')
+          .click()
+          .then(() => {
+            checkStatusSorting(statuses);
+          });
+      });
+
+      it(`should trigger the ${DATA_TABLE_EVENTS.DATA_SORTING} event`, () => {
+        it('should sort by status in asc and desc', () => {
+          cy.window()
+            .its('dataTablePortalExample')
+            .then(dataTablePortalExample => {
+              const spy = cy.spy();
+              const dataTableRef =
+                dataTablePortalExample.$refs.dataTablePortalRef;
+
+              dataTableRef.$on(`${DATA_TABLE_EVENTS.DATA_SORTING}`, spy);
+              cy.get(`[data-cy='data-table-portal'] .${DATA_TABLE_CLASSES.ROW}`)
+                .eq(0)
+                .find(`.${DATA_TABLE_CLASSES.CELL}`)
+                .eq(3)
+                .as('idCell')
+                .click()
+                .then(() => {
+                  expect(spy).to.be.calledOnceWith({
+                    column: 'id',
+                    direction: 'ascending'
+                  });
+                });
+              cy.get('@idCell')
+                .click()
+                .then(() => {
+                  expect(spy).to.be.calledWith({
+                    column: 'id',
+                    direction: 'descending'
+                  });
+                });
+            });
+        });
+      });
     });
   });
 });
