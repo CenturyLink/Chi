e8db5294c82d2c6a6d22bab0d9d1cabf633d3ec9
Evan <cugat.evan@gmail.com>
2021-07-22 17:06:53 +0200

    add waiting time for another e2e test

--- cypress/integration/chi-vue/data-table.spec.js
+++ cypress/integration/chi-vue/data-table.spec.js
@@ -1376,12 +1376,14 @@ describe('Server Side Data Table', () => {
           .eq(2)
           .click()
           .then(() => {
+            cy.wait(2000);
             cy.get('@row').should('contain', 'Name 4');
           });
         cy.get('@paginationIcons')
           .eq(1)
           .click()
           .then(() => {
+            cy.wait(2000);
             cy.get(`@row`).should('contain', 'Name 1');
           });
       });
