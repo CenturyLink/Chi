28e80dd3a32f8c2722d73122d027489ed5341269
soujanreddy <soujan.reddy@lumen.com>
2021-10-25 12:22:28 -0400

    [FSDE-8075] Migrate Flag Icon component to vue documentation

---
+++ src/documentation/pages/components/flag-icon/examples/_area.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  <ComponentExample title="Country/Region" id="area_lumen_centurylink" :tabs="exampleTabs">
+    div(slot="example", class="example -b--0")
+       section.chi-table.-bordered
+        div
+          table
+            thead
+              tr
+                th
+                  div Icon
+                th
+                  div Name
+                th
+                  div Class
+            tbody
+              each val, type in {ar:'Argentina', au:'Australia', br:'Brazil', co:'Colombia', de:'Germany', hk:'Hong Kong', in:'India', jp:'Japan', mx:'Mexico', sg:'Singapore', gb:'United Kingdom', us:'United States'}
+                tr
+                  td.-w--20
+                    i(class=`chi-flag-icon icon-${type}`, aria-hidden='true')
+                  td
+                    p.-m--0=val
+                  td
+                    code=`icon--${type}`
+    div(slot="code-webcomponent")
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+      </pre>
+    div(slot="code-htmlblueprint")
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+      </pre>          
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+declare const chi: any;
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          disabled: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: ``,
+        htmlblueprint: `
+      <i class="chi-flag-icon icon-ar" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-au" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-br" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-co" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-de" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-hk" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-in" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-jp" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-mx" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-sg" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-gb" aria-hidden="true"></i>
+      <i class="chi-flag-icon icon-us" aria-hidden="true"></i>`
+      }
+    };
+  }
+})
+export default class SolidExample extends Vue {
+  mounted() {
+    chi.tab(document.querySelectorAll('.chi-tabs-panel .chi-tabs'));
+  }
+}
+</script>
---
+++ src/documentation/pages/components/flag-icon/examples/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    h2 Examples
+    div
+      <Area />
+      <Sizes />     
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Area from './_area.vue';
+import Sizes from './_sizes.vue';
+
+@Component({
+  components: {
+    Area,
+    Sizes,
+  }
+})
+export default class FlagIconExamples extends Vue {}
+</script>
