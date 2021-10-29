ea7ccafa00e2af35b381666b2b5ee9cff14b1ee7
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2021-10-29 17:42:40 +0200

    [FSDE-8524] Added conditional Tooltip to Data Table header cells

--- src/chi-vue/src/components/data-table/DataTableTooltip.tsx
+++ src/chi-vue/src/components/data-table/DataTableTooltip.tsx
@@ -1,20 +1,41 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import Tooltip from '@/components/tooltip/tooltip';
-import { UTILITY_CLASSES } from '@/constants/classes';
 import { TOOLTIP_ANIMATION_DELAY } from '@/constants/constants';
+import { UTILITY_CLASSES } from '@/constants/classes';
 
 @Component({})
 export default class DataTableTooltip extends Vue {
   @Prop() msg!: string;
+  @Prop({ default: false }) header?: boolean;
 
   tooltip = false;
 
   onShow() {
-    this.tooltip = true;
+    const isTruncated = this._isTruncated();
+
+    this.tooltip = isTruncated;
   }
 
   onHide() {
     window.setTimeout(() => (this.tooltip = false), TOOLTIP_ANIMATION_DELAY);
+  }
+
+  _isTruncated() {
+    const wrapper = this.$refs.wrapper as HTMLElement;
+    const wrapperClone = wrapper.cloneNode(true) as HTMLElement;
+
+    wrapperClone.style.display = 'inline';
+    wrapperClone.style.width = 'auto';
+    wrapperClone.style.visibility = 'hidden';
+    wrapperClone.style.overflow = 'visible';
+
+    wrapper.parentNode?.appendChild(wrapperClone);
+
+    const isTruncated = wrapperClone.offsetWidth > wrapper.offsetWidth;
+
+    wrapperClone.parentNode?.removeChild(wrapperClone);
+
+    return isTruncated;
   }
 
   render() {
@@ -23,12 +44,17 @@ export default class DataTableTooltip extends Vue {
         onMouseenter={() => this.onShow()}
         onFocus={() => this.onShow()}
         onMouseleave={() => this.onHide()}
-        onBlur={() => this.onHide()}>
+        onBlur={() => this.onHide()}
+        style={`max-width: fit-content; width: ${this.$props.header ? 'calc(100% - 20px)' : '100%'};`}>
         {!this.tooltip ? (
-          <div class={UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE}>{this.msg}</div>
+          <div class={UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE} ref="wrapper">
+            {this.msg}
+          </div>
         ) : (
-          <Tooltip message={this.msg} class="-w--100">
-            <div class={UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE}>{this.msg}</div>
+          <Tooltip message={this.msg}>
+            <div class={UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE} ref="wrapper">
+              {this.msg}
+            </div>
           </Tooltip>
         )}
       </div>
