67e049cfd6139613af592565750956dfe7b79a64
soujanreddy <soujan.reddy@lumen.com>
2021-10-29 10:33:05 -0400

    [FSDE-8075] Code updates

--- src/documentation/pages/components/flag-icon/examples/_area.vue
+++ src/documentation/pages/components/flag-icon/examples/_area.vue
@@ -35,8 +35,6 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-declare const chi: any;
-
 @Component({
   data: () => {
     return {
--- src/documentation/pages/components/flag-icon/examples/_sizes.vue
+++ src/documentation/pages/components/flag-icon/examples/_sizes.vue
@@ -24,8 +24,6 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-declare const chi: any;
-
 @Component({
   data: () => {
     return {
