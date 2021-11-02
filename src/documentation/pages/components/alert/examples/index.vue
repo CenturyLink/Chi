0a127b92dfdb9a8fb59148a7bd3fff928e96e971
Evan <76445286+evansito@users.noreply.github.com>
2021-11-02 18:55:08 +0100

    [FSDE-4561] rename alert component names in vue doc
    Co-authored-by: Jose Lopez Ruiz <jose@elasticbox.com>

--- src/documentation/pages/components/alert/examples/index.vue
+++ src/documentation/pages/components/alert/examples/index.vue
@@ -23,21 +23,21 @@ div
       | Banner alerts are commonly presented above or below the Header component and are used for scenarios such as reminding new users
       | to confirm their email address or notifying users when they are experiencing connectivity issues.
 
-    <BannerDefaultCenturyLink />
-    <BannerCenteredCenturyLink />
-    <BannerTitledCenturyLink />
-    <BannerSizesCenturyLink />
-    <BannerClosableCenturyLink />
-    <BannerActionableCenturyLink />
+    <BannerDefaultLumenCenturyLink />
+    <BannerCenteredLumenCenturyLink />
+    <BannerTitledLumenCenturyLink />
+    <BannerSizesLumenCenturyLink />
+    <BannerClosableLumenCenturyLink />
+    <BannerActionableLumenCenturyLink />
 
     div.chi-divider.-my--6.-bt--2
 
     h3 Toast
     p.-text Toasts are unobtrusive alerts used to display brief, auto-expiring information.
 
-    <ToastDefaultCenturyLink />
-    <ToastTitledCenturyLink />
-    <ToastSizesCenturyLink />
+    <ToastDefaultLumenCenturyLink />
+    <ToastTitledLumenCenturyLink />
+    <ToastSizesLumenCenturyLink />
 
   div(v-if='$store.state.themes.theme === "portal"')
     <BubbleTitled />
@@ -53,15 +53,15 @@ import BubbleCenteredLumenCenturylink from './_bubble_centered_lumen_centurylink
 import BubbleSizesLumenCenturyLink from './_bubble_sizes_lumen_centurylink.vue';
 import BubbleClosableLumenCenturyLink from './_bubble_closable_lumen_centurylink.vue';
 import BubbleActionable from './_bubble_actionable.vue';
-import BannerDefaultCenturyLink from './_banner_default_lumen_centurylink.vue';
-import BannerCenteredCenturyLink from './_banner_centered_lumen_centurylink.vue';
-import BannerTitledCenturyLink from './_banner_titled_lumen_centurylink.vue';
-import BannerSizesCenturyLink from './_banner_sizes_lumen_centurylink.vue';
-import BannerClosableCenturyLink from './_banner_closable_lumen_centurylink.vue';
-import BannerActionableCenturyLink from './_banner_actionable_lumen_centurylink.vue';
-import ToastDefaultCenturyLink from './_toast_default_lumen_centurylink.vue';
-import ToastTitledCenturyLink from './_toast_titled_lumen_centurylink.vue';
-import ToastSizesCenturyLink from './_toast_sizes_lumen_centurylink.vue';
+import BannerDefaultLumenCenturyLink from './_banner_default_lumen_centurylink.vue';
+import BannerCenteredLumenCenturyLink from './_banner_centered_lumen_centurylink.vue';
+import BannerTitledLumenCenturyLink from './_banner_titled_lumen_centurylink.vue';
+import BannerSizesLumenCenturyLink from './_banner_sizes_lumen_centurylink.vue';
+import BannerClosableLumenCenturyLink from './_banner_closable_lumen_centurylink.vue';
+import BannerActionableLumenCenturyLink from './_banner_actionable_lumen_centurylink.vue';
+import ToastDefaultLumenCenturyLink from './_toast_default_lumen_centurylink.vue';
+import ToastTitledLumenCenturyLink from './_toast_titled_lumen_centurylink.vue';
+import ToastSizesLumenCenturyLink from './_toast_sizes_lumen_centurylink.vue';
 import BubbleDefault from './_bubble_default.vue';
 import BubbleTitled from './_bubble_titled.vue';
 import BubbleSizesPortal from './_portal/_bubble_sizes.vue';
@@ -75,15 +75,15 @@ import BubbleClosablePortal from './_portal/_bubble_closable.vue';
     BubbleSizesLumenCenturyLink,
     BubbleClosableLumenCenturyLink,
     BubbleActionable,
-    BannerDefaultCenturyLink,
-    BannerCenteredCenturyLink,
-    BannerTitledCenturyLink,
-    BannerSizesCenturyLink,
-    BannerClosableCenturyLink,
-    BannerActionableCenturyLink,
-    ToastDefaultCenturyLink,
-    ToastTitledCenturyLink,
-    ToastSizesCenturyLink,
+    BannerDefaultLumenCenturyLink,
+    BannerCenteredLumenCenturyLink,
+    BannerTitledLumenCenturyLink,
+    BannerSizesLumenCenturyLink,
+    BannerClosableLumenCenturyLink,
+    BannerActionableLumenCenturyLink,
+    ToastDefaultLumenCenturyLink,
+    ToastTitledLumenCenturyLink,
+    ToastSizesLumenCenturyLink,
     BubbleSizesPortal,
     BubbleClosablePortal
   }
