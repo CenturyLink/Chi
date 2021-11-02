cc3b01b2dfc9c52f7490ff20f1b97710e39a1fcd
soujanreddy <soujan.reddy@lumen.com>
2021-11-02 17:36:41 -0400

    [FSDE-6408] Add header component to vue documentation

---
+++ src/documentation/pages/components/header/examples/index.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  div
+    h3 Examples
+    p.-text 
+      | To use headers, apply the class <code>chi-header</code> to <code>&lt;header&gt;</code>
+      | and wrap all content in <code>chi-header__content</code>. Use <code>chi-header__start</code>
+      | to position elements at the start of the header and <code>chi-header__end</code> to
+      | position elements at the end of the header.
+    h3 Responsiveness
+    p.-text
+      | By default, all content stored in headers will render in drawer
+      | components when the viewport is too small to accommodate.
+
+    div
+      <Base />
+      <HeaderTitleFile />
+      <PortalHeader />
+      <PortalHeaderNavbar />
+      <HeaderImpersonation />
+      <HeaderTitleButtons />
+      <HeaderCollapseSearch />
+      <HeaderTopToolBar />
+      <HeaderMobileSecondary />
+      h3 Sizes
+      <SizesMedium />
+      <SizesLarge />
+      <SizesXLarge />
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+import HeaderTitleFile from './_header_title_file.vue';
+import PortalHeader from './_portal_header.vue';
+import PortalHeaderNavbar from './_portal_header_navbar.vue';
+import HeaderImpersonation from './_header_impersonation.vue';
+import HeaderTitleButtons from './_header_title_buttons.vue';
+import HeaderCollapseSearch from './_header_collapse_search.vue';
+import HeaderTopToolBar from './_header_toptoolbar.vue';
+import HeaderMobileSecondary from './_header_mobile_secondary.vue';
+import SizesMedium from './_sizes_medium.vue';
+import SizesLarge from './_sizes_large.vue';
+import SizesXLarge from './_sizes_xlarge.vue';
+
+@Component({
+  components: {
+    Base,
+    HeaderTitleFile,
+    PortalHeader,
+    PortalHeaderNavbar,
+    HeaderImpersonation,
+    HeaderTitleButtons,
+    HeaderCollapseSearch,
+    HeaderTopToolBar,
+    HeaderMobileSecondary,
+    SizesMedium,
+    SizesLarge,
+    SizesXLarge
+  }
+})
+export default class HeaderExamples extends Vue {}
+</script>
