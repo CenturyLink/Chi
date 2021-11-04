70dce0712b2f016a03ca9953232cf7b99939dcbc
soujanreddy <soujan.reddy@lumen.com>
2021-11-04 13:39:35 -0400

    [FSDE-8427] Add/update sizes for portal theme

--- src/documentation/pages/components/file-input/examples/index.vue
+++ src/documentation/pages/components/file-input/examples/index.vue
@@ -4,9 +4,15 @@
     div(slot="example-description")
       p.-text To render a file input, apply the class <code>chi-file-input</code>
         | to an <code>input type="file"</code> and add a label next to it.
-    div
+
+    div(v-if="$store.state.themes.theme === 'portal'")
       <Base />
-      <Disabled />    
+      <Disabled />
+      <SizesPortal />
+
+    div(v-if="['lumen', 'centurylink'].includes($store.state.themes.theme)")
+      <Base />
+      <Disabled />  
       <Sizes />
 </template>
 
@@ -15,12 +21,14 @@ import { Component, Vue } from 'vue-property-decorator';
 import Base from './_base.vue';
 import Disabled from './_disabled.vue';
 import Sizes from './_sizes.vue';
+import SizesPortal from './_portal/_sizes_portal.vue'
 
 @Component({
   components: {
     Base,
     Disabled,
     Sizes,
+    SizesPortal
   }
 })
 export default class Examples extends Vue {}
