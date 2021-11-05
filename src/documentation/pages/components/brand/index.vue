014ddf26aed5f2b8df449c33b9ebb28536ec6bb2
José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>
2021-11-05 12:49:38 +0100

    [FSDE-5953] Migrate Brand component to new Vue documentation (#222)
    * [FSDE-5953] Migrate Brand component to new Vue documentation
    
    [FSDE-5953] Migrate brand component to new Vue documentation
    
    * [FSDE-5953] Removed from Brand and Badge component examples mounted method to prevent unpredictable side effects
    
    * [FSDE-5953] Rebase and fix Badge examples title to h2 instead of h3
    
    * [FSDE-5953] Removed unnecessary stuff, changed paddings and naming in examples
    
    * [FSDE-5953] Removing blank spaces
    
    * [FSDE-5953] Removing unnecessary file

--- src/documentation/pages/components/brand/index.vue
+++ src/documentation/pages/components/brand/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    <TitleBar title="Badge" description="Badges are inline elements used to label, categorize, or organize a list of items." :tabs="pageTabs" />
+    <TitleBar title="Brand" description="Use brand to display brand logos in your project." :tabs="pageTabs" />
     .chi-grid__container.-pt--3
       .chi-tabs-panel.-active#examples
         <Examples />
@@ -12,26 +12,27 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Examples from './examples/index.vue';
 import PageContentTabs from '../../../components/PageContentTabs.vue';
 import Properties from './_properties.vue';
 import Accessibility from './_accessibility.vue';
+import Examples from './examples/index.vue';
 import { standardComponentPageTabs } from '../../../constants/constants';
 
 @Component({
   components: {
     Accessibility,
-    Examples,
     PageContentTabs,
-    Properties
+    Properties,
+    Examples
   },
   data: () => {
     return {
-      pageTabs: standardComponentPageTabs
-    };
-  }
+      pageTabs: standardComponentPageTabs,
+    }
+  },
 })
-export default class Badge extends Vue {
-  mounted() {}
+export default class Brand extends Vue {
+  mounted() {
+  }
 }
 </script>
