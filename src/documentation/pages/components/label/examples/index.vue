4223c607774e98bb5c62b617bb2566c5c97ec908
evansito <cugat.evan@gmail.com>
2021-12-15 13:23:20 +0100

    migrate label to new vue doc

---
+++ src/documentation/pages/components/label/examples/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  <Wrapper>
+    h2 Examples
+
+    <Base />
+    <Required />
+  </Wrapper>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+import Required from './_required.vue';
+
+@Component({
+  components: {
+    Base,
+    Required
+  }
+})
+export default class Examples extends Vue {}
+</script>
