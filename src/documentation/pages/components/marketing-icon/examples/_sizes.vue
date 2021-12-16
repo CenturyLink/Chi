607c3eb68d4154ff4fd432273846eceb4a3d031c
evansito <cugat.evan@gmail.com>
2021-12-16 16:50:01 +0100

    migrate marketing icon to new vue doc

---
+++ src/documentation/pages/components/marketing-icon/examples/_sizes.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  <ComponentExample title="Sizes" id="sizes" :tabs="exampleTabs">
+    p.-text(slot="example-description")
+      | Icons support the following sizes:
+      | <code>xs</code>, <code>sm</code>, <code>md</code>, <code>lg</code>.
+      | By default, icons render at 96x96px (<code>md</code>).
+    <Wrapper slot="example">
+      .chi-grid.-align-items--end
+        each size in ['xs', 'sm', 'md', 'lg']
+          div.-p--2.-m--1
+            chi-marketing-icon(icon="business-big-data", size=size)
+            p.-text.-text--center= size
+    </Wrapper>
+    <Wrapper slot="code-webcomponent">
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+      </pre>
+    </Wrapper>
+    <Wrapper slot="code-htmlblueprint">
+      <JSNeeded />
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+      </pre>
+    </Wrapper>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          disabled: true,
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<!-- xs : 72x72px -->
+<chi-marketing-icon icon="business-big-data" size="xs"></chi-marketing-icon>
+<!-- sm : 80x80px -->
+<chi-marketing-icon icon="business-big-data" size="sm"></chi-marketing-icon>
+<!-- md : 96x96px -->
+<chi-marketing-icon icon="business-big-data" size="md"></chi-marketing-icon>
+<!-- lg : 128x128px -->
+<chi-marketing-icon icon="business-big-data" size="lg"></chi-marketing-icon>`,
+        htmlblueprint: ``
+      }
+    };
+  }
+})
+export default class Sizes extends Vue {}
+</script>
---
+++ src/documentation/pages/components/marketing-icon/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  div
+    <TitleBar title="Marketing icon" description="Use Marketing icon to render Lumen marketing icons in your project." :tabs="pageTabs" />
+    .chi-grid__container.-pt--3
+      .chi-tabs-panel.-active#examples
+        <Examples />
+      .chi-tabs-panel#properties
+        <Properties />
+      .chi-tabs-panel#accessibility
+        <Accessibility />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+import PageContentTabs from '../../../components/PageContentTabs.vue';
+import Properties from './_properties.vue';
+import Accessibility from './_accessibility.vue';
+import { standardComponentPageTabs } from '../../../constants/constants';
+
+@Component({
+  components: {
+    Accessibility,
+    Examples,
+    PageContentTabs,
+    Properties
+  },
+  data: () => {
+    return {
+      pageTabs: standardComponentPageTabs
+    };
+  }
+})
+export default class MarketingIcon extends Vue {}
+</script>
