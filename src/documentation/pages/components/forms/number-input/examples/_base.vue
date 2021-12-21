800610abb208510229fc557c3142e2833e9b08f1
evansito <cugat.evan@gmail.com>
2021-12-21 12:39:22 +0100

    migrate number input to new vue doc

---
+++ src/documentation/pages/components/forms/number-input/examples/_base.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  <ComponentExample title="Base" id="base" :tabs="exampleTabs">
+    <Wrapper slot="example">
+      .chi-form__item
+        chi-label(for="example-1") Label
+        chi-number-input#example-1
+    </Wrapper>
+    <Wrapper slot="code-webcomponent">    
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+      </pre>
+    </Wrapper>
+    <Wrapper slot="code-htmlblueprint">
+      <JSNeeded /> 
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+      </pre>
+    </Wrapper>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-label for="example-1">Label</chi-label>
+<chi-number-input id="example-1"></chi-number-input>`,
+        htmlblueprint: `<div class="chi-form__item">
+  <label class="chi-label" for="input-example-base">Label</label>
+  <div class="chi-number-input">
+    <input id="input-example-base" type="number" class="chi-input" value="0" aria-label="Input Label">
+    <button aria-label="Decrease"></button>
+    <button aria-label="Increase"></button>
+  </div>
+</div>
+
+<script>chi.numberInput(document.getElementById('input-example-base'));<\/script>`
+      }
+    };
+  }
+})
+export default class Base extends Vue {}
+</script>
---
+++ src/documentation/pages/components/forms/number-input/examples/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    h2 Examples
+
+    <Base />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+
+@Component({
+  components: {
+    Base,
+  }
+})
+export default class Examples extends Vue {}
+</script>
