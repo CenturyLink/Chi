11afcf716510d8308d95ff6564f27ec3072200e5
evansito <cugat.evan@gmail.com>
2021-12-21 13:55:08 +0100

    migrate number input to new vue doc

--- src/documentation/pages/components/forms/number-input/examples/_disabled.vue
+++ src/documentation/pages/components/forms/number-input/examples/_disabled.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
-  <ComponentExample title="Base" id="base" :tabs="exampleTabs">
+  <ComponentExample title="Disabled" id="disabled" :tabs="exampleTabs">
     p.-text(slot="example-description")
-      | An initial value is optional. If an initial value is not provided, <code>0</code> will be taken as default.
+      | Use the <code>disabled</code> boolean attribute to prevent users from interacting with an input.
+      | Disabled inputs are not submitted with the form and can not receive any browsing events such as mouse clicks or focus.
+      | <strong>Note:</strong> The required attribute can not be used on inputs with a disabled attribute specified.
     <Wrapper slot="example">
       .chi-form__item
-        chi-label(for="example-1") Label
-        chi-number-input#example-1
+        chi-label(for="example-2") Label
+        chi-number-input(disabled=true)#example-2
     </Wrapper>
     <Wrapper slot="code-webcomponent">    
       <pre class="language-html">
@@ -40,21 +42,21 @@ import { Component, Vue } from 'vue-property-decorator';
         }
       ],
       codeSnippets: {
-        webcomponent: `<chi-label for="example-1">Label</chi-label>
-<chi-number-input id="example-1"></chi-number-input>`,
+        webcomponent: `<chi-label for="example-2">Label</chi-label>
+<chi-number-input disabled id="example-2"></chi-number-input>`,
         htmlblueprint: `<div class="chi-form__item">
-  <label class="chi-label" for="input-example-base">Label</label>
+  <label class="chi-label" for="input-example-disabled">Label</label>
   <div class="chi-number-input">
-    <input id="input-example-base" type="number" class="chi-input" value="0" aria-label="Input Label">
+    <input id="input-example-disabled" type="number" class="chi-input" value="0" aria-label="Input Label" disabled>
     <button aria-label="Decrease"></button>
     <button aria-label="Increase"></button>
   </div>
 </div>
 
-<script>chi.numberInput(document.getElementById('input-example-base'));<\/script>`
+<script>chi.numberInput(document.getElementById('input-example-disabled'));<\/script>`
       }
     };
   }
 })
-export default class Base extends Vue {}
+export default class Disabled extends Vue {}
 </script>
--- src/documentation/pages/components/forms/number-input/examples/index.vue
+++ src/documentation/pages/components/forms/number-input/examples/index.vue
@@ -3,15 +3,21 @@
     h2 Examples
 
     <Base />
+    <Disabled />
+    <MinMaxStep />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Base from './_base.vue';
+import Disabled from './_disabled.vue';
+import MinMaxStep from './_min-max-step.vue';
 
 @Component({
   components: {
     Base,
+    Disabled,
+    MinMaxStep
   }
 })
 export default class Examples extends Vue {}
