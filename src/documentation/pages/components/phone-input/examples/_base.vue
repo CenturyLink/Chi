30381a95f892d98bbdbefa7802ec2835ed18cc5a
evansito <cugat.evan@gmail.com>
2021-12-28 11:18:11 +0100

    migrate phone input to new vue doc

---
+++ src/documentation/pages/components/phone-input/examples/_base.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  <ComponentExample title="Base" id="base" :tabs="exampleTabs">
+    <Wrapper slot="example">
+      div(style="max-width: 18rem;")
+        chi-label(for="phone-input-base") Phone Number
+        chi-phone-input(id="phone-input-base")
+    </Wrapper>
+    <Wrapper slot="code-webcomponent">
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+      </pre>
+    </Wrapper>
+    <Wrapper slot="code-htmlblueprint">
+      <JSNeeded />
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+      </pre>
+    </Wrapper>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          disabled: true,
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-label for="phone-input-base">Phone Number</chi-label>
+<chi-phone-input id="phone-input-base"></chi-phone-input>`,
+        htmlblueprint: ``
+      }
+    };
+  }
+})
+export default class Base extends Vue {}
+</script>
---
+++ src/documentation/pages/components/phone-input/examples/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  <Wrapper>
+    h2 Examples
+
+    <Base />
+  </Wrapper>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+
+@Component({
+  components: {
+    Base
+  }
+})
+export default class Examples extends Vue {}
+</script>
