c3435e55029fbe8b69f3656c30eb4bd135c5bae4
evanakin <76445286+evansito@users.noreply.github.com>
2022-01-10 14:45:57 +0100

    [FSDE-12388] Remove unnecessary wrappers from vue doc (#277)
    Co-authored-by: Jani Bolkvadze <jani.bolkvadze@gmail.com>

--- src/documentation/pages/components/header/examples/_centurylink/_header_centurylink.vue
+++ src/documentation/pages/components/header/examples/_centurylink/_header_centurylink.vue
@@ -18,16 +18,12 @@
             span.chi-header__title.-d--none.-d-sm--flex Application Title
           .chi-header__start
           .chi-header__end
-    <Wrapper slot="code-webcomponent">
-      <pre class="language-html">
-        <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
-      </pre>
-    </Wrapper>
-    <Wrapper slot="code-htmlblueprint">
-      <pre class="language-html">
-        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
-      </pre>
-    </Wrapper>          
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
   </ComponentExample>
 </template>
 
