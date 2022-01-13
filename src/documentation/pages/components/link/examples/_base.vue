374b44b21b5c0ab501a5130a2a3350dc21d38e3c
José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>
2022-01-13 11:21:52 +0100

    [FSDE-11701] Migrate Link component to new Vue documentation (#268)
    * [FSDE-11701] Migrate Link component to new Vue documentation
    
    * [FSDE-11701] Fix identation on base example
    
    Co-authored-by: evanakin <76445286+evansito@users.noreply.github.com>
    
    * [FSDE-11701] Added additional class -text in the examples and removed <Wrapper> from the index
    
    * [FSDE-11701] Fix id in No Underline on Hover example
    
    * [FSDE-11701] Fix HTML code to PUG style in Link and Progress component
    
    Co-authored-by: evanakin <76445286+evansito@users.noreply.github.com>
    Co-authored-by: Jani Bolkvadze <jani.bolkvadze@gmail.com>

---
+++ src/documentation/pages/components/link/examples/_base.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  <ComponentExample title="Base" id="base" :tabs="exampleTabs" additionalClasses="-text">
+    chi-link(href="#" slot="example") Link
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-link href="#">Link</chi-link>`,
+        htmlblueprint: `<a class="chi-link" href="#">Link</a>`
+      }
+    };
+  }
+})
+export default class Base extends Vue {}
+</script>
---
+++ src/documentation/pages/components/link/examples/_download.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  <ComponentExample title="Download" id="download" :tabs="exampleTabs" additionalClasses="-text">
+    p.-text(slot="example-description")
+      | Use download to prompt users to save a page or document instead of navigating to it. 
+      | The download attribute accepts an optional value used to name the file. 
+      | If no value is provided, the original filename is used.
+    chi-link(href="#" slot="example" download) Link
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      string: 'String',
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<!-- Use default filename -->
+<chi-link href="#" download>Link</chi-link>
+
+<!-- Specify custom filename -->
+<chi-link href="#" download="custom-filename">Link</chi-link>`,
+        htmlblueprint: `<!-- Use default filename -->
+<a class="chi-link" href="#" download>Link</a>
+
+<!-- Specify custom filename -->
+<a class="chi-link" href="#" download="custom-filename">Link</a>`
+      }
+    };
+  }
+})
+export default class Download extends Vue {}
+</script>
