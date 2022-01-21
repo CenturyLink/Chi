b7e731413a9221252bf3b081c9e6bb8ec6114819
evansito <cugat.evan@gmail.com>
2022-01-21 13:08:33 +0100

    migrate text-input to vue doc

---
+++ src/documentation/pages/components/forms/text-input/examples/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div
+    h2 Examples
+
+    <Base />
+    <Disabled />
+    <Readonly />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+import Disabled from './_disabled.vue';
+import Readonly from './_readonly.vue';
+
+@Component({
+  components: {
+    Base,
+    Disabled,
+    Readonly
+  }
+})
+export default class Examples extends Vue {}
+</script>
