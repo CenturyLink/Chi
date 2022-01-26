7076e900e00c21f5eb7a4762863b5b86317af1c5
soujanreddy <92325462+soujanreddy@users.noreply.github.com>
2022-01-26 06:01:55 -0700

    [FSDE-12538] Migrate price component to vue documentation (#280)
    * Implementation for price component migration
    
    * [FSDE-12538] Wrapper updates
    
    * [FSDE-12538] Review updates
    
    * [FSDE-12538] Review updates
    
    * Update src/documentation/pages/components/price/examples/_base.vue
    
    Co-authored-by: Jani Bolkvadze <jani.bolkvadze@gmail.com>
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>
    Co-authored-by: Jose Lopez Ruiz <jose@elasticbox.com>

---
+++ src/documentation/pages/components/price/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  div
+    <TitleBar title="Price" description="Use price to display consistent price text in your project." :tabs="pageTabs" />
+    .chi-grid__container.-pt--3
+      .chi-tabs-panel.-active#examples
+        <Examples />
+      .chi-tabs-panel#properties
+        <Properties />
+      .chi-tabs-panel#accessibility
+        <Accessibility />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+import PageContentTabs from '../../../components/PageContentTabs.vue';
+import Properties from './_properties.vue';
+import Accessibility from './_accessibility.vue';
+import { standardComponentPageTabs } from '../../../constants/constants';
+
+@Component({
+  components: {
+    Examples,
+    Accessibility,
+    PageContentTabs,
+    Properties
+  },
+  data: () => {
+    return {
+      pageTabs: standardComponentPageTabs
+    };
+  }
+})
+export default class Price extends Vue {}
+</script>
