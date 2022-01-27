5fd84e09578ea3c44d0f5ac4bbaaa8509437fba7
josecarlosgonzalezv <josecarlos.gonzalezvillegas@cgi.com>
2022-01-27 13:22:07 +0100

    [FSDE-12571] Removed strings variable, added ref instead of ids and using new components for properties and accessibility

--- src/documentation/pages/components/forms/radio-button/examples/_checked.vue
+++ src/documentation/pages/components/forms/radio-button/examples/_checked.vue
@@ -27,7 +27,6 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   data: () => {
     return {
-      string: 'String',
       exampleTabs: [
         {
           disabled: true,
