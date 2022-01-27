91ae884fed78b633acda2c9a8e059ef3906a3de5
soujanreddy <soujan.reddy@lumen.com>
2022-01-27 11:02:21 -0500

    [FSDE-11702] Review updates

--- src/documentation/pages/components/modal/examples/_subtitle.vue
+++ src/documentation/pages/components/modal/examples/_subtitle.vue
@@ -81,5 +81,5 @@ import { Component, Vue } from 'vue-property-decorator';
     };
   },
 })
-export default class SubTitle extends Vue {}
+export default class ModalSubtitle extends Vue {}
 </script>
