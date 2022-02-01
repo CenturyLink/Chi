cc6cbbbc75ac4eedea0049f8ee58866e5c8be589
soujanreddy <soujan.reddy@lumen.com>
2022-02-01 11:28:24 -0500

    Resolve conflicts and merge

---
+++ src/documentation/pages/components/forms/radio-button/_properties.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  <PropertiesComingSoon />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Properties extends Vue {}
+</script>
