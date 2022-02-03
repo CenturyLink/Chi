1f07c3bda074696e9a0a1c19892d84ea739c153a
soujanreddy <soujan.reddy@lumen.com>
2022-02-03 07:01:13 -0500

    Init implementation code

---
+++ src/documentation/components/ComponentTest.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="example-wrapper">
+    <h4 v-if="titleSize === 'h4'" class="-anchor" :id="id">
+      {{ title }}
+      <span>
+        <a
+          class="-ml--1"
+          :href="'?theme=' + $store.state.themes.theme + '#' + id"
+          >#</a
+        >
+      </span>
+    </h4>
+    <h3 v-else class="-anchor" :id="id">
+      {{ title }}
+      <span>
+        <a
+          class="-ml--1"
+          :href="'?theme=' + $store.state.themes.theme + '#' + id"
+          >#</a
+        >
+      </span>
+    </h3>
+    <!-- <slot name="example-description"></slot> -->
+    <div>
+      <ul
+        :id="'chi-tabs--menu' + id"
+        class="chi-tabs"
+        role="tablist"
+        aria-label="chi-tabs-horizontal-examples"
+      >
+        <li
+          :class="[
+            tab.active ? '-active' : '',
+            tab.disabled ? '-disabled' : '',
+          ]"
+          v-for="tab in menuTabs"
+          :key="tab.id"
+          @click="setMenuId(tab.id)"
+        >
+          <a
+            role="tab"
+            :aria-controls="'#chi-tabs-panel--horizontal-' + tab.id"
+            :aria-selected="tab.active ? true : false"
+            :href="'#chi-tabs-panel--horizontal-' + tab.id"
+            :tabindex="tab.disabled ? -1 : null"
+          >
+            {{ tab.label }}</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="example -mb--3">
+      <div :class="[padding || '-p--3', additionalClasses]">
+        <slot name="example"></slot>
+        <slot :name="'menu-' + menuId"></slot>
+      </div>
+      <div class="example-tabs -pl--2">
+        <ul
+          class="chi-tabs -animated"
+          :id="'code-snippet-tabs' + id"
+          role="tabs"
+        >
+          <li
+            :class="[
+              tab.active ? '-active' : '',
+              tab.disabled ? '-disabled' : '',
+            ]"
+            v-for="tab in tabs"
+            :key="tab.id"
+          >
+            <a
+              role="tab"
+              :aria-controls="'#example-' + '-' + id + '-' + tab.id"
+              :aria-selected="tab.active ? true : false"
+              :href="'#example-' + '-' + id + '-' + tab.id"
+              :tabindex="tab.disabled ? -1 : null"
+            >
+              {{ tab.label }}
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div
+        :class="['chi-tabs-panel', tab.active ? '-active' : '']"
+        v-for="tab in tabs"
+        :key="tab.id"
+        :id="'example-' + '-' + id + '-' + tab.id"
+        role="tabpanel"
+      >
+        <slot :name="'code-' + tab.id"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { TabsInterface } from '../models/models';
+
+declare const chi: any;
+
+@Component({})
+export default class ComponentTest extends Vue {
+  @Prop() id?: string;
+  @Prop() title?: string;
+  @Prop() titleSize?: 'h3' | 'h4';
+  @Prop() tabs?: TabsInterface[];
+  @Prop() menuTabs?: TabsInterface[];
+  @Prop() padding?: string;
+  @Prop() additionalClasses?: string;
+  @Prop() menuId?: string;
+  chiTabs: any;
+  chiMainTabs: any;
+  mounted() {
+    const chiTabs = document.getElementById(
+      'code-snippet-tabs' + this.$props.id
+    );
+
+    if (chiTabs) {
+      this.chiTabs = chi.tab(chiTabs);
+    }
+
+    const chiMainTabs = document.getElementById(
+      'chi-tabs--menu' + this.$props.id
+    );
+
+    if (chiMainTabs) {
+      this.chiMainTabs = chi.tab(chiMainTabs);
+    }
+  }
+
+  setMenuId(tabId: string) {
+      this.$props.menuId = tabId;
+  }
+
+  
+}
+</script>
--- src/documentation/pages/components/tabs/examples/_horizontal.vue
+++ src/documentation/pages/components/tabs/examples/_horizontal.vue
@@ -1,23 +1,24 @@
 <template lang="pug">
-  <ComponentExample title="Horizontal" id="horizontal-lumen-centurylink" padding="-p--0" :tabs="exampleTabs">
-    div(slot="example-description")
-      ul#chi-tabs--menu.chi-tabs(
-        role="tablist"
-        aria-label="chi-tabs-horizontal-examples")
-        li.-active
-          a(
-            href='#chi-tabs-panel--horizontal-base'
-            role="tab"
-            aria-selected="true"
-            aria-controls="chi-tabs-panel--horizontal-base") Base
-        li
-          a(
-            href='#chi-tabs-panel--horizontal-inverse'
-            role="tab"
-            aria-selected="false"
-            tabindex="-1"
-            aria-controls="chi-tabs-panel--horizontal-inverse") Inverse
-    .-p--3.-bg--grey-20(slot="example")
+  <ComponentTest title="Horizontal" id="horizontal-lumen-centurylink" padding="-p--0" :tabs="exampleTabs" :menuTabs="menuTabs" :menuId="menuTabs[0].id">
+    //- div(slot="example-description")
+    //-   ul#chi-tabs--menu.chi-tabs(
+    //-     role="tablist"
+    //-     aria-label="chi-tabs-horizontal-examples")
+    //-     li.-active
+    //-       a(
+    //-         href='#chi-tabs-panel--horizontal-base'
+    //-         role="tab"
+    //-         aria-selected="true"
+    //-         aria-controls="chi-tabs-panel--horizontal-base") Base
+    //-     li
+    //-       a(
+    //-         href='#chi-tabs-panel--horizontal-inverse'
+    //-         role="tab"
+    //-         aria-selected="false"
+    //-         tabindex="-1"
+    //-         aria-controls="chi-tabs-panel--horizontal-inverse") Inverse
+
+    .-p--3.-bg--grey-20(slot="menu-base")
       .-bg--white.-px--3
         ul.chi-tabs#example-horizontal-base(
           role="tablist"
@@ -52,6 +53,37 @@
           .chi-tabs-panel#horizontal-base-3(role="tabpanel")
             .-text Tab 3 content
 
+    .-p--3.-bg--grey-20(slot="menu-inverse")
+      .-bg--black.-px--3
+        ul.chi-tabs.-inverse#example-horizontal-inverse(role="tablist" aria-label="chi-tabs-horizontal-inverse")
+          li.-active
+            a(
+              href='#horizontal-inverse-1'
+              role="tab"
+              aria-selected="true"
+              aria-controls="horizontal-inverse-1") Active Tab
+          li
+            a(
+              href='#horizontal-inverse-2'
+              role="tab"
+              aria-selected="false"
+              tabindex="-1"
+              aria-controls="horizontal-inverse-2") Tab Link
+          li
+            a(
+              href='#horizontal-inverse-3'
+              role="tab"
+              aria-selected="false"
+              tabindex="-1"
+              aria-controls="horizontal-inverse-3") Tab Link
+      .-bg--white.-p--3
+        .chi-tabs-panel.-active#horizontal-inverse-1(role="tabpanel")
+          .-text Tab 1 content
+        .chi-tabs-panel#horizontal-inverse-2(role="tabpanel")
+          .-text Tab 2 content
+        .chi-tabs-panel#horizontal-inverse-3(role="tabpanel")
+          .-text Tab 3 content
+
     <pre class="language-html" slot="code-webcomponent">
       <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
     </pre>
@@ -61,7 +93,7 @@
         <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
       </pre>
     </Wrapper>
-  </ComponentExample>
+  </ComponentTest>
 </template>
 
 <script lang="ts">
@@ -82,6 +114,17 @@ declare const chi: any;
           active: true,
           id: 'htmlblueprint',
           label: 'HTML blueprint',
+        },
+      ],
+       menuTabs: [
+        {
+          active: true,
+          id: 'base',
+          label: 'Base',
+        },
+        {
+          id: 'inverse',
+          label: 'Inverse',
         },
       ],
       codeSnippets: {
