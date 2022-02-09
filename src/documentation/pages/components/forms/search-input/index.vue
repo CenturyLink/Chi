2fd5ada02222f32b76542f07bcfae2adf4f19ecb
soujanreddy <soujan.reddy@lumen.com>
2022-02-09 10:36:43 -0500

    [FSDE-12681] Add ignored elements

--- src/documentation/pages/components/forms/search-input/index.vue
+++ src/documentation/pages/components/forms/search-input/index.vue
@@ -18,6 +18,8 @@ import Properties from './_properties.vue';
 import Accessibility from './_accessibility.vue';
 import { standardComponentPageTabs } from '../../../../constants/constants';
 
+Vue.config.ignoredElements = ['chi-search-input', 'chi-brand'];
+
 @Component({
   components: {
     Examples,
