16b6a1b6e3149d18b6b6de42c480d5028822d579
evansito <cugat.evan@gmail.com>
2022-02-10 16:59:28 +0100

    migrate time picker to new vue doc

--- src/documentation/components/Navigation.vue
+++ src/documentation/components/Navigation.vue
@@ -58,6 +58,7 @@
           <li><NuxtLink to="/components/forms/text-input">Text input</NuxtLink></li>
           <li><NuxtLink to="/components/forms/textarea">Textarea</NuxtLink></li>
           <li><NuxtLink to="/components/tooltip">Tooltip</NuxtLink></li>
+          <li><NuxtLink to="/components/time-picker">Time picker</NuxtLink></li>
           <!-- <li><NuxtLink to="/components/activity">Activity</NuxtLink></li>
           <li><NuxtLink to="/components/avatar">Avatar</NuxtLink></li>
           <li><NuxtLink to="/components/breadcrumb">Breadcrumb</NuxtLink></li>
---
+++ src/documentation/pages/components/time-picker/examples/_time-base.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  <ComponentExample titleSize="h4" title="Base" id="time-base" :tabs="exampleTabs">
+    chi-time(slot="example")
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true, 
+          id: 'webcomponent',
+          label: 'Web component'
+        },
+        {
+          disabled: true,
+          id: 'htmlblueprint',
+          label: 'HTML blueprint'
+        }
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-time></chi-time>`,
+        htmlblueprint: ``
+      }
+    };
+  }
+})
+export default class TimeBase extends Vue {}
+</script>
