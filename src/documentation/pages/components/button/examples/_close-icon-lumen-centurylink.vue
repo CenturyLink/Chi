9165672b99803c1f2c8a824729f393c37557e9a9
soujanreddy <soujan.reddy@lumen.com>
2022-02-15 21:12:16 -0500

    Squash and merge commits

---
+++ src/documentation/pages/components/button/examples/_close-icon-lumen-centurylink.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  <ComponentExample titleSize="h4" title="Close icon button" id="close-icon-lumen-centurylink" :tabs="exampleTabs">
+    chi-button(type='close' slot="example")
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component',
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint',
+        },
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-button type="close"></chi-button>`,
+        htmlblueprint: `<button class="chi-button -icon -close" aria-label="Close">
+  <div class="chi-button__content">
+    <i class="chi-icon icon-x" aria-hidden="true"></i>
+  </div>
+</button>`,
+      },
+    };
+  },
+})
+export default class CloseIconLumenCenturyLink extends Vue {}
+</script>
---
+++ src/documentation/pages/components/button/examples/_icon-buttons-lumen-centurylink.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+  <ComponentExample title="Icon buttons" id="icon-button-lumen-centurylink" :tabs="exampleTabs">
+    p.-text(slot="example-description")
+      | For interfaces with limited space, an Icon button can
+      | be used for common actions such as editing, deleting, closing, etc.
+    <Wrapper slot="example">
+      chi-button(type='icon' alternative-text='Edit' ref='edit' data-tooltip='Edit')
+        chi-icon(icon='edit')
+      chi-button.-ml--2(variant='flat' type='icon' alternative-text='Edit' ref='editFlat' data-tooltip='Edit')
+        chi-icon(icon='edit')
+    </Wrapper>
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <Wrapper slot="code-htmlblueprint">
+      <JSNeeded />
+      <pre class="language-html">
+        <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+      </pre>
+    </Wrapper>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+declare const chi: any;
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component',
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint',
+        },
+      ],
+      codeSnippets: {
+        webcomponent: `<!-- base -->
+<chi-button type="icon" alternative-text="Edit" data-tooltip="Edit">
+  <chi-icon icon="edit"></chi-icon>
+</chi-button>
+
+<!-- flat -->
+<chi-button variant="flat" type="icon" alternative-text="Edit" data-tooltip="Edit">
+  <chi-icon icon="edit"></chi-icon>
+</chi-button>
+
+<!-- Tooltip -->
+<script>chi.tooltip(document.querySelectorAll('[data-tooltip]'));<\/script>`,
+        htmlblueprint: `<!-- base -->
+<button class="chi-button -icon" aria-label="Edit" data-tooltip="Edit">
+  <div class="chi-button__content">
+    <i class="chi-icon icon-edit" aria-hidden="true"></i>
+  </div>
+</button>
+
+<!-- flat -->
+<button class="chi-button -icon -flat" aria-label="Edit" data-tooltip="Edit">
+  <div class="chi-button__content">
+    <i class="chi-icon icon-edit" aria-hidden="true"></i>
+  </div>
+</button>
+
+<!-- Tooltip -->
+<script>chi.tooltip(document.querySelectorAll('[data-tooltip]'));<\/script>`,
+      },
+    };
+  },
+})
+export default class IconButtonLumenCenturyLink extends Vue {
+  tooltip: any;
+  tooltipFlat: any;
+
+  mounted() {
+    this.tooltip = chi.tooltip(this.$refs.edit as HTMLElement);
+    this.tooltipFlat = chi.tooltip(this.$refs.editFlat as HTMLElement);
+  }
+
+  beforeDestroy() {
+    this.tooltip.dispose();
+    this.tooltipFlat.dispose();
+  }
+}
+</script>
