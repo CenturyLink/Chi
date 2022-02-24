da1d77e45f72f9cf70fd81d54ccabeb7a833cfbe
soujanreddy <92325462+soujanreddy@users.noreply.github.com>
2022-02-24 04:00:07 -0700

    [FSDE-15311] Migrate Toggle switch component to new vue documentation (#327)
    * Squash commits and push
    
    * Update src/documentation/pages/components/forms/toggle-switch/examples/_portal/_sizes.vue
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>
    Co-authored-by: Jose Lopez Ruiz <jose@elasticbox.com>
    Co-authored-by: Jani Bolkvadze <jani.bolkvadze@gmail.com>

---
+++ src/documentation/pages/components/forms/toggle-switch/examples/_base-lumen-centurylink.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  <ComponentExample titleSize="h4" title="Base" id="base-lumen-centurylink" :tabs="exampleTabs">
+    .chi-form__item(slot="example")
+      chi-switch(id='toggle-ba1',label='Label')
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'webcomponent',
+          label: 'Web component',
+        },
+        {
+          id: 'htmlblueprint',
+          label: 'HTML blueprint',
+        },
+      ],
+      codeSnippets: {
+        webcomponent: `<chi-switch id="toggle-ba1" label="Label"></chi-switch>`,
+        htmlblueprint: `<div class="chi-form__item">
+  <label class="chi-switch" for="toggle-ba1">
+    <input type="checkbox" class="chi-switch__input" id="toggle-ba1">
+    <span class="chi-switch__content">
+      <span class="chi-switch__thumb"></span>
+    </span>
+    <span class="chi-switch__label">Label</span>
+  </label>
+</div>`,
+      },
+    };
+  },
+})
+export default class BaseLumenCenturyLink extends Vue {}
+</script>
