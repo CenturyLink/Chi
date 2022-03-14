771b9485aa5236b2dac4f962d9bbe8a5bd2e4013
evanakin <76445286+evansito@users.noreply.github.com>
2022-03-14 17:56:52 +0100

    [FSDE-16525] Migrate carousel to vue documentation (#346)
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>

---
+++ src/documentation/pages/components/carousel/examples/index.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  div
+    h2 Examples
+    <Base />
+    <Single />
+    <DotControllers />
+    <Pagination />
+    <CustomControllers />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+import Single from './_single.vue';
+import DotControllers from './_dot-controllers.vue';
+import Pagination from './_pagination.vue';
+import CustomControllers from './_custom-controllers.vue';
+
+@Component({
+  components: {
+    Base,
+    Single,
+    DotControllers,
+    Pagination,
+    CustomControllers
+  }
+})
+export default class Examples extends Vue {}
+</script>
