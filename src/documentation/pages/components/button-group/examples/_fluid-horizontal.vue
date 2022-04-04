24d06adf9b24a5b4b8397fc5f36c9d05c7ceaf89
soujanreddy <92325462+soujanreddy@users.noreply.github.com>
2022-04-04 04:06:21 -0600

    [FSDE-16082] Migrate Button group component to vue documentation (#339)
    * Squash commits and push
    
    * [FSDE-16082] Fix sizing issue

---
+++ src/documentation/pages/components/button-group/examples/_fluid-horizontal.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  <ComponentExample titleSize="h4" title="Horizontal" id="fluid-horizontal" :tabs="exampleTabs">
+    .chi-button-group.-fluid(slot="example")
+      button(class=`chi-button` v-for="item in Array(3).fill(0)") Button
+    <pre class="language-html" slot="code-webcomponent">
+      <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
+    </pre>
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="$data.codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          disabled: true,
+          id: 'webcomponent',
+          label: 'Web component',
+        },
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML blueprint',
+        },
+      ],
+      codeSnippets: {
+        webcomponent: ``,
+        htmlblueprint: `<div class="chi-button-group -fluid">
+  <button class="chi-button">Button</button>
+  <button class="chi-button">Button</button>
+  <button class="chi-button">Button</button>
+</div>`,
+      },
+    };
+  },
+})
+export default class FluidHorizontal extends Vue {}
+</script>
