520cc40175d458485a92e4d01e1f5a6978f38fc9
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2022-06-02 18:36:39 +0400

    [FSDE-20652] Created Chi Vue Expansion Panel component (#393)

--- src/chi-vue/src/constants/types.ts
+++ src/chi-vue/src/constants/types.ts
@@ -182,6 +182,11 @@ export type DrawerPositions = typeof GENERAL_POSITIONS[number];
 export type Backdrop = 'inverse' | '';
 //#endregion
 
+//#region Expansion Panel
+export const EXPANSION_PANEL_STATES = ['done', 'active', 'pending', 'disabled'] as const;
+export type ExpansionPanelState = typeof EXPANSION_PANEL_STATES[number];
+//#endregion
+
 //#region Pagination
 export const PAGINATION_SIZES = ['sm', 'md', 'lg', 'xl'] as const;
 export type PaginationSizes = typeof PAGINATION_SIZES[number];
