858c1f5cd30db57eaa754bea332fbc7cd6cfdd38
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2022-06-28 16:51:27 +0400

    [FSDE-22886] Fixed Copy to clipboard on PR instances

--- src/documentation/components/ComponentExample.vue
+++ src/documentation/components/ComponentExample.vue
@@ -166,6 +166,7 @@ pre code.hljs {
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { TabsInterface, HeadTabsInterface } from '../models/models';
 import { COMPONENT_EXAMPLE_EVENTS } from '../constants/constants';
+import { SR_ONLY } from '../../chi-vue/src/constants/classes';
 
 declare const chi: any;
 
@@ -179,7 +180,35 @@ Vue.config.warnHandler = (msg: string, _vm: Vue, trace: string) => {
   }
 };
 
-@Component({})
+@Component({
+  methods: {
+    copy(id: string) {
+      const tabElement = (this.$refs[id] as HTMLElement[])[0];
+
+      if (tabElement) {
+        const codeElement = (tabElement as HTMLElement).querySelector('code');
+        const codeSnippet = codeElement?.textContent;
+
+        if (codeSnippet || typeof codeSnippet === 'string') {
+          if (navigator.clipboard && window.isSecureContext) {
+            navigator.clipboard
+              .writeText(codeSnippet);
+          } else {
+            const textArea = document.createElement("textarea");
+
+            textArea.value = codeSnippet as string;
+            textArea.classList.add(SR_ONLY);
+            tabElement.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            document.execCommand('copy');
+            textArea.remove();
+          }
+        }
+      }
+    }
+  }
+})
 export default class ComponentExample extends Vue {
   @Prop() id?: string;
   @Prop() title?: string;
@@ -192,20 +221,6 @@ export default class ComponentExample extends Vue {
 
   chiTabs: any;
   chiHeadTabs: any;
-
-  copy(id: string) {
-    const tabElement = (this.$refs[id] as HTMLElement[])[0];
-
-    if (tabElement) {
-      const codeElement = (tabElement as HTMLElement).querySelector('code');
-      const codeSnippet = codeElement?.textContent;
-
-      if(navigator.clipboard && codeSnippet) {
-        navigator.clipboard
-          .writeText(codeSnippet);
-      }
-    }
-  }
 
   mounted() {
     const chiTabs = document.getElementById(
