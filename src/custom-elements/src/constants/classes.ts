ec546742ad4e94403962b4c772eab87eaa392b8a
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2022-08-25 14:34:11 +0400

    [FSDE-26322] Created Tabs web component

--- src/custom-elements/src/constants/classes.ts
+++ src/custom-elements/src/constants/classes.ts
@@ -21,7 +21,7 @@ export const GENERIC_SIZES = {
   SM: '-sm',
   MD: '-md',
   LG: '-lg',
-  XL: '-xl',
+  XL: '-xl'
 };
 export const LIST_CLASS = '-list';
 //#endregion
@@ -32,10 +32,11 @@ export const ACCORDION_CLASSES = {
   CONTENT: 'chi-accordion__content',
   ITEM: 'chi-accordion__item',
   TRIGGER: 'chi-accordion__trigger',
-  TITLE: 'chi-accordion__title',
+  TITLE: 'chi-accordion__title'
 };
-//#region
+//#endregion
 
+//#region Bulk Actions
 export const BULK_ACTIONS_CLASSES = {
   BULK_ACTIONS: 'chi-bulk-actions',
   BULK_ACTIONS_TOP: 'chi-bulk-actions__top',
@@ -45,13 +46,14 @@ export const BULK_ACTIONS_CLASSES = {
   BULK_ACTIONS_END: 'chi-bulk-actions__end',
   BULK_ACTIONS_BUTTONS: 'chi-bulk-actions__buttons',
   BULK_ACTIONS_BUTTONS_MOBILE: 'chi-bulk-actions__buttons-mobile',
-  BULK_ACTIONS_BUTTONS_DESKTOP: 'chi-bulk-actions__buttons-desktop',
+  BULK_ACTIONS_BUTTONS_DESKTOP: 'chi-bulk-actions__buttons-desktop'
 };
+//#endregion
 
 //#region Backdrop
 export const BACKDROP_CLASSES = {
   BACKDROP: 'chi-backdrop',
-  WRAPPER: 'chi-backdrop__wrapper',
+  WRAPPER: 'chi-backdrop__wrapper'
 };
 //#endregion
 
@@ -70,8 +72,8 @@ export const BUTTON_CLASSES = {
     SM: '-sm',
     MD: '-md',
     LG: '-lg',
-    XL: '-xl',
-  },
+    XL: '-xl'
+  }
 };
 //#endregion
 
@@ -92,7 +94,7 @@ export const CAROUSEL_CLASSES = {
   DOTS_ADDITION: '-dots',
   PAGINATION_ADDITION: '-pagination',
   DOT: 'chi-carousel__dot',
-  PAGINATION: 'chi-carousel__pagination',
+  PAGINATION: 'chi-carousel__pagination'
 };
 //#endregion
 
@@ -111,14 +113,14 @@ export const CHECKBOX_CLASSES = {
   checkbox: 'chi-checkbox',
   INPUT: 'chi-checkbox__input',
   LABEL: 'chi-checkbox__label',
-  INDETERMINATE: '-indeterminate',
+  INDETERMINATE: '-indeterminate'
 };
 //#endregion
 
 //#region Divider
 export const DIVIDER_CLASSES = {
   DIVIDER: 'chi-divider',
-  VERTICAL: '-vertical',
+  VERTICAL: '-vertical'
 };
 //#endregion
 
@@ -126,7 +128,7 @@ export const DIVIDER_CLASSES = {
 export const RADIO_CLASSES = {
   RADIO: 'chi-radio',
   INPUT: 'chi-radio__input',
-  LABEL: 'chi-radio__label',
+  LABEL: 'chi-radio__label'
 };
 //#endregion
 
@@ -157,13 +159,13 @@ export const DATA_TABLE_CLASSES = {
   SORTING: '-sorting',
   COMPACT: '-compact',
   TRUNCATED: '-truncated',
-  PRINT_FOOTER: 'chi-data-table__print-footer',
+  PRINT_FOOTER: 'chi-data-table__print-footer'
 };
 //#endregion
 
 //#region Table
 export const TABLE_CLASSES = {
-  TABLE: 'chi-table',
+  TABLE: 'chi-table'
 };
 //#endregion
 
@@ -173,14 +175,14 @@ export const DRAWER_CLASSES = {
   HEADER: 'chi-drawer__header',
   TITLE: 'chi-drawer__title',
   CONTENT: 'chi-drawer__content',
-  TRIGGER: 'chi-drawer__trigger',
+  TRIGGER: 'chi-drawer__trigger'
 };
 //#endregion
 
 //#region Form
 export const FORM_CLASSES = {
   FORM_ITEM: 'chi-form__item',
-  LABEL: 'chi-label',
+  LABEL: 'chi-label'
 };
 //#endregion
 
@@ -190,7 +192,7 @@ export const MODAL_CLASSES = {
   MODAL: 'chi-modal',
   HEADER: 'chi-modal__header',
   CONTENT: 'chi-modal__content',
-  TITLE: 'chi-modal__title',
+  TITLE: 'chi-modal__title'
 };
 //#endregion
 
@@ -200,7 +202,7 @@ export const GENERIC_SIZE_CLASSES = {
   SM: '-sm',
   MD: '-md',
   LG: '-lg',
-  XL: '-xl',
+  XL: '-xl'
 };
 //#endregion
 
@@ -213,8 +215,8 @@ export const INPUT_CLASSES = {
   INPUT: 'chi-input',
   WRAPPER: 'chi-input__wrapper',
   ICON: {
-    RIGHT: '-icon--right',
-  },
+    RIGHT: '-icon--right'
+  }
 };
 //#endregion
 
@@ -229,25 +231,25 @@ export const PAGINATION_CLASSES = {
   START: 'chi-pagination__start',
   CENTER: 'chi-pagination__center',
   END: 'chi-pagination__end',
-  JUMPER: 'chi-pagination__jumper',
+  JUMPER: 'chi-pagination__jumper'
 };
 //#endregion
 
 //#region Search Input
 export const SEARCH_INPUT_CLASSES = {
-  SEARCH_INPUT: 'chi-search__input',
+  SEARCH_INPUT: 'chi-search__input'
 };
 //#endregion
 
 //#region Search Input
 export const SELECT_CLASSES = {
-  SELECT: 'chi-select',
+  SELECT: 'chi-select'
 };
 //#endregion
 
 //#region Tooltip
 export const TOOLTIP_CLASSES = {
-  TOOLTIP_ELEMENT: 'chi-tooltip',
+  TOOLTIP_ELEMENT: 'chi-tooltip'
 };
 //#endregion
 
@@ -290,46 +292,61 @@ export const POPOVER_CLASSES = {
 };
 //#endregion
 
+//#region Tabs
+export const TABS_CLASSES = {
+  TABS: 'chi-tabs',
+  ANIMATE: '-animated',
+  BORDER: '-border',
+  PANEL: 'chi-tabs-panel',
+  SLIDING: '-sliding',
+  SLIDING_BORDER: 'chi-sliding-border',
+  SOLID: '-solid',
+  TRIGGER: 'chi-tabs__trigger',
+  VERTICAL: '-vertical',
+};
+//#endregion
+
 //#region Utility Classes
 export const UTILITY_CLASSES = {
   Z_INDEX: {
     Z_10: '-z--10',
     Z_30: '-z--30',
-    Z_40: '-z--40',
+    Z_40: '-z--40'
   },
   ALIGN_ITEMS: {
-    CENTER: '-align-items--center',
+    CENTER: '-align-items--center'
   },
   ALIGN_SELF: {
     CENTER: '-align-self--center',
-    LEFT: '-align-self--left',
+    LEFT: '-align-self--left'
   },
   DISPLAY: {
     FLEX: '-d--flex',
     SCREEN_ONLY: '-d-screen--only',
-    PRINT_ONLY: '-d-print--only',
+    PRINT_ONLY: '-d-print--only'
   },
   FLEX: {
     FLEX_GROW1: '-flex--grow1',
-    COLUMN: '-flex--column',
+    COLUMN: '-flex--column'
   },
   JUSTIFY: {
     CENTER: '-justify-content--center',
+    BETWEEN: '-justify-content--between',
     MD_START: '-justify-content-md--start',
     MD_CENTER: '-justify-content-md--center',
-    MD_END: '-justify-content-md--end',
+    MD_END: '-justify-content-md--end'
   },
   TYPOGRAPHY: {
     TEXT_TRUNCATE: '-text--truncate',
     TEXT_NORMAL: '-text--normal',
-    TEXT_BOLD: '-text--bold',
+    TEXT_BOLD: '-text--bold'
   },
   POSITION: {
     RELATIVE: '-position--relative',
-    ABSOLUTE: '-position--absolute',
+    ABSOLUTE: '-position--absolute'
   },
   SIZING: {
-    W100: '-w--100',
+    W100: '-w--100'
   },
   PADDING: {
     1: '-p--1',
@@ -341,13 +358,13 @@ export const UTILITY_CLASSES = {
       1: '-px--1',
       2: '-px--2',
       3: '-px--3',
-      4: '-px--4',
+      4: '-px--4'
     },
     Y: {
       1: '-py--1',
       2: '-py--2',
       3: '-py--3',
-      4: '-py--4',
+      4: '-py--4'
     },
     LEFT: {
       1: '-pl--1',
@@ -355,26 +372,26 @@ export const UTILITY_CLASSES = {
       3: '-pl--3',
       4: '-pl--4',
       5: '-pl--5',
-      6: '-pl--6',
+      6: '-pl--6'
     },
     RIGHT: {
       1: '-pr--1',
       2: '-pr--2',
       3: '-pr--3',
-      4: '-pr--4',
+      4: '-pr--4'
     },
     TOP: {
       1: '-pt--1',
       2: '-pt--2',
       3: '-pt--3',
-      4: '-pt--4',
+      4: '-pt--4'
     },
     BOTTOM: {
       1: '-pb--1',
       2: '-pb--2',
       3: '-pb--3',
-      4: '-pb--4',
-    },
+      4: '-pb--4'
+    }
   },
   MARGIN: {
     1: '-m--1',
@@ -385,38 +402,39 @@ export const UTILITY_CLASSES = {
       1: '-mx--1',
       2: '-mx--2',
       3: '-mx--3',
-      4: '-mx--4',
+      4: '-mx--4'
     },
     Y: {
       1: '-my--1',
       2: '-my--2',
       3: '-my--3',
-      4: '-my--4',
+      4: '-my--4'
     },
     LEFT: {
       1: '-ml--1',
       2: '-ml--2',
       3: '-ml--3',
-      4: '-ml--4',
+      4: '-ml--4'
     },
     RIGHT: {
+      0: '-mr--0',
       1: '-mr--1',
       2: '-mr--2',
       3: '-mr--3',
-      4: '-mr--4',
+      4: '-mr--4'
     },
     TOP: {
       1: '-mt--1',
       2: '-mt--2',
       3: '-mt--3',
-      4: '-mt--4',
+      4: '-mt--4'
     },
     BOTTOM: {
       1: '-mb--1',
       2: '-mb--2',
       3: '-mb--3',
-      4: '-mb--4',
-    },
-  },
+      4: '-mb--4'
+    }
+  }
 };
 //#endregion
