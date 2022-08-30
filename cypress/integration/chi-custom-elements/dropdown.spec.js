123dbd727cd974f5c8d5e052889b6671287c9483
soujanreddy <soujan.reddy@lumen.com>
2022-08-30 11:13:39 -0400

    FSDE-24710-Review updates

--- cypress/integration/chi-custom-elements/dropdown.spec.js
+++ cypress/integration/chi-custom-elements/dropdown.spec.js
@@ -25,7 +25,7 @@ const positions = [
   },
   {
     placement:'right-start',
-    transform: 'translate3d(122px, 0px, 0px)',
+    transform: 'translate3d(123px, 0px, 0px)',
   },
   {
     placement:'right-end',
@@ -59,43 +59,22 @@ const DROPDOWN_DATA_CY = {
       transform: position.transform
     };
   }),
-  STATE: {
-    EXPANSION_PANEL_DONE: '[data-cy="state-expansion-panel-done"]',
-    EXPANSION_PANEL_ACTIVE: '[data-cy="state-expansion-panel-active"]',
-    EXPANSION_PANEL_PENDING: '[data-cy="state-expansion-panel-pending"]',
-  },
-  NUMBER: {
-    NUMBERED_EXPANSION_PANEL: '[data-cy="number-expansion-panel-numbered"]',
-    NON_NUMBERED_EXPANSION_PANEL: '[data-cy="number-expansion-panel-non-numbered"]'
-  },
-  BORDER: {
-    BORDERED_EXPANSION_PANEL: '[data-cy="border-expansion-panel-bordered"]',
-    NON_BORDERED_EXPANSION_PANEL: '[data-cy="border-expansion-panel-non-bordered"]',
-  },
-  SLOT: {
-    ACTIVE_EXPANSION_PANEL: `[data-cy="slot-expansion-panel-active"]`,
-    DONE_EXPANSION_PANEL: `[data-cy="slot-expansion-panel-done"]`
-  },
   METHOD: {
     TOGGLE: {
-      EXPANSION_PANEL: '[data-cy="method-dropdown-toggle"]',
+      DROPDOWN_TOGGLE: '[data-cy="method-dropdown-toggle"]',
       TRIGGER: '#test-toggle-trigger'
     },
     SHOW: {
-      EXPANSION_PANEL: '[data-cy="method-dropdown-show"]',
+      DROPDOWN_SHOW: '[data-cy="method-dropdown-show"]',
       TRIGGER: '#test-show-trigger'
     },
     HIDE: {
-      EXPANSION_PANEL: '[data-cy="method-dropdown-hide"]',
+      DROPDOWN_HIDE: '[data-cy="method-dropdown-hide"]',
       TRIGGER: '#test-hide-trigger'
     }
   }
 };
-const EXPANSION_PANEL_BUTTONS = {
-  NEXT_BUTTON: `[data-chi-epanel-action='next']`,
-  PREVIOUS_BUTTON: `[data-chi-epanel-action='previous']`,
-  CHANGE_BUTTON: `[data-chi-epanel-action='change']`
-}
+
 const DROPDOWN_TRIGGER = '.chi-button.chi-dropdown__trigger';
 const DROPDOWN_MENU = '.chi-dropdown__menu';
 const ACTIVE_CLASS = '-active';
@@ -187,122 +166,13 @@ describe('Dropdown', () => {
     });
   });
 
-  describe('State', () => {
-    beforeEach(() => {
-      cy.get(EXPANSION_PANEL_DATA_CY.STATE.EXPANSION_PANEL_DONE).as(
-        'doneExpansionPanel'
-      );
-      cy.get(EXPANSION_PANEL_DATA_CY.STATE.EXPANSION_PANEL_ACTIVE).as(
-        'activeExpansionPanel'
-      );
-      cy.get(EXPANSION_PANEL_DATA_CY.STATE.EXPANSION_PANEL_PENDING).as(
-        'pendingExpansionPanel'
-      );
-    });
-
-    it('Should check if expansion panel has state as done', () => {
-      hasClassAssertion(`@doneExpansionPanel`, DONE_CLASS);
-    });
-
-    it('Should check if expansion panel has state as active', () => {
-      hasClassAssertion(`@activeExpansionPanel`, ACTIVE_CLASS);
-    });
-
-    it('Should check if expansion panel has state as pending', () => {
-      hasClassAssertion(`@pendingExpansionPanel`, PENDING_CLASS);
-    });
-  });
-
-  describe('Number', () => {
-    beforeEach(() => {
-      cy.get(EXPANSION_PANEL_DATA_CY.NUMBER.NUMBERED_EXPANSION_PANEL).as(
-        'numberedExpansionPanel'
-      );
-      cy.get(EXPANSION_PANEL_DATA_CY.NUMBER.NON_NUMBERED_EXPANSION_PANEL).as(
-        'noNumberedExpansionPanel'
-      );
-    });
-
-    it('Should check if expansion panel has step number', () => {
-      cy.get(`@numberedExpansionPanel`)
-        .find(`.${NUMBER_CLASS}`)
-        .should('exist');
-    });
-
-    it('Should check if expansion panel has no step number', () => {
-      cy.get(`@noNumberedExpansionPanel`)
-        .find(`.${NUMBER_CLASS}`)
-        .should('not.exist');
-    });
-  });
-
-  describe('Border', () => {
-    beforeEach(() => {
-      cy.get(EXPANSION_PANEL_DATA_CY.BORDER.BORDERED_EXPANSION_PANEL).as(
-        'borderedExpansionPanel'
-      );
-      cy.get(EXPANSION_PANEL_DATA_CY.BORDER.NON_BORDERED_EXPANSION_PANEL).as(
-        'nonBorderedExpansionPanel'
-      );
-    });
-
-    it('Should check if bordered expansion panel has border', () => {
-      hasClassAssertion(`@borderedExpansionPanel`, BORDER_CLASS);
-    });
-
-    it('Should check if non bordered expansion panel has no border', () => {
-      cy.get(`@nonBorderedExpansionPanel`)
-        .should('not.have.class', BORDER_CLASS);
-    });
-  });
-
-  describe('Slot', () => {
-    beforeEach(() => {
-      cy.get(EXPANSION_PANEL_DATA_CY.SLOT.ACTIVE_EXPANSION_PANEL).as(
-        'slotActiveExpansionPanel'
-      );
-      cy.get(EXPANSION_PANEL_DATA_CY.SLOT.DONE_EXPANSION_PANEL).as(
-        'slotDoneExpansionPanel'
-      );
-    });
-
-    const slotSelectors = [
-      { el: `@slotActiveExpansionPanel`, slot: 'active', class: SLOT_CLASS_ACTIVE },
-      { el: `@slotDoneExpansionPanel`, slot: 'done', class: SLOT_CLASS_DONE },
-      { el: `@slotActiveExpansionPanel`, slot: 'footer', class: SLOT_CLASS_FOOTER },
-    ];
-    const buttonsSelectors = [
-      { slot: 'change', button: 'Change' },
-      { slot: 'footer start', button: 'Previous' },
-      { slot: 'footer end', button: 'Continue' },
-    ];
-
-    slotSelectors.forEach(sel => {
-      it(`Should check if ${sel.slot} is available in the panel`, () => {
-        cy.get(sel.el)
-          .find(`.${sel.class}`)
-          .should('exist')
-          .should('be.visible');
-      });
-    });
-
-    buttonsSelectors.forEach(sel => {
-      it(`Should check if ${sel.slot} slot is available in the panel`, () => {
-        cy.get(`@slotActiveExpansionPanel`)
-          .find('button')
-          .contains(sel.button)
-          .should(sel.slot === 'change' ? 'not.be.visible' : 'be.visible');
-      });
-    });
-  });
-
   describe.only('Methods', () => {
     describe('Toggle', () => {
       beforeEach(() => {
         cy.get(DROPDOWN_DATA_CY.METHOD.TOGGLE.TRIGGER).as(
           'dropdownTrigger'
         );
-        cy.get(DROPDOWN_DATA_CY.METHOD.TOGGLE.EXPANSION_PANEL).find(DROPDOWN_MENU).as(
+        cy.get(DROPDOWN_DATA_CY.METHOD.TOGGLE.DROPDOWN_TOGGLE).find(DROPDOWN_MENU).as(
           'dropdownMenu'
         );
       });
@@ -323,7 +193,7 @@ describe('Dropdown', () => {
         cy.get(DROPDOWN_DATA_CY.METHOD.SHOW.TRIGGER).as(
           'dropdownTrigger'
         );
-        cy.get(DROPDOWN_DATA_CY.METHOD.SHOW.EXPANSION_PANEL).find(DROPDOWN_MENU).as(
+        cy.get(DROPDOWN_DATA_CY.METHOD.SHOW.DROPDOWN_SHOW).find(DROPDOWN_MENU).as(
           'dropdownMenu'
         );
       });
@@ -344,7 +214,7 @@ describe('Dropdown', () => {
         cy.get(DROPDOWN_DATA_CY.METHOD.HIDE.TRIGGER).as(
           'dropdownTrigger'
         );
-        cy.get(DROPDOWN_DATA_CY.METHOD.HIDE.EXPANSION_PANEL).find(DROPDOWN_MENU).as(
+        cy.get(DROPDOWN_DATA_CY.METHOD.HIDE.DROPDOWN_HIDE).find(DROPDOWN_MENU).as(
           'dropdownMenu'
         );
       });
