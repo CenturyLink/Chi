4325dad02e0344ba164ecfcd27b7ee9f83435b59
josecarlosgonzalezv <josecarlos.gonzalezvillegas@lumen.com>
2023-02-17 13:29:51 +0100

    [FSDE-30681] Improve order update of cells in data table

--- src/chi-vue/src/components/data-table/DataTable.tsx
+++ src/chi-vue/src/components/data-table/DataTable.tsx
@@ -35,6 +35,7 @@ import {
   DataTableModes,
   DataTableRowLevels,
   DataTableColumnDescription,
+  DataTableColumn,
 } from '@/constants/types';
 import { DATA_TABLE_NO_RESULTS_FOUND, DATA_TABLE_SORT_ICONS, SCREEN_BREAKPOINTS } from '@/constants/constants';
 import DataTableTooltip from './DataTableTooltip';
@@ -1337,7 +1338,15 @@ export default class DataTable extends Vue {
 
   beforeDestroy() {
     window.removeEventListener('resize', this.resizeHandler, true);
-    this._chiDropdownSelectAll.dispose();
+    this._chiDropdownSelectAll?.dispose();
+  }
+
+  _headIndex(column: string) {
+    if (Array.isArray(this.data.head) && isNaN(Number(column))) {
+      return this.data.head.findIndex((col: DataTableColumn) => col.name === column);
+    }
+
+    return column;
   }
 
   sortData(column: string, direction: string, sortBy: string | undefined) {
@@ -1345,12 +1354,13 @@ export default class DataTable extends Vue {
       this._showSelectedOnlyRowsData && this._showSelectedOnlyRowsData.length > 0
         ? [...this._showSelectedOnlyRowsData]
         : [...this._serializedDataBody];
-    const columnData = this.data.head[column];
+    const columnData = this.data.head[this._headIndex(column)];
     const ascending: boolean = direction === 'ascending';
 
     if (columnData) {
-      const columnIndex = !Array.isArray(this.data.head) ? Object.keys(this.data.head).indexOf(column) : column;
-
+      const columnIndex = !Array.isArray(this.data.head)
+        ? Object.keys(this.data.head).indexOf(column)
+        : this._headIndex(column);
       const locateData = (data: DataTableRow, sortBy: string | undefined) => {
         return sortBy && data.data[columnIndex].payload && data.data[columnIndex].payload[sortBy]
           ? data.data[columnIndex].payload[sortBy]
