c290b4382a2e9eccd469eb29fec4c605ac32963f
krzysztofdrozdowski <krzysztof.drozdowski@ready4s.com>
2023-02-28 11:35:01 +0100

    [FSDE-30730] Migrate 500 error template

---
+++ src/documentation/pages/components-wip/error-500/examples/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    <Base />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+
+@Component({
+  components: {
+    Base
+  }
+})
+export default class Examples extends Vue {}
+</script>
