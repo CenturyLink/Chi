e663eb1ee7a518212de4fd2ec01144a1058f9819
krzysztofdrozdowski <krzysztof.drozdowski@ready4s.com>
2023-03-01 17:15:17 +0100

    [FSDE-30730] Generate footer in base example

--- src/documentation/pages/components-wip/app-layout/examples/_dashboard.vue
+++ src/documentation/pages/components-wip/app-layout/examples/_dashboard.vue
@@ -84,59 +84,7 @@
               .chi-card__title Widget
               chi-link(size='md' href='#' cta='cta') View
             .chi-card__content Content here
-      footer.chi-footer(slot='footer')
-        .chi-footer__content
-          .chi-footer__internal
-            .chi-footer__internal-content
-              .chi-dropdown.chi-footer__language
-                a#dashboard-language-dropdown-button.chi-button.-icon.-flat.-light.-sm.chi-dropdown__trigger(data-position='top-start' aria-label='Select your preferred language')
-                  .chi-button__content
-                    i.chi-icon.icon-globe-network-outline
-                    span English
-                .chi-dropdown__menu.-w--sm.-text--body
-                  a.chi-dropdown__menu-item.-active(href='#') English
-                  a.chi-dropdown__menu-item(href='#') Espa&ntilde;ol
-                  a.chi-dropdown__menu-item(href='#') Portugu&ecirc;s
-                  a.chi-dropdown__menu-item(href='#') Fran&ccedil;ais
-                  a.chi-dropdown__menu-item(href='#') Deutsch
-                  a.chi-dropdown__menu-item(href='#') &#x7B80;&#x4F53;&#x4E2D;&#x6587;
-                  a.chi-dropdown__menu-item(href='#') &#x65E5;&#x672C;&#x8A9E;
-              .chi-footer__links
-                ul
-                  li
-                    a(href='https://www.lumen.com/en-us/about.html') About Us
-                  li
-                    a(href='https://www.centurylink.com/aboutus/community/community-development/programs-for-customers-with-disabilities.html') Accessibility
-                  li
-                    a(href='https://jobs.lumen.com' target='_blank') Careers
-                  li
-                    a(href='https://www.lumen.com/en-us/contact-us.html') Contact Us
-                  li
-                    a.optanon-toggle-display(href='#') Cookie Settings
-                  li
-                    a(href='https://www.centurylink.com/aboutus/legal.html' target='_blank') Legal
-                  li
-                    a(href='https://www.lumen.com/en-us/about/legal/acceptable-use-policy.html' target='_blank') Legal Notices
-                  li
-                    a(href='https://www.lumen.com/en-us/about/legal/privacy-notice.html') Privacy
-                  li
-                    a(href='https://www.centurylink.com/aboutus/company-information/public-policy.html' target='_blank') Public Policy
-                  li
-                    a(href='https://www.lumen.com/en-us/about/legal/commission-for-complaints-for-telecom-television-services-ccts' target='_blank') Support
-                  li
-                    a(href='https://www.lumen.com/help/en-us/home.html') Canadian Ombudsman
-                  li
-                    a(href='https://assets.lumen.com/is/content/Lumen/gender-pay-gap-report?Creativeid=b565bf28-6d55-4f86-a245-2f0eb40b91a3' target='_blank') Gender Pay Gap 2020 (UK)
-                  li
-                    a(href='https://www.centurylink.com/aboutus/legal/latam/tariffs-and-regulatory-information.html') Legal (Latin America)
-                  li
-                    a(href='https://www.lumen.com/en-us/about/legal/de-legal-notices.html') Legal Notices (Germany)
-                  li
-                    a(href='https://assets.lumen.com/is/content/Lumen/modern-slavery-statement-1?Creativeid=5dbb9687-e4a8-4a40-ae00-673ce51d6a80' target='_blank') Modern Slavery Statement 2020 (UK)
-                  li
-                    a(href='https://www.lumen.com/en-us/about/legal/uk-tax-strategy.html') UK Tax Strategy
-                .chi-footer__copyright
-                  | &copy; 2021 Lumen Technologies. All Rights Reserved. Lumen is a registered trademark in the United States, EU and certain other countries.
+      div(v-html="footerTemplate" slot="footer")
 
   <pre class="language-html" slot="code-webcomponent">
     <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
@@ -149,12 +97,14 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { generateExampleFooter } from './_base.vue';
 
 declare const chi: any;
 
 @Component({
   data: () => {
     return {
+      footerTemplate: generateExampleFooter('dashboard-language-dropdown-button').htmlblueprint,
       exampleTabs: [
         {
           id: 'webcomponent',
@@ -294,86 +244,7 @@ declare const chi: any;
       </div>
     </div>
   </div>
-  <footer class="chi-footer" slot="footer">
-    <div class="chi-footer__content">
-      <div class="chi-footer__internal">
-        <div class="chi-footer__internal-content">
-          <div class="chi-dropdown chi-footer__language">
-            <a class="chi-button -icon -flat -light -sm chi-dropdown__trigger" id="language-dropdown-button" data-position="top-start" aria-label="Select your preferred language">
-              <div class="chi-button__content">
-                <i class="chi-icon icon-globe-network-outline"></i>
-                <span>English</span>
-              </div>
-            </a>
-            <div class="chi-dropdown__menu -w--sm -text--body">
-              <a class="chi-dropdown__menu-item -active" href="#">English</a>
-              <a class="chi-dropdown__menu-item" href="#">Español</a>
-              <a class="chi-dropdown__menu-item" href="#">Português</a>
-              <a class="chi-dropdown__menu-item" href="#">Français</a>
-              <a class="chi-dropdown__menu-item" href="#">Deutsch</a>
-              <a class="chi-dropdown__menu-item" href="#">简体中文</a>
-              <a class="chi-dropdown__menu-item" href="#">日本語</a>
-            </div>
-          </div>
-          <div class="chi-footer__links">
-            <ul>
-              <li>
-                <a href="https://www.lumen.com/en-us/about.html">About Us</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/community/community-development/programs-for-customers-with-disabilities.html">Accessibility</a>
-              </li>
-              <li>
-                <a href="https://jobs.lumen.com" target="_blank">Careers</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/contact-us.html">Contact Us</a>
-              </li>
-              <!-- OneTrust Cookies Settings button start-->
-              <li>
-                <a class="optanon-toggle-display" href="#">Cookie Settings</a>
-              </li>
-              <!-- OneTrust Cookies Settings button end-->
-              <li>
-                <a href="https://www.centurylink.com/aboutus/legal.html" target="_blank">Legal</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/acceptable-use-policy.html" target="_blank">Legal Notices</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/privacy-notice.html">Privacy</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/company-information/public-policy.html" target="_blank">Public Policy</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/commission-for-complaints-for-telecom-television-services-ccts" target="_blank">Support</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/help/en-us/home.html">Canadian Ombudsman</a>
-              </li>
-              <li>
-                <a href="https://assets.lumen.com/is/content/Lumen/gender-pay-gap-report?Creativeid=b565bf28-6d55-4f86-a245-2f0eb40b91a3" target="_blank">Gender Pay Gap 2020 (UK)</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/legal/latam/tariffs-and-regulatory-information.html">Legal (Latin America)</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/de-legal-notices.html">Legal Notices (Germany)</a>
-              </li>
-              <li>
-                <a href="https://assets.lumen.com/is/content/Lumen/modern-slavery-statement-1?Creativeid=5dbb9687-e4a8-4a40-ae00-673ce51d6a80" target="_blank">Modern Slavery Statement 2020 (UK)</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/uk-tax-strategy.html">UK Tax Strategy</a>
-              </li>
-            </ul>
-            <div class="chi-footer__copyright">&copy; 2021 Lumen Technologies. All Rights Reserved. Lumen is a registered trademark in the United States, EU and certain other countries.</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  ${generateExampleFooter().webcomponent}
 </chi-main>
 
 <script>
@@ -532,86 +403,7 @@ declare const chi: any;
     <div class="chi-main__background-image">
     </div>
   </div>
-  <footer class="chi-footer">
-    <div class="chi-footer__content">
-      <div class="chi-footer__internal">
-        <div class="chi-footer__internal-content">
-          <div class="chi-dropdown chi-footer__language">
-            <a class="chi-button -icon -flat -light -sm chi-dropdown__trigger" id="language-dropdown-button" data-position="top-start" aria-label="Select your preferred language">
-              <div class="chi-button__content">
-                <i class="chi-icon icon-globe-network-outline"></i>
-                <span>English</span>
-              </div>
-            </a>
-            <div class="chi-dropdown__menu -w--sm -text--body">
-              <a class="chi-dropdown__menu-item -active" href="#">English</a>
-              <a class="chi-dropdown__menu-item" href="#">Español</a>
-              <a class="chi-dropdown__menu-item" href="#">Português</a>
-              <a class="chi-dropdown__menu-item" href="#">Français</a>
-              <a class="chi-dropdown__menu-item" href="#">Deutsch</a>
-              <a class="chi-dropdown__menu-item" href="#">简体中文</a>
-              <a class="chi-dropdown__menu-item" href="#">日本語</a>
-            </div>
-          </div>
-          <div class="chi-footer__links">
-            <ul>
-              <li>
-                <a href="https://www.lumen.com/en-us/about.html">About Us</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/community/community-development/programs-for-customers-with-disabilities.html">Accessibility</a>
-              </li>
-              <li>
-                <a href="https://jobs.lumen.com" target="_blank">Careers</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/contact-us.html">Contact Us</a>
-              </li>
-              <!-- OneTrust Cookies Settings button start-->
-              <li>
-                <a class="optanon-toggle-display" href="#">Cookie Settings</a>
-              </li>
-              <!-- OneTrust Cookies Settings button end-->
-              <li>
-                <a href="https://www.centurylink.com/aboutus/legal.html" target="_blank">Legal</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/acceptable-use-policy.html" target="_blank">Legal Notices</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/privacy-notice.html">Privacy</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/company-information/public-policy.html" target="_blank">Public Policy</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/commission-for-complaints-for-telecom-television-services-ccts" target="_blank">Support</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/help/en-us/home.html">Canadian Ombudsman</a>
-              </li>
-              <li>
-                <a href="https://assets.lumen.com/is/content/Lumen/gender-pay-gap-report?Creativeid=b565bf28-6d55-4f86-a245-2f0eb40b91a3" target="_blank">Gender Pay Gap 2020 (UK)</a>
-              </li>
-              <li>
-                <a href="https://www.centurylink.com/aboutus/legal/latam/tariffs-and-regulatory-information.html">Legal (Latin America)</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/de-legal-notices.html">Legal Notices (Germany)</a>
-              </li>
-              <li>
-                <a href="https://assets.lumen.com/is/content/Lumen/modern-slavery-statement-1?Creativeid=5dbb9687-e4a8-4a40-ae00-673ce51d6a80" target="_blank">Modern Slavery Statement 2020 (UK)</a>
-              </li>
-              <li>
-                <a href="https://www.lumen.com/en-us/about/legal/uk-tax-strategy.html">UK Tax Strategy</a>
-              </li>
-            </ul>
-            <div class="chi-footer__copyright">&copy; 2021 Lumen Technologies. All Rights Reserved. Lumen is a registered trademark in the United States, EU and certain other countries.</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  ${generateExampleFooter().htmlblueprint}
 </div>
 
 <script>
