fc2e120b14db79322a3416c9f38294d5d5dc8e5c
krzysztofdrozdowski <krzysztof.drozdowski@ready4s.com>
2023-03-27 13:09:57 +0200

    [FSDE-30898] Move sidenav script to separate file

---
+++ src/website/assets/scripts/sidenav.js
@@ -0,0 +1,12 @@
+(function() {
+  var accordions = document.querySelectorAll('.chi-accordion');
+
+  accordions.forEach((accordion) => {
+    var accordionItem = accordion.querySelector('.chi-accordion__item');
+    var hasActiveChild = !!accordion.querySelector('.-active');
+
+    if (hasActiveChild) {
+      accordionItem.classList.add('-expanded');
+    }
+  });
+})();
