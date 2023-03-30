31268e91a3ead98c486717bf47bd13e2a257b151
krzysztofdrozdowski <krzysztof.drozdowski@ready4s.com>
2023-03-30 08:55:17 +0200

    [FSDE-30898] Resolve PR review comments

--- src/website/assets/scripts/sidenav.js
+++ src/website/assets/scripts/sidenav.js
@@ -1,12 +1,10 @@
-(function() {
-  var accordions = document.querySelectorAll('.chi-accordion');
+var accordions = document.querySelectorAll('.chi-accordion');
 
-  accordions.forEach((accordion) => {
-    var accordionItem = accordion.querySelector('.chi-accordion__item');
-    var hasActiveChild = !!accordion.querySelector('.-active');
+accordions.forEach((accordion) => {
+  var accordionItem = accordion.querySelector('.chi-accordion__item');
+  var hasActiveChild = !!accordion.querySelector('.-active');
 
-    if (hasActiveChild) {
-      accordionItem.classList.add('-expanded');
-    }
-  });
-})();
+  if (hasActiveChild) {
+    accordionItem.classList.add('-expanded');
+  }
+});
