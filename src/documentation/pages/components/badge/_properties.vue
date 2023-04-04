4df0e5191081e85c2e2d4cc0b1b337f5cdb60e77
josecarlosgonzalezv <josecarlos.gonzalezvillegas@lumen.com>
2023-04-04 18:01:48 +0200

    [FSDE-31040] Test

--- src/documentation/pages/components/badge/_properties.vue
+++ src/documentation/pages/components/badge/_properties.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div
     h2 Web Component
-    <PropertiesGenerator tag="chi-badge" />
+    <client-only placeholder="loading...">
+      <PropertiesGenerator tag="chi-badge" />
+    </client-only>
 </template>
 
 <script lang="ts">
