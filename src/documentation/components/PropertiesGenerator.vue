bbc5abf0ac369c50312ece44771e2f3e21209260
josecarlosgonzalezv <josecarlos.gonzalezvillegas@lumen.com>
2023-04-17 11:48:08 +0200

    [FSDE-31040] Test props without v-if

--- src/documentation/components/PropertiesGenerator.vue
+++ src/documentation/components/PropertiesGenerator.vue
@@ -1,42 +1,41 @@
 <template lang="pug">
-  div(v-if="props.length")
-    h3 Properties
-    section.chi-table.chi-table__options.-bordered.-my--3
-      div(style='overflow-x:auto')
-        table.-text(cellpadding='0', cellspacing='0')
-          thead
-            tr
-              th
-                div Property
-              th
-                div Attribute
-              th
-                div Description
-              th
-                div Type
-              th
-                div Default
-          tbody
-            tr(v-for="prop in props")
-              td(v-if="prop.name !== 'extraClass'")
-                div
-                  code {{ prop.name }}
-                  strong(v-if="prop.required") (required)
-              td(v-if="prop.name !== 'extraClass'")
-                div
-                  code(v-if="prop.attr") {{prop.attr}}
-                  div(v-else)| --
-              td(v-if="prop.name !== 'extraClass'")
-                div
-                  div(v-if="prop.deprecation !== undefined").-text--danger **[DEPRECATED]** {{ prop.deprecation }}
-                  | {{ prop.docs }}
-              td(v-if="prop.name !== 'extraClass'")
-                div
-                  code {{ prop.type }}
-              td(v-if="prop.name !== 'extraClass'")
-                div
-                  code(v-if="prop.default") {{ prop.default }}
-                  em(v-else) undefined
+  h3 Properties
+  section.chi-table.chi-table__options.-bordered.-my--3
+    div(style='overflow-x:auto')
+      table.-text(cellpadding='0', cellspacing='0')
+        thead
+          tr
+            th
+              div Property
+            th
+              div Attribute
+            th
+              div Description
+            th
+              div Type
+            th
+              div Default
+        tbody
+          tr(v-for="prop in props")
+            td(v-if="prop.name !== 'extraClass'")
+              div
+                code {{ prop.name }}
+                strong(v-if="prop.required") (required)
+            td(v-if="prop.name !== 'extraClass'")
+              div
+                code(v-if="prop.attr") {{prop.attr}}
+                div(v-else)| --
+            td(v-if="prop.name !== 'extraClass'")
+              div
+                div(v-if="prop.deprecation !== undefined").-text--danger **[DEPRECATED]** {{ prop.deprecation }}
+                | {{ prop.docs }}
+            td(v-if="prop.name !== 'extraClass'")
+              div
+                code {{ prop.type }}
+            td(v-if="prop.name !== 'extraClass'")
+              div
+                code(v-if="prop.default") {{ prop.default }}
+                em(v-else) undefined
 </template>
 
 <script lang="ts">
@@ -60,16 +59,14 @@ export default class PropertiesGenerator extends Vue {
   }
 
   created() {
-    if (this.$data.docs) {
-      const component = this.$data.docs.components?.find(
-        (component: {tag: string}) => component.tag === this.tag
-      );
+    const component = this.$data.docs.components?.find(
+      (component: {tag: string}) => component.tag === this.tag
+    );
 
-      if (component) {
-        this.$data.props = component.props;
-        this.$data.events = component.events;
-        this.$data.methods = component.methods;
-      }
+    if (component) {
+      this.$data.props = component.props;
+      this.$data.events = component.events;
+      this.$data.methods = component.methods;
     }
   }
 }
