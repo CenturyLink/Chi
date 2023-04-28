8e399ab9d918389be0272b70373fa1225a44d709
pedrotaglialatela-lumen <pedro.taglialatela@lumen.com>
2023-04-27 23:43:58 -0300

    [FSDE-30940] Implement cell wrapping in data table header

--- src/chi-vue/src/components/data-table/DataTable.tsx
+++ src/chi-vue/src/components/data-table/DataTable.tsx
@@ -168,6 +168,18 @@ export default class DataTable extends Vue {
     );
   }
 
+  _getLabel(label: string) {
+    if (this.cellWrap) {
+      return <DataTableTooltip textWrap={this.cellWrap} msg={label} class="-w--100" />;
+    }
+
+    if (this.config.truncation) {
+      return <DataTableTooltip msg={label} header />;
+    }
+
+    return label;
+  }
+
   _head() {
     const tableHeadCells = [
       this.config.selectable ? (
@@ -236,6 +248,7 @@ export default class DataTable extends Vue {
           this.config.columnSizes && this._currentScreenBreakpoint
             ? this.config.columnSizes[this._currentScreenBreakpoint][cellIndex]
             : null;
+
       const sortableColumnHead = (
         <div
           aria-label={`Sort Column ${label}`}
@@ -256,7 +269,7 @@ export default class DataTable extends Vue {
               ${cellWidth === 0 ? 'display: none;' : ''}
               ${this.data.head[columnIndex].allowOverflow ? 'overflow: visible;' : ''}
               `}>
-          {this.config.truncation ? <DataTableTooltip msg={label} header /> : label}
+          {this._getLabel(label as string)}
           {infoIcon}
           {sortIcon}
         </div>
@@ -271,7 +284,7 @@ export default class DataTable extends Vue {
               ${cellWidth === 0 ? 'display: none;' : ''}
               ${this.data.head[columnIndex].allowOverflow ? 'overflow: visible;' : ''}
               `}>
-          {this.config.truncation ? <DataTableTooltip msg={label} header /> : label}
+          {this._getLabel(label as string)}
           {infoIcon}
         </div>
       );
