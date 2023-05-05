5c2385497aaf2df59f4a67f1fc5de183a36587a5
lumdmtr <ss@ss>
2023-05-05 18:59:04 +0200

    [FSDE-18259] add Image to new vue doc

---
+++ src/documentation/pages/utilities-wip/image/examples/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .chi-grid__container.-pt--3
+    h2 Examples
+    <Base />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Base from './_base.vue';
+
+@Component({
+  components: {
+    Base
+  }
+})
+export default class Examples extends Vue {}
+</script>
---
+++ src/documentation/pages/utilities-wip/image/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    <TitleBar title="Image" description="Use to change the responsive behavior of an image element." />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+@Component({
+  head() {
+    return {
+      title: 'Chi - Image'
+    }
+  },
+  components: {
+    Examples
+  }
+})
+export default class Image extends Vue {}
+</script>
