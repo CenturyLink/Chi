bc0d5463adb6f936ee4d63468d80da8cb21e4c11
Ismael Teixeira <ismael@MBP-de-Ismael.Home>
2023-05-12 10:27:59 +0100

    [FSDE-18268] z-index page migrated

---
+++ src/documentation/pages/utilities-wip/zindex/example/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .chi-grid__container.-pt--3
+    <SupportedValues :indexes="indexes" />
+    <Example :indexes="indexes" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SupportedValues from './_supportedvalues.vue';
+import Example from './_example.vue';
+
+@Component({
+  components: {
+    SupportedValues,
+    Example
+  },
+  data: () => {
+    return {
+      indexes: [
+        {key: '0', value:    {color: '-bg--black',    margin: 1, text: 'Initial value'},},
+        {key: '1', value:    {color: '',              margin: 0, text: 'Prominent elements (E.g. focused buttons)'},},
+        {key: '10', value:   {color: '-bg--danger',   margin: 2, text: 'Dropdowns'},},
+        {key: '20', value:   {color: '-bg--warning',  margin: 3, text: 'Fixed elements (E.g. drawer)'},},
+        {key: '30', value:   {color: '-bg--success',  margin: 4, text: 'Backdrops'},},
+        {key: '40', value:   {color: '-bg--secondary',margin: 5, text: 'Fixed elements with backdrop (E.g. drawer with backdrop)'},},
+        {key: '50', value:   {color: '-bg--grey-20',  margin: 6, text: 'Modals'},},
+        {key: '60', value:    {color: '-bg--grey-10',  margin: 7, text: 'Popovers'},},
+        {key: '70', value:   {color: '-bg--primary',  margin: 8, text: 'Tooltips'},},
+        {key: 'auto', value: {color: '',              margin: 0, text: '-'}},
+      ],
+    }
+  }
+})
+export default class Examples extends Vue {}
+</script>
---
+++ src/documentation/pages/utilities-wip/zindex/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    <TitleBar title="Z-index" description="Use z-index utility classes to control the index of your elements in the z dimension. When two elements overlap each other, z-index establishes what element overlaps the other. Usually elements with larger z-index overlap elements with smaller ones." />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './example/index.vue';
+
+@Component({
+  head() {
+    return {
+      title: 'Chi - Z-index'
+    }
+  },
+  components: {
+    Examples
+  }
+})
+export default class ImageUtility extends Vue {}
+</script>
