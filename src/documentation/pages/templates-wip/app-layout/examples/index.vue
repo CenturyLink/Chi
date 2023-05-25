fb81f73bb241739737fb27bf384a865e5abd9135
krzysztofdrozdowskilumen <125541591+krzysztofdrozdowskilumen@users.noreply.github.com>
2023-05-25 16:34:26 +0200

    [FSDE-30730] Migrate app layout and error 500 templates to new Vue doc (#599)
    * [FSDE-30730] Migrate 500 error template
    
    * [FSDE-30730] Migrate app layout templates
    
    * [FSDE-30730] Generate footer in base example
    
    * [FSDE-30730] Move app layouts and error 500 to templates-wip folder
    
    * [FSDE-30730] Change active tab to web component
    
    * [FSDE-30730] Resolve PR review comments
    
    * [FSDE-30730] Remove chi dropdown when it's not necessary
    
    * [FSDE-30730] Simplify footer generation
    
    * [FSDE-30730] Improve templates readability
    
    ---------
    
    Co-authored-by: krzysztofdrozdowski <krzysztof.drozdowski@ready4s.com>
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>
    Co-authored-by: José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>

---
+++ src/documentation/pages/templates-wip/app-layout/examples/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div
+    h2 Examples
+    p.-text To render application layout, use the tag <code>chi-main</code>.
+
+    <Base />
+    <HelpIcon />
+    <Alert />
+    <BackLink />
+    <BackLinkSubtitle />
+    <BackLinkSubtitleButtons />
+    <BackLinkSubtitleButtonsPageLevelActions />
+    <Dashboard />
+    <DashboardAlert />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Alert from './_alert.vue';
+import Base from './_base.vue';
+import HelpIcon from './_help_icon.vue';
+import BackLink from './_back_link.vue';
+import BackLinkSubtitle from './_back_link_subtitle.vue';
+import BackLinkSubtitleButtons from './_back_link_subtitle_buttons.vue';
+import BackLinkSubtitleButtonsPageLevelActions from './_back_link_subtitle_buttons_page_level_actions.vue';
+import Dashboard from './_dashboard.vue';
+import DashboardAlert from './_dashboard_alert.vue';
+
+@Component({
+  components: {
+    Base,
+    HelpIcon,
+    Alert,
+    BackLink,
+    BackLinkSubtitle,
+    BackLinkSubtitleButtons,
+    BackLinkSubtitleButtonsPageLevelActions,
+    Dashboard,
+    DashboardAlert,
+  }
+})
+export default class Examples extends Vue {}
+</script>
