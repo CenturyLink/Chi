239a16adac35fe230445a32b84204320b9fcac15
mtadejalumen <131356709+mtadejalumen@users.noreply.github.com>
2023-06-01 16:58:02 +0200

    [FSDE-18260] Opacity (#688)
    * [FSDE-18260]: Opacity - move to new docs
    
    * [FSDE-18260]: Opacity - move to new docs
    
    * [FSDE-18260]: Opacity - move to new docs
    
    * [FSDE-18260]: Opacity - move to new docs
    
    * [FSDE-18260]: Opacity - move to new docs
    
    * [FSDE-18260]: Opacity - move to new docs
    
    ---------
    
    Co-authored-by: José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>

---
+++ src/documentation/pages/utilities-wip/opacity/examples/_hover.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  <ComponentExample padding="-p--2" title="Hover" id="opacity-hover" :tabs="exampleTabs" :showSnippetTabs="false">
+    p.-text(slot="example-description")
+      | Use <code>-opacity-hover--{value}</code> to change the opacity of an element on hover.
+    .chi-grid.-no-gutter(slot="example")
+      .chi-col.-w--6.-w-md--4.-w-lg--2(v-for="opacity in opacities")
+        .-text.-text--center.-b--1.-rounded.-m--1.-p--2(:class="[`-opacity-hover--${ opacity }`, opacity === 100 ? '-opacity--60' : '']") {{ opacity + '% on hover' }}
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      opacities: [0, 20, 40, 60, 80, 100],
+      exampleTabs: [
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML Blueprint',
+        },
+      ],
+    };
+  },
+})
+export default class Hover extends Vue {
+  get codeSnippets() {
+    return {
+      htmlblueprint: this.generateHtml(),
+    };
+  }
+
+  generateHtml() {
+    return (
+      this.$data.opacities
+        .slice(0, 5)
+        .map(
+          (opacity: number) =>
+            `<div class="-opacity-hover--${opacity}">...</div>\n`
+        )
+        .join('') + '<div class="-opacity--60 -opacity-hover--100">...</div>'
+    );
+  }
+}
+</script>
