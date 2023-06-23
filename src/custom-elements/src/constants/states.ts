f2a831ea2f33a4e532f323e14053b6538cdc92f4
pedrotaglialatela-lumen <pedro.taglialatela@lumen.com>
2023-06-22 22:25:32 -0300

    Create warning color and object literal to work with icons

--- src/custom-elements/src/constants/states.ts
+++ src/custom-elements/src/constants/states.ts
@@ -1,3 +1,18 @@
 export const CHI_STATES = ['success', 'warning', 'danger'] as const;
 
 export type ChiStates = typeof CHI_STATES[number];
+
+export const TEXT_FIELD_STATES = {
+  warning: {
+    class: '-warning',
+    icon: 'warning'
+  },
+  danger: {
+    class: '-danger',
+    icon: 'circle-warning'
+  },
+  success: {
+    class: '-success',
+    icon: 'circle-check'
+  }
+}
