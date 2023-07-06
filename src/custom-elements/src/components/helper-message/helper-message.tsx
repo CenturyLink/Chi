9d4a027333fc1190fbff826f7ab378783f3c0510
pedrotaglialatela-lumen <127979195+pedrotaglialatela-lumen@users.noreply.github.com>
2023-07-06 11:17:50 -0300

    [FSDE-31409] Add support for additional messaging below text input components (#726)
    * Create helper text component
    
    * Create warning color and object literal to work with icons
    
    * implement helper component to text input
    
    * Updates requested by JC and JL
    
    * Replace helperText prop to be helperMessage
    
    * Update visual test - checkbox test states
    
    * Update component name + fix indentation
    
    * Rename component to use components pattern prefix "chi-"
    
    ---------
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>

---
+++ src/custom-elements/src/components/helper-message/helper-message.tsx
@@ -0,0 +1,30 @@
+import { Component, Element, Prop, h } from '@stencil/core';
+import { ChiStates, STATES } from "../../constants/states";
+import { FORM_CLASSES, STATUS_CLASS } from "../../constants/classes";
+
+@Component({
+  tag: 'chi-helper-message',
+  styleUrl: 'helper-message.scss',
+  scoped: true
+})
+
+export class HelperMessage {
+  @Element() el: HTMLElement;
+
+  /**
+   * To define state color of message text
+   */
+  @Prop({ reflect: true }) state?: ChiStates;
+
+  render() {
+    const stateIcon = this.state ? STATES[this.state].icon : null;
+    const stateClass = this.state ? STATES[this.state].class : null;
+
+    return (
+      <div class={`${FORM_CLASSES.LABEL} ${STATUS_CLASS} ${stateClass}`}>
+        {stateIcon && <chi-icon icon={stateIcon}></chi-icon>}
+        <slot></slot>
+      </div>
+    );
+  }
+}
