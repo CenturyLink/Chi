1ab074fd7fada3c23aee964ed86fb5f30dc27ed7
kosgor <kostiantyn.horbachenko@lumen.com>
2023-07-14 13:02:59 +0200

    [FSDE-18271] Migrate foundations accessibility keyboard to new Vue documentation

---
+++ src/documentation/pages/foundations-wip/accessibility/using-keyboard/examples/index.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .chi-grid__container.-pt--3
+    <Description />
+    <InfoAndRelationships />
+    <Keyboard />
+    <NoKeyboard />
+    <FocusOrder />
+    <FocusVisible />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Description from './_description.vue';
+import InfoAndRelationships from './_infoAndRelationships.vue';
+import Keyboard from './_keyboard.vue';
+import NoKeyboard from './_noKeyboard.vue';
+import FocusOrder from './_focusOrder.vue';
+import FocusVisible from './_focusVisible.vue';
+
+@Component({
+  components: {
+    Description,
+    InfoAndRelationships,
+    Keyboard,
+    NoKeyboard,
+    FocusOrder,
+    FocusVisible,
+  }
+})
+export default class Examples extends Vue {}
+</script>
