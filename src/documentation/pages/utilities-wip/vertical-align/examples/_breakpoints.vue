cbca6c5570b7b776c7cabd3cdbbb80b3cf5621ae
kosgor <kostiantyn.horbachenko@lumen.com>
2023-07-25 16:34:48 +0200

    [FSDE-18267] Migrate utilities vertical align to new Vue documentation

---
+++ src/documentation/pages/utilities-wip/vertical-align/examples/_breakpoints.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div
+    <TitleAnchor title="Target all breakpoints" id="breakpoints-base-vertical" />
+    p.-text
+      | Vertical-align utility classes with no breakpoint defined apply to all screen sizes.
+    table.chi-table.-bordered.-mt--3.-mb--3
+      thead
+        tr
+          th
+            div Class
+          th
+            div Value
+      tbody
+        tr(v-for="value in values")
+          td.-text(width='40%')
+            code -vertical-align--{{ value }}
+          td
+            code vertical-align: {{ value }}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      values: ['baseline', 'top', 'middle', 'bottom', 'text-top', 'text-bottom'],
+    };
+  },
+})
+export default class Breakpoints extends Vue {}
+</script>
---
+++ src/documentation/pages/utilities-wip/vertical-align/examples/_text-bottom.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  <ComponentExample padding="-p--2" title="Text bottom" id="vertical-align-text-bottom" :tabs="exampleTabs" :showSnippetTabs="false">
+    p.-text(slot="example-description")
+      | Use <code>-vertical-align--text-bottom</code> to align the bottom of an element with the bottom of the parent element's font.
+    .-d--flex.-flex--wrap.-p--1.-text(slot="example")
+      .-position--relative.-w--100.example-vertical-align
+        span.-d--inline-block.-bg--secondary.-vertical-align--text-bottom.-mr--1.example-vertical-align__block
+        span text-bottom
+    <pre class="language-html" slot="code-htmlblueprint">
+      <code v-highlight="codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML Blueprint',
+        },
+      ],
+      codeSnippets: {
+        htmlblueprint: '<span class="-vertical-align--text-bottom">...</span>',
+      }
+    };
+  },
+})
+export default class TextBottom extends Vue {}
+</script>
