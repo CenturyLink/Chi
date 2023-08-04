9c1eb06ba3c2d1c801d9dd7ae6889dc780d25068
ismael-teixeira <132471304+ismael-teixeira@users.noreply.github.com>
2023-08-04 09:17:12 +0100

    [FSDE-31343] Migrating foundations Grid page to new vue doc (#723)
    * [FSDE-31343] Migrating foundations page to new vue doc
    
    * [FSDE-31343] Applying more fixes to formatting and naming
    
    * [FSDE-31343] fixing broken examples and using controller variables in the markup
    
    * [FSDE-31343] removing unnecessary spaces and divs and updating functions for generate html
    
    * [FSDE-31343] fixing spaces issues and wrong names in template
    
    * [FSDE-31343] fix to generateHtml function in viewport column sizing
    
    * [FSDE-31343] formatting files
    
    * [FSDE-31343] one liner for array definition
    
    ---------
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>
    Co-authored-by: José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>

---
+++ src/documentation/pages/foundations-wip/grid/examples/_column-specific-vertical-alignment.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  <ComponentExample title="Column-Specific Vertical Alignment" id="column-specific-vertical-alignment" :tabs="exampleTabs" :showSnippetTabs="false" padding="-p--0" additionalStyle="border: none;">
+    p.-text(slot="example-description")
+      | Changing the alignment of an individual column is as easy as applying 
+      | an alignment modifier to the column in question.
+      
+    .chi-grid.-show--example.-mb--3(style="height: 160px" slot="example")
+      .chi-col(v-for="item in gridCols" :class="item" :key="item")
+
+    <pre class="language-html" slot="code-htmlblueprint" style="border:none;">
+      <code v-highlight="codeSnippets.htmlblueprint" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML Blueprint'
+        }
+      ]
+    };
+  }
+})
+export default class ColumnSpecificVerticalAlignment extends Vue {
+  gridCols = ['-align--top', '-align--center', '-align--bottom'];
+
+  get codeSnippets() {
+    return {
+      htmlblueprint: this.generateHtml()
+    };
+  }
+
+  generateHtml() {
+    return `<div class="chi-grid" style="height: 160px;">
+${this.gridCols
+  .map((item: string) => `  <div class="chi-col ${item}"></div>`)
+  .join('\n')}
+</div>`;
+  }
+}
+</script>
---
+++ src/documentation/pages/foundations-wip/grid/examples/_column-wrapping.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  <ComponentExample title="Column Wrapping" id="column-wrapping" :tabs="exampleTabs" :showSnippetTabs="false" padding="-p--0" additionalStyle="border: none;">
+    p.-text(slot="example-description")
+      | Grid containers can accommodate any number of columns, however, the 
+      | grid system is restricted to a maximum of 12 column units per row. 
+      | Overflowing the 12 column units will automatically wrap additional 
+      | columns onto a new row. For example, the following layout is the 
+      | result of having 24 column units in a single container:
+    
+    .chi-grid.-mb--2.-show--example(slot="example")
+      .chi-col(v-for="item in grid4Columns" :key="item" class="-w--3")
+      .chi-col(v-for="item in grid2Columns" :key="item" class="-w--6")
+    
+    <pre class="language-html" slot="code-htmlblueprint" style="border:none;">
+      <code v-highlight="codeSnippets.htmlblueprint3Cols" class="html"></code>
+    </pre>
+  </ComponentExample>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      exampleTabs: [
+        {
+          active: true,
+          id: 'htmlblueprint',
+          label: 'HTML Blueprint'
+        }
+      ]
+    };
+  }
+})
+export default class ColumnWrapping extends Vue {
+  grid2Columns = Array(2).fill('chi-col');
+  grid4Columns = Array(4).fill('chi-col');
+
+  get codeSnippets() {
+    return {
+      htmlblueprint3Cols: this.generateHtml()
+    };
+  }
+
+  generateHtml() {
+    return `<div class="chi-grid">
+
+  <!-- first 12 column units -->
+${this.generateHtmlList(this.grid4Columns, '3')}
+
+  <!-- ... and another row consisting of 12 more units -->
+${this.generateHtmlList(this.grid2Columns, '6')}
+
+</div>`;
+  }
+
+  generateHtmlList(array: string[], length: string) {
+    return `${array
+      .map(() => `  <div class="chi-col -w--${length}"></div>`)
+      .join('\n')}`;
+  }
+}
+</script>
