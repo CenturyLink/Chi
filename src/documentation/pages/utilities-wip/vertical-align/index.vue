7feb33c880e610361d0053783852bec14b1c0227
kosgor <125550088+kosgor@users.noreply.github.com>
2023-08-10 16:10:42 +0200

    [FSDE-18267] Migrate utilities vertical align to new Vue documentation (#760)
    * [FSDE-18267] Migrate utilities vertical align to new Vue documentation
    
    * [FSDE-18267] Fix PR comments
    
    * [FSDE-18267] Fix PR comments
    
    ---------
    
    Co-authored-by: José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>

---
+++ src/documentation/pages/utilities-wip/vertical-align/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    <TitleBar title="Vertical-align" description="Use to change the vertical alignment of inline, inline-block, inline-table, and table cell elements." />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+
+@Component({
+  head() {
+    return {
+      title: 'Chi - Vertical-align'
+    }
+  },
+  components: {
+    Examples
+  }
+})
+export default class VerticalAlign extends Vue {}
+</script>
