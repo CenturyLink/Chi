721cfd08a2e16dec3d1ec79da4cae32ad6e8072c
José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>
2023-08-31 18:19:41 +0200

    [DPEDE-539] Fix documentation dependencies (#793)
    * [DPEDE-539] Fix documentation dependencies
    
    * [DPEDE-539] Change theme in URL when change theme with the ThemeSwitcher
    
    ---------
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>

--- src/documentation/utilities/utilities.ts
+++ src/documentation/utilities/utilities.ts
@@ -37,3 +37,7 @@ export const generateUtilitiesTextContent = (
       return '';
   }
 };
+
+export function capitalize(word: string): string {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
