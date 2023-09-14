f6236d7b6fec4205a3a6a75501276ae140c1c0d3
kosgor <kostiantyn.horbachenko@lumen.com>
2023-09-14 12:01:50 +0200

    [DPEDE-577] Migrate foundations accessibility other to new Vue documentation

---
+++ src/documentation/pages/foundations-wip/accessibility/other/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    <TitleBar title="Other" />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+
+@Component({
+  head() {
+    return {
+      title: 'Chi - Other'
+    }
+  },
+  components: {
+    Examples
+  }
+})
+export default class LinksForms extends Vue {}
+</script>
