4f8b22f1b96103d8776bd252b8cebe1809fbcf6a
kosgor <125550088+kosgor@users.noreply.github.com>
2023-09-21 16:24:07 +0200

    [DPEDE-374] Migrate getting started development workflow to new Vue documentation (#814)
    * [DPEDE-374] Migrate getting started development workflow to new Vue documentation
    
    * [DPEDE-374] Fix copy component
    
    ---------
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>

---
+++ src/documentation/pages/getting-started-wip/development-workflow/examples/_component-ts-class.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div
+    <TitleAnchor title="Component (TS class)" id="component-ts-class" titleSize="h3" />
+    p.-text The name of the ES6 class of the component SHOULD NOT have a prefix since classes are scoped. There is no risk of collision.
+
+    <Copy id="ts-class">
+      <pre class="language-html" slot="code">
+        <code v-highlight="codeSnippets.htmlblueprint" lang='bash' class="html"></code>
+      </pre>
+    </Copy>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => {
+    return {
+      codeSnippets: {
+        htmlblueprint: `@Component({
+  tag: 'chi-button'
+})
+export class Button { ... }
+
+@Component({
+  tag: 'chi-icon'
+})
+export class Icon { ... }`
+      }
+    }
+  }
+})
+export default class ComponentTsClass extends Vue {}
+</script>
---
+++ src/documentation/pages/getting-started-wip/development-workflow/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    <TitleBar title="Development workflow" />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Examples from './examples/index.vue';
+
+@Component({
+  head() {
+    return {
+      title: 'Chi - Development workflow'
+    }
+  },
+  components: {
+    Examples
+  }
+})
+export default class DevelopmentWorkflow extends Vue {}
+</script>
