15956d4ed8b0d60b24a323272f8b1b6eed94614d
kosgor <kostiantyn.horbachenko@lumen.com>
2023-09-21 15:45:34 +0200

    [DPEDE-375] Migrate getting started browser support to new Vue documentation

---
+++ src/documentation/pages/getting-started-wip/browsers-devices/examples/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .chi-grid__container.-pt--3
+    <Setup />
+    <BrowserSupport />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import BrowserSupport from './_browser-support.vue';
+
+@Component({
+  components: {
+    BrowserSupport
+  }
+})
+export default class Examples extends Vue {}
+</script>
