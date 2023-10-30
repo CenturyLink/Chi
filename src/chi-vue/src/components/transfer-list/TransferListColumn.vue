ec85af425fb6fb1914a0c15e46a26f6e454d0d0b
Pedro Taglialatela <pedrohtag@gmail.com>
2023-10-30 09:47:33 -0300

    [DPEDE-776] Create HTML/CSS Transfer list component

---
+++ src/chi-vue/src/components/transfer-list/TransferListColumn.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="transfer-list-column">
+    <p class="transfer-list-column__title">{{ title }}</p>
+    <div class="transfer-list-column-box">
+      <div class="transfer-list-column-box__search">
+        <chi-search-input placeholder="Filter" />
+      </div>
+      <div class="transfer-list-column-box__content">
+        <div v-for="(item, index) in list" :key="index" class="transfer-list-column-box__item">
+          <chi-checkbox :id="item.name" :label="item.label" v-if="withCheckbox === false" />
+          <span v-else>{{ item.label }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop } from 'vue-property-decorator';
+import { Component, Vue } from '@/build/vue-wrapper';
+import list from './__mock__/list.json';
+
+@Component({
+  data() {
+    return {
+      list,
+    };
+  },
+})
+export default class TransferListColumn extends Vue {
+  @Prop() title!: string;
+  @Prop() withSearch?: boolean;
+  @Prop() withCheckbox?: boolean;
+}
+</script>
