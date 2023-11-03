955668866cb5818aec9a9cb78f8fe828f226e60e
Pedro Taglialatela <pedrohtag@gmail.com>
2023-11-03 09:11:16 -0300

    Implement new style + tooltip + actions

--- src/chi-vue/src/components/transfer-list/TransferListColumn.vue
+++ src/chi-vue/src/components/transfer-list/TransferListColumn.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="transfer-list-column">
-    <p class="transfer-list-column__title">{{ title }}</p>
-    <div class="transfer-list-column-box">
-      <div class="transfer-list-column-box__header">
-        <chi-search-input placeholder="Filter" />
+    <div class="transfer-list-column-header">
+      <p class="transfer-list-column-header__title">
+        {{ title }}
+        <ChiTooltip message="Your tooltip text">
+          <chi-icon icon="circle-info-outline" size="xs" />
+        </ChiTooltip>
+      </p>
+      <div class="transfer-list-column-header__actions">
+        <chi-icon icon="atom" size="sm--3" />
+        <chi-icon icon="atom" size="sm--3" />
       </div>
-      <div class="transfer-list-column-box__content">
-        <div v-for="(item, index) in list" :key="index" class="transfer-list-column-box__item">
-          <chi-checkbox :id="item.name" :label="item.label" />
-        </div>
+    </div>
+    <div class="transfer-list-column__search">
+      <chi-search-input placeholder="Filter" />
+    </div>
+    <div class="transfer-list-column__content">
+      <div v-for="(item, index) in list" :key="index" class="transfer-list-item">
+        <chi-checkbox :id="item.name" :label="item.label" />
       </div>
     </div>
   </div>
@@ -18,8 +27,11 @@
 import { Prop } from 'vue-property-decorator';
 import { Component, Vue } from '@/build/vue-wrapper';
 import list from './__mock__/list.json';
-
+import Tooltip from '../tooltip/tooltip';
 @Component({
+  components: {
+    ChiTooltip: Tooltip,
+  },
   data() {
     return {
       list,
