0c149064d8c11a2f00afe812e23af7369b607455
Wojciech Wlodarczyk <wojciech.wlodarczyk@lumen.com>
2023-11-08 11:27:02 +0100

    [DPEDE-807] Show console logs, computed set on whole PG component

--- src/documentation/pages/components/badge/_properties.vue
+++ src/documentation/pages/components/badge/_properties.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
   div
     h2 Web Component
+    <PropertiesGenerator v-if="isChiDocsLoaded" tag="chi-badge" />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component({})
+@Component({
+  computed: {
+    isChiDocsLoaded() {
+      return Vue?.prototype?.$chiDocs !== undefined;
+    }
+  }
+})
 export default class Properties extends Vue {}
 </script>
