6391bda9d8d29bd42a9a4e6e772d6c343e5253b2
Pedro Taglialatela <pedrohtag@gmail.com>
2023-11-16 14:13:51 -0300

    Clean up component + add locked state + refactor

--- src/chi-vue/src/components/transfer-list/TransferListColumn.vue
+++ src/chi-vue/src/components/transfer-list/TransferListColumn.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="chi-transfer-list">
+  <div class="chi-transfer-list__column">
     <div class="chi-transfer-list__header">
       <p class="chi-transfer-list__title">
         {{ title }}
-        <slot name="chi-transfer-list-title-tooltip"></slot>
+
+        <ChiTooltip :message="description" v-if="description">
+          <chi-icon icon="circle-info-outline" size="xs" />
+        </ChiTooltip>
       </p>
       <div class="chi-transfer-list__header-actions">
-        <slot name="chi-transfer-list-header-actions"></slot>
+        <slot name="header-actions"></slot>
       </div>
     </div>
     <div class="chi-transfer-list__search">
       <chi-search-input placeholder="Filter" />
     </div>
-    <select multiple class="chi-transfer-list__menu">
+    <select multiple class="chi-select chi-transfer-list__menu">
       <option
         v-for="(item, index) in list"
         :key="index"
         :value="item.name"
-        :class="`chi-transfer-list__menu-item ${withCheckbox ? '-checkbox' : null}`">
+        :disabled="item.locked"
+        :class="getMenuItemClasses(item)">
         {{ item.label }}
       </option>
     </select>
@@ -29,6 +33,7 @@ import { Prop } from 'vue-property-decorator';
 import { Component, Vue } from '@/build/vue-wrapper';
 import list from './__mock__/list.json';
 import Tooltip from '../tooltip/tooltip';
+import { TransferListItem } from './TransferList';
 @Component({
   components: {
     ChiTooltip: Tooltip,
@@ -41,7 +46,22 @@ import Tooltip from '../tooltip/tooltip';
 })
 export default class TransferListColumn extends Vue {
   @Prop() title!: string;
+  @Prop() description?: string;
   @Prop() withSearch?: boolean;
   @Prop() withCheckbox?: boolean;
+
+  getMenuItemClasses({ locked }: TransferListItem) {
+    const classes = ['chi-transfer-list__menu-item'];
+
+    if (this.withCheckbox) {
+      classes.push('-checkbox');
+    }
+
+    if (locked) {
+      classes.push('-locked');
+    }
+
+    return classes.join(' ');
+  }
 }
 </script>
