2034901c9867eaf999695135f695c7b5f2f06cbe
Wojciech Wlodarczyk <wojciech.wlodarczyk@lumen.com>
2024-01-03 16:31:01 +0100

    [DPEDE-717] Refactored and improved empty data tables

--- src/documentation/pages/components-wip/data-table/examples/portal/_empty.vue
+++ src/documentation/pages/components-wip/data-table/examples/portal/_empty.vue
@@ -1,45 +1,54 @@
 <template lang="pug">
   div
     <TitleAnchor title="Empty" />
-    <ComponentExample title="No Results" titleSize="h4" id="no-results-data-table" :tabs="exampleTabs">
-      chi-data-table(:config="noResultsConfig" :data='table' slot="example")
+    <ComponentExample v-for="dataTable in emptyDataTables" :title="dataTable.title" titleSize="h4" :id="dataTable.id" :key="dataTable.id" :tabs="exampleTabs">
+      chi-data-table(:config="getConfig(dataTable.config)" :data='table' slot="example")
       <Wrapper slot='code-vue'>
-        .chi-tab__description
-          | Use <code>noResultsMessage</code> config to customize the no results data message
+        .chi-tab__description(v-if="dataTable.description" v-html="dataTable.description")
         pre.language-html
-          code(v-highlight="codeSnippets.noResults.vue" class="html")
+          code(v-highlight="getVueCode(dataTable.config)" class="html")
       </Wrapper>
       pre.language-html(slot="code-htmlblueprint")
-        code(v-highlight="codeSnippets.noResults.htmlblueprint" class="html")
-    </ComponentExample>
-
-    <ComponentExample title="No Filters" titleSize="h4" id="no-filters-data-table" :tabs="exampleTabs">
-      chi-data-table(:config="noFiltersConfig" :data='table' slot="example")
-      <Wrapper slot='code-vue'>
-        .chi-tab__description
-          | Use <code>noFiltersMessage</code> config to customize the no filters data message
-        pre.language-html
-          code(v-highlight="codeSnippets.noFilters.vue" class="html")
-      </Wrapper>
-      pre.language-html(slot="code-htmlblueprint")
-        code(v-highlight="codeSnippets.noFilters.htmlblueprint" class="html")
-    </ComponentExample>
-
-    <ComponentExample title="Empty Actionable" titleSize="h4" id="empty-actionable-data-table" :tabs="exampleTabs">
-      chi-data-table(:config="emptyActionableConfig" :data='table' slot="example")
-      pre.language-html(slot='code-vue')
-        code(v-highlight="codeSnippets.emptyActionable.vue" class="html")
-      pre.language-html(slot="code-htmlblueprint")
-        code(v-highlight="codeSnippets.emptyActionable.htmlblueprint" class="html")
+        code(v-highlight="getHtmlCode(dataTable.config)" class="html")
     </ComponentExample>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+type ConfigStyleType = {
+  [key: string]: any;
+};
+
 @Component({
   data: () => {
     return {
+      emptyDataTables: [
+        {
+          title: "No Results",
+          id: "no-results-data-table",
+          description: "Use <code>noResultsMessage</code> config to customize the no results data message",
+          config: {
+            noResultsMessage: 'No matching results',
+          }
+        }, {
+          title: "No Filters",
+          id: "no-filters-data-table",
+          description: "Use <code>noFiltersMessage</code> config to customize the no filters data message",
+          config: {
+            noFiltersMessage: "Search for or select at least one filter to get results",
+          }
+        }, {
+          title: "Empty Actionable",
+          id: "empty-actionable-data-table",
+          description: null,
+          config: {
+            emptyActionable: {
+              isActionable: true,
+            },
+          }
+        },
+      ],
       exampleTabs: [
         {
           active: false,
@@ -58,45 +67,7 @@ import { Component, Vue } from 'vue-property-decorator';
           label: 'HTML Blueprint',
         }
       ],
-      noResultsConfig: {
-        columnResize: true,
-        noResultsMessage: 'No matching results',
-        style: {
-          portal: true,
-          noBorder: false,
-          bordered: false,
-          hover: false,
-          size: 'md',
-          striped: false,
-        },
-        pagination: {
-          activePage: 1,
-          compact: false,
-          firstLast: false,
-          pageJumper: true,
-        },
-        resultsPerPage: 3,
-      },
-      noFiltersConfig: {
-        columnResize: true,
-        noFiltersMessage: "Search for or select at least one filter to get results",
-        style: {
-          portal: true,
-          noBorder: false,
-          bordered: false,
-          hover: false,
-          size: 'md',
-          striped: false,
-        },
-        pagination: {
-          activePage: 1,
-          compact: false,
-          firstLast: false,
-          pageJumper: true,
-        },
-        resultsPerPage: 3,
-      },
-      emptyActionableConfig: {
+      config: {
         columnResize: true,
         style: {
           portal: true,
@@ -113,9 +84,6 @@ import { Component, Vue } from 'vue-property-decorator';
           pageJumper: true,
         },
         resultsPerPage: 3,
-        emptyActionable: {
-          isActionable: true,
-        },
       },
       table: {
         head: {
@@ -125,183 +93,77 @@ import { Component, Vue } from 'vue-property-decorator';
         },
         body: [],
       },
-      codeSnippets: {
-        noResults: {
-          vue: `<!-- Vue component -->
-  <ChiDataTable :config="config" :data="table"></ChiDataTable>
-  
-  <!-- Config and Data -->
-  data: {
-    config: {
-      columnResize: true,
-      noResultsMessage: 'No matching results',
-      style: {
-        portal: true,
-        noBorder: false,
-        bordered: false,
-        hover: false,
-        size: 'md',
-        striped: false,
-      },
-      pagination: {
-        activePage: 1,
-        compact: false,
-        firstLast: false,
-        pageJumper: true,
-      },
-      resultsPerPage: 3,
-    },
-    table: {
-      head: {
-        name: { label: 'Name' },
-        id: { label: 'ID' },
-        lastLogin: { label: 'Last Login' },
-      },
-      body: []
-    }
-  }`,
-          htmlblueprint: `<div class="chi-data-table">
-    <div class="chi-data-table__head">
-      <div class="chi-data-table__row">
-        <div class="chi-data-table__cell">
-          <div>Name</div>
-        </div>
-        <div class="chi-data-table__cell">
-          <div>ID</div>
-        </div>
-        <div class="chi-data-table__cell">
-          <div>Last Login</div>
-        </div>
-      </div>
-    </div>
-    <div class="chi-data-table__body">
-      <div class="chi-data-table__row-empty">
-        <div>No matching results</div>
-      </div>
-    </div>
-  </div>`
-        },
-        noFilters: {
-          vue: `<!-- Vue component -->
-<ChiDataTable :config="config" :data="table"></ChiDataTable>
-
-<!-- Config and Data -->
-data: {
-  config: {
-    columnResize: true,
-    noFiltersMessage: "Search for or select at least one filter to get results",
-    style: {
-      portal: true,
-      noBorder: false,
-      bordered: false,
-      hover: false,
-      size: 'md',
-      striped: false,
-    },
-    pagination: {
-      activePage: 1,
-      compact: false,
-      firstLast: false,
-      pageJumper: true,
-    },
-    resultsPerPage: 3,
-  },
-  table: {
-    head: {
-      name: { label: 'Name' },
-      id: { label: 'ID' },
-      lastLogin: { label: 'Last Login' },
-    },
-    body: []
-  }
-}`,
-          htmlblueprint: `<div class="chi-data-table">
-  <div class="chi-data-table__head">
-    <div class="chi-data-table__row">
-      <div class="chi-data-table__cell">
-        <div>Name</div>
-      </div>
-      <div class="chi-data-table__cell">
-        <div>ID</div>
-      </div>
-      <div class="chi-data-table__cell">
-        <div>Last Login</div>
-      </div>
-    </div>
-  </div>
-  <div class="chi-data-table__body">
-    <div class="chi-data-table__row-empty">
-      <i class="-mr--1 chi-icon icon-search"></i>
-      Search for or select at least one filter to get results
-    </div>
-  </div>
-</div>`
-        },
-        emptyActionable: {
-          vue: `<!-- Vue component -->
-<ChiDataTable :config="config" :data="table"></ChiDataTable>
-
-<!-- Config and Data -->
-data: {
-  config: {
-    columnResize: true,
-    style: {
-      portal: true,
-      noBorder: false,
-      bordered: false,
-      hover: false,
-      size: 'md',
-      striped: false,
-    },
-    pagination: {
-      activePage: 1,
-      compact: false,
-      firstLast: false,
-      pageJumper: true,
-    },
-    resultsPerPage: 3,
-    emptyActionable: {
-      isActionable: true,
-    },
-  },
-  table: {
-    head: {
-      name: { label: 'Name' },
-      id: { label: 'ID' },
-      lastLogin: { label: 'Last Login' },
-    },
-    body: []
-  }
-}`,
-          htmlblueprint: `<div class="chi-data-table">
-  <div class="chi-data-table__head">
-    <div class="chi-data-table__row">
-      <div class="chi-data-table__cell">
-        <div>Name</div>
-      </div>
-      <div class="chi-data-table__cell">
-        <div>ID</div>
-      </div>
-      <div class="chi-data-table__cell">
-        <div>Last Login</div>
-      </div>
-    </div>
-  </div>
-  <div class="chi-data-table__body">
-    <div class="chi-data-table__row-empty -actionable">
-      <div>
-        <i class="chi-icon icon-circle-plus-outline -icon--grey" aria-hidden="true"></i>
-        <span>
-          <a>Add a new or existing service</a>, then manage here.
-        </span>
-      </div>
-    </div>
-  </div>
-</div>`
-        }
-      },
     };
   },
+  methods: {
+    getConfig(config: ConfigType) {
+      return {
+        ...this.$data.config,
+        ...config
+      }
+    },
+    getVueCode(config: ConfigType) {
+      return `<!-- Vue component -->
+<ChiDataTable :config="config" :data="table"></ChiDataTable>
+
+<!-- Config and Data -->
+data: {
+  config: {
+    columnResize: true,${config.noFiltersMessage ? `\n    noFiltersMessage: '${config.noFiltersMessage}',` : ''}${config.noResultsMessage ? `\n    noResultsMessage: '${config.noResultsMessage}',` : ''}
+    style: {
+      portal: true,
+      noBorder: false,
+      bordered: false,
+      hover: false,
+      size: 'md',
+      striped: false,
+    },
+    pagination: {
+      activePage: 1,
+      compact: false,
+      firstLast: false,
+      pageJumper: true,
+    },${config.emptyActionable ? `\n    emptyActionable: {
+      isActionable: true,
+    },` : ''}
+    resultsPerPage: 3,
+  },
+  table: {
+    head: {
+      name: { label: 'Name' },
+      id: { label: 'ID' },
+      lastLogin: { label: 'Last Login' },
+    },
+    body: []
+  }
+}`
+    },
+    getHtmlCode(config: ConfigType) {
+      return `<div class="chi-data-table">
+  <div class="chi-data-table__head">
+    <div class="chi-data-table__row">
+      <div class="chi-data-table__cell">
+        <div>Name</div>
+      </div>
+      <div class="chi-data-table__cell">
+        <div>ID</div>
+      </div>
+      <div class="chi-data-table__cell">
+        <div>Last Login</div>
+      </div>
+    </div>
+  </div>
+  <div class="chi-data-table__body">
+    <div class="chi-data-table__row-empty${config.emptyActionable ? ' -actionable' : ''}">
+      ${config.noResultsMessage ? `<div>${config.noResultsMessage}</div>` : ''}${config.noFiltersMessage ? `<i class="-mr--1 chi-icon icon-search"></i>\n      ${config.noFiltersMessage}` : ''}${config.emptyActionable ? `<i class="chi-icon icon-circle-plus-outline -icon--grey" aria-hidden="true"></i>
+        <span>
+          <a>Add a new or existing service</a>, then manage here.
+        </span>` : ''}
+    </div>
+  </div>
+</div>`
+    }
+  }
 })
 export default class DataTableEmpty extends Vue { }
 </script>
