b2d9d349d2a173e7ed8bc483e13327cbcbc932c1
Emerson Pereira <Emerson.Pereira@lumen.com>
2024-01-24 15:20:29 +0000

    [DPEDE-1250] Step minutes and step seconds (#968)
    * add minutes and step seconds for time separately
    
    * add minutes and seconds steps to timepicker
    
    * add step to new doc
    
    * remove unneded comma
    
    ---------
    
    Co-authored-by: Jose Lopez Ruiz <jose.luis@ctl.io>

--- src/documentation/pages/components-wip/time-picker/examples/_minutes-step.vue
+++ src/documentation/pages/components-wip/time-picker/examples/_minutes-step.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
-  <ComponentExample titleSize="h4" title="24-hour format" id="time-24hour" :tabs="exampleTabs">
-    chi-time(format='24hr' slot="example")
+  <ComponentExample titleSize="h4" title="Minutes step" id="minutes-step" :tabs="exampleTabs">
+    .chi-form__item(style="max-width: 14rem;" slot="example")
+      chi-label(for="example__minutes-step") Label
+      chi-time-picker(id="example__minutes-step" minutes-step="20")
     <pre class="language-html" slot="code-webcomponent">
       <code v-highlight="$data.codeSnippets.webcomponent" class="html"></code>
     </pre>
@@ -29,11 +31,17 @@ import { Component, Vue } from 'vue-property-decorator';
         }
       ],
       codeSnippets: {
-        webcomponent: `<chi-time format="24hr"></chi-time>`,
+        webcomponent: `<!-- Minutes step with default value (15 minutes) -->
+<chi-label for="example__minutes-step">Label</chi-label>
+<chi-time-picker id="example__minutes-step" minutes-step></chi-time-picker>
+
+<!-- Minutes step with custom value of 20 minutes -->
+<chi-label for="example__minutes-step">Label</chi-label>
+<chi-time-picker id="example__minutes-step" minutes-step="20"></chi-time-picker>`,
         htmlblueprint: ``
       }
     };
   }
 })
-export default class TimeTwentyFourHourFormat extends Vue {}
+export default class TwentyFourHourFormat extends Vue {}
 </script>
