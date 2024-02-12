402925e8fb67f6504342672eb578e1f69620ee87
ASanchLumen <Alejandro.Pineda@lumen.com>
2024-02-12 13:21:54 +0100

    [DPEDE-1327] enable redirect pages/index in dev mode

--- src/documentation/pages/index.vue
+++ src/documentation/pages/index.vue
@@ -4,13 +4,13 @@ import { SECTION_URLS } from '~/constants/constants';
 const { path, query } = useRoute();
 const isDev = useRuntimeConfig().public.isDev;
 
-console.debug('Path:', path)
-console.debug('Dev mode:', isDev)
+console.log('Path:', path)
+console.log('Dev mode:', isDev)
 
-if (path === '/') {
-  // await navigateTo({
-  //   path: `/${SECTION_URLS.gettingStarted}${isDev ? '-wip' : ''}`,
-  //   query,
-  // });
+if (path === '/' && isDev) {
+  await navigateTo({
+    path: `/${SECTION_URLS.gettingStarted}${isDev ? '-wip' : ''}`,
+    query,
+  });
 }
 </script>
