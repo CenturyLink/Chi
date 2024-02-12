c2f5983f79f6bf0ba7ea74b4e63faece0282c9d4
wojciechwlodarczyklumen <139760197+wojciechwlodarczyklumen@users.noreply.github.com>
2024-02-12 11:51:59 +0100

    [DPEDE-1282] Adjusted code in generateContent function (#1010)

--- src/documentation/utilities/utilities.ts
+++ src/documentation/utilities/utilities.ts
@@ -22,15 +22,13 @@ export const generateUtilitiesTextContent = (
 ): string => {
   switch (column.key) {
     case 'class':
-      return `<td class="-w--30 -w-md--20" width="20%">
-            <code>-text--${content.name}</code>
+      return `<code>-text--${content.name}</code>
             <dl class="-text -text--xs -mt--2">
               <dt class="-text--bold">Font-size:</dt>
               <dt>${content.value[0]}</dt>
               <dt class="-text--bold -mt--1">Line-height:</dt>
               <dt>${content.value[1]}</dt>
-            </dl>
-          </td>`;
+            </dl>`;
     case 'value':
       return `<p class="-text--${content.name} -px--1">The quick brown fox jumps over the lazy dog.</p>`;
     default:
