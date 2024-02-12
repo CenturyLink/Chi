ec41fb4623094ab5f34f39270c5aa783ea8d4341
ASanchLumen <Alejandro.Pineda@lumen.com>
2024-02-12 11:15:48 +0100

    [DPEDE-1327] remove chi-vue plugin

--- src/documentation/plugins/chi-vue.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Use chi-vue components
- */
-// import { ChiVue } from "@centurylink/chi-vue";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // console.log('ChiVue plugin:', ChiVue)
-  // nuxtApp.vueApp.use(ChiVue)
-});
