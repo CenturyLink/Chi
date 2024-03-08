23233f5b8eedaeefaa1e3648d4184ff943bc31c8
xft-dev <xft.lumn@gmail.com>
2024-03-08 08:35:47 +0100

    [DPEDE-1413] Add mutation observer to app layout web component

--- src/custom-elements/src/components/popover/popover.tsx
+++ src/custom-elements/src/components/popover/popover.tsx
@@ -5,7 +5,6 @@ import { Drag } from '../../utils/Drag';
 import { ANIMATION_DURATION, CLASSES, ESCAPE_KEYCODE } from '../../constants/constants';
 import Popper, { Placement } from 'popper.js';
 import { POPOVER_CLASSES } from '../../constants/classes';
-import { addMutationObserver } from '../../utils/mutationObserver';
 
 @Component({
   tag: 'chi-popover',
@@ -94,6 +93,7 @@ export class Popover {
   private _closePreventedTimeout: number;
   private _documentClickHandler: () => void;
   private _documentKeyHandler: (event: KeyboardEvent) => void;
+  private mutationObserver;
   private _drag: Drag;
 
   @Watch('position')
@@ -335,15 +335,24 @@ export class Popover {
   }
 
   connectedCallback() {
-    addMutationObserver.call(this, () => {
-        this.popoverTitle = this.el.title;
-      },
-      { attributes: true, 
-        attributeOldValue: true, 
-        attributeFilter: ['title'], 
-        childList: true 
-      }
-    );
+    const observerTarget = this.el;
+    const mutationObserverConfig = {
+      attributes: true,
+      attributeOldValue: true,
+      attributeFilter: ['title'],
+    };
+
+    if (!this.mutationObserver) {
+      const subscriberCallback = (mutations) => {
+        mutations.forEach((mutation) => {
+          this.popoverTitle = mutation.target.title;
+        });
+      };
+
+      this.mutationObserver = new MutationObserver(subscriberCallback);
+    }
+
+    this.mutationObserver.observe(observerTarget, mutationObserverConfig);
   }
 
   componentWillLoad(): void {
@@ -395,6 +404,7 @@ export class Popover {
     this._componentLoaded = false;
     document.removeEventListener('click', this._documentClickHandler);
     document.removeEventListener('keyup', this._documentKeyHandler);
+    this.mutationObserver.disconnect();
   }
 
   componentDidUpdate(): void {
