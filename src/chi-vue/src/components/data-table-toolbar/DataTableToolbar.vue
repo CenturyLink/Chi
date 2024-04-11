961db65c43a8ba17272fa82a9f0c1d967828f3fe
xft-dev <xft.lumn@gmail.com>
2024-04-11 06:00:06 +0200

    [DPEDE-1683] Add prop to make column customization btn visible in toolbar

--- src/chi-vue/src/components/data-table-toolbar/DataTableToolbar.vue
+++ src/chi-vue/src/components/data-table-toolbar/DataTableToolbar.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
 import { DATA_TABLE_CLASSES } from '@/constants/classes';
+import ColumnCustomization from '../../components/column-customization/ColumnCustomization';
+import { DataTableColumnsData } from '@/constants/types';
+
+const props = defineProps<{
+  columnCustomizationBtn?: boolean;
+  columnsData?: DataTableColumnsData | {};
+}>();
 </script>
 
 <template>
@@ -10,6 +17,7 @@ import { DATA_TABLE_CLASSES } from '@/constants/classes';
       </div>
       <div :class="`${DATA_TABLE_CLASSES.TOOLBAR}__end`">
         <slot name="end"></slot>
+        <ColumnCustomization v-if="columnCustomizationBtn" :columnsData="columnsData" />
       </div>
     </div>
   </div>
