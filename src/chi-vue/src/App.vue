db09e649565e7cab18fb99cf3a8672e812e2a42b
Emerson Pereira <Emerson.Pereira@lumen.com>
2024-05-13 11:07:25 +0100

    [DPEDE-1820] Create data table inside modal example

--- src/chi-vue/src/App.vue
+++ src/chi-vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="-p--4">
     <h1>Chi Vue 3</h1>
+    <DataTableModalView />
     <DataTableClientView />
     <DataTableServerView />
     <ExpansionPanelView />
@@ -17,6 +18,7 @@
 <script lang="ts" setup>
 import CheckboxView from './views/CheckboxView.vue';
 import DataTableClientView from './views/DataTable/ClientSide/DataTableClientView.vue';
+import DataTableModalView from './views/DataTableModalView.vue';
 import DataTableServerView from './views/DataTable/ServerSide/DataTableServerView.vue';
 import DrawerView from './views/DrawerView.vue';
 import ExpansionPanelView from './views/ExpansionPanelView.vue';
