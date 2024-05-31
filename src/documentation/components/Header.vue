bd3b7ee2c4fa83db1f4e674e3862f82f23d8fec3
ASanchLumen <Alejandro.Pineda@lumen.com>
2024-05-31 17:03:10 +0200

    [DPEDE-1976] Add theme query param to docs sidenav in both website and vue docs

--- src/documentation/components/Header.vue
+++ src/documentation/components/Header.vue
@@ -1,159 +1,165 @@
 <template lang="pug">
-  header.chi-header.docs-header.-z--20
-    #version-check.-w--100
-    ClientOnly
-      component(is="style" type='text/css').
-        .theme-lumen a.theme-trigger-lumen.chi-dropdown__menu-item,
-        .theme-centurylink a.theme-trigger-centurylink.chi-dropdown__menu-item,
-        .theme-portal a.theme-trigger-portal.chi-dropdown__menu-item {
-          background-color: #e6f0ff;
-          color: #0047bb;
-          font-weight: 600;
+header.chi-header.docs-header.-z--20
+  #version-check.-w--100
+  ClientOnly
+    component(is="style" type='text/css').
+      .theme-lumen a.theme-trigger-lumen.chi-dropdown__menu-item,
+      .theme-centurylink a.theme-trigger-centurylink.chi-dropdown__menu-item,
+      .theme-portal a.theme-trigger-portal.chi-dropdown__menu-item {
+        background-color: #e6f0ff;
+        color: #0047bb;
+        font-weight: 600;
+      }
+      .theme-lumenrebrand24 a.theme-trigger-lumenrebrand24.chi-dropdown__menu-item {
+        background-color: #fef6f0;
+        color: #007981;
+        font-weight: 600;
+      }
+      .theme-portalrebrand24 a.theme-trigger-portalrebrand24.chi-dropdown__menu-item {
+        background-color: #fef6f0;
+        color: #b35009;
+        font-weight: 600;
+      }
+      .theme-brightspeed a.theme-trigger-brightspeed.chi-dropdown__menu-item {
+        background-color: #fff1bf;
+        color: #000;
+        font-weight: 600;
+      }
+      .chi-brand {
+        margin: 0;
+      }
+      @media only screen and (min-width: 992px) {
+        .chi .docs-header__logo .chi-brand {
+          margin: 0 3rem 0 0;
         }
-        .theme-lumenrebrand24 a.theme-trigger-lumenrebrand24.chi-dropdown__menu-item {
-          background-color: #fef6f0;
-          color: #007981;
-          font-weight: 600;
-        }
-        .theme-portalrebrand24 a.theme-trigger-portalrebrand24.chi-dropdown__menu-item {
-          background-color: #fef6f0;
-          color: #b35009;
-          font-weight: 600;
-        }
-        .theme-brightspeed a.theme-trigger-brightspeed.chi-dropdown__menu-item {
-          background-color: #fff1bf;
-          color: #000;
-          font-weight: 600;
-        }
-        .chi-brand {
-          margin: 0;
-        }
-        @media only screen and (min-width: 992px) {
-          .chi .docs-header__logo .chi-brand {
-            margin: 0 3rem 0 0;
-          }
-        }
+      }
+      .chi .docs-header__logo .chi-brand.-centurylink {
+        width: 6.09375rem;
+      }
+      @media only screen and (min-width: 768px) {
         .chi .docs-header__logo .chi-brand.-centurylink {
-          width: 6.09375rem;
+          margin: 0 3rem 0 0;
+          width: 9.75rem;
         }
-        @media only screen and (min-width: 768px) {
-          .chi .docs-header__logo .chi-brand.-centurylink {
-            margin: 0 3rem 0 0;
-            width: 9.75rem;
-          }
-        }
+      }
+      .chi .docs-header__logo .chi-brand.-centurylink > img,
+      .chi .docs-header__logo .chi-brand.-centurylink > svg {
+        height: 1.25rem;
+        width: 6.09375rem;
+      }
+      @media only screen and (min-width: 768px) {
         .chi .docs-header__logo .chi-brand.-centurylink > img,
         .chi .docs-header__logo .chi-brand.-centurylink > svg {
-          height: 1.25rem;
-          width: 6.09375rem;
+          height: 2rem;
+          width: 9.75rem;
         }
-        @media only screen and (min-width: 768px) {
-          .chi .docs-header__logo .chi-brand.-centurylink > img,
-          .chi .docs-header__logo .chi-brand.-centurylink > svg {
-            height: 2rem;
-            width: 9.75rem;
-          }
-        }
+      }
+      .chi .docs-header__logo .chi-brand.-lumen {
+        width: 7.1875rem;
+      }
+      @media only screen and (min-width: 768px) {
         .chi .docs-header__logo .chi-brand.-lumen {
-          width: 7.1875rem;
+          margin: 0 1rem 0 0;
+          width: 8.75rem;
         }
-        @media only screen and (min-width: 768px) {
-          .chi .docs-header__logo .chi-brand.-lumen {
-            margin: 0 1rem 0 0;
-            width: 8.75rem;
-          }
+      }
+      @media only screen and (min-width: 992px) {
+        .chi .docs-header__logo .chi-brand.-lumen {
+          margin: 0 3rem 0 0;
         }
-        @media only screen and (min-width: 992px) {
-          .chi .docs-header__logo .chi-brand.-lumen {
-            margin: 0 3rem 0 0;
-          }
-        }
+      }
+      .chi .docs-header__logo .chi-brand.-lumen > img,
+      .chi .docs-header__logo .chi-brand.-lumen > svg {
+        height: 1rem;
+        width: 7.1875rem;
+      }
+      @media only screen and (min-width: 768px) {
         .chi .docs-header__logo .chi-brand.-lumen > img,
         .chi .docs-header__logo .chi-brand.-lumen > svg {
-          height: 1rem;
-          width: 7.1875rem;
-        }
-        @media only screen and (min-width: 768px) {
-          .chi .docs-header__logo .chi-brand.-lumen > img,
-          .chi .docs-header__logo .chi-brand.-lumen > svg {
-            height: 1.25rem;
-            width: 8.75rem;
-          }
-        }
-        .chi.theme-portal .docs-header .chi-header__content .chi-header__start .-chi-search .chi-input {
-          height: 2.5rem !important;
-        }
-        .chi.theme-portal .docs-header .chi-header__content .chi-header__start .-chi-search .chi-icon {
-          color: #0262b9;
-          font-size: 1.25rem;
           height: 1.25rem;
-          line-height: 1.25rem;
-          top: 0.625rem;
-          width: 1.25rem;
+          width: 8.75rem;
         }
-        .chi.theme-portal .example .example-tabs .chi-tabs,
-        .chi.theme-brightspeed .example .example-tabs .chi-tabs {
-          height: 3rem;
-        }
-        .chi.theme-portal .example .example-tabs .chi-tabs > li > a,
-        .chi.theme-brightspeed .example .example-tabs .chi-tabs > li > a {
-          line-height: 1.5rem;
-          padding: 0.75rem 0;
-        }
-    nav.chi-header__content
-      #header-brand.chi-header__brand
-        button#drawer-trigger-1.chi-button.-icon.-flat.-mr-md--2.-d--flex.-d-lg--none.chi-drawer__trigger(
-          data-target='#drawer-sidenav',
-          aria-label='Toggle documentation navigation'
-        )
-          .chi-button__content
-            i.chi-icon.-sm--2.icon-menu(aria-hidden='true')
-        <a class="docs-header__logo" aria-label='Portal' :href="baseUrl">
-          chi-brand#header-logo(logo='lumen')
-        </a>
-        span.chi-header__title.-d--none.-d-lg--flex Chi Design System
-      .chi-header__start
-        #docsearch.-chi-search
-        component(is="script" v-html="algoliaScript")
-      .chi-header__end
-        .chi-dropdown
-          button.chi-button.-flat.-px--1.chi-dropdown__trigger.-animate(
-            data-position='bottom-end',
-            aria-label='Toggle Chi version'
-            ref='versionDropdown'
-          ) v{{ACTIVE_CHI_VERSION}}
-          .chi-dropdown__menu.-mh--400.-overflow--auto
-            a.chi-dropdown__menu-item(
-              :class="version === ACTIVE_CHI_VERSION ? '-active' : ''"
-              v-for="(version, index) in CHI_VISIBLE_VERSION"
-              :key="index"
-              :href="`https://lib.lumen.com/chi/${version}`"
-            )
-              | v{{version}}
-        .chi-dropdown
-          button#support.chi-button.-flat.-px--1.-ml--1.chi-dropdown__trigger.-animate(
-            data-position='bottom-end'
-            ref='supportDropdown'
-          ) Support
-          .chi-dropdown__menu
-            a#contact.chi-dropdown__menu-item(
-              href='mailto:chi.support@centurylink.com'
-            ) Email Support
-            a#teams.chi-dropdown__menu-item(
-              href='https://teams.microsoft.com/l/channel/19%253aa08fdfde482543ea80f81ce484cea21b%2540thread.skype/CHI?groupId=8631295d-43fc-4e40-93e5-dc8c9ab4d127&tenantId=72b17115-9915-42c0-9f1b-4f98e5a4bcd2',
-              target='_blank',
-              rel='noopener'
-            ) Talk #Chi on Microsoft Teams
-        a.chi-button.-icon.-flat.-ml--1(
-          href='https://github.com/CenturyLink/Chi',
-          rel='noopener',
-          aria-label='GitHub',
-          data-tooltip='GitHub',
-          aria-describedby='chi-tooltip-1'
-          ref="githubButtonTooltip"
-        )
-          .chi-button__content
-            i.chi-icon.icon-logo-github(aria-hidden='true')
+      }
+      .chi.theme-portal .docs-header .chi-header__content .chi-header__start .-chi-search .chi-input {
+        height: 2.5rem !important;
+      }
+      .chi.theme-portal .docs-header .chi-header__content .chi-header__start .-chi-search .chi-icon {
+        color: #0262b9;
+        font-size: 1.25rem;
+        height: 1.25rem;
+        line-height: 1.25rem;
+        top: 0.625rem;
+        width: 1.25rem;
+      }
+      .chi.theme-portal .example .example-tabs .chi-tabs,
+      .chi.theme-brightspeed .example .example-tabs .chi-tabs {
+        height: 3rem;
+      }
+      .chi.theme-portal .example .example-tabs .chi-tabs > li > a,
+      .chi.theme-brightspeed .example .example-tabs .chi-tabs > li > a {
+        line-height: 1.5rem;
+        padding: 0.75rem 0;
+      }
+  nav.chi-header__content
+    #header-brand.chi-header__brand
+      button.chi-button.-icon.-flat.-mr-md--2.-d--flex.-d-lg--none.chi-drawer__trigger(
+        @click="toggleSidenavDrawer",
+        ref="drawerSidenavTrigger"
+        aria-label='Toggle documentation navigation'
+      )
+        .chi-button__content
+          i.chi-icon.-sm--2.icon-menu(aria-hidden='true')
+      <a class="docs-header__logo" aria-label='Portal' :href="baseUrl">
+        chi-brand#header-logo(logo='lumen')
+      </a>
+      span.chi-header__title.-d--none.-d-lg--flex Chi Design System
+    .chi-header__start
+      #docsearch.-chi-search
+      component(is="script" v-html="algoliaScript")
+    .chi-header__end
+      .chi-dropdown
+        button.chi-button.-flat.-px--1.chi-dropdown__trigger.-animate(
+          data-position='bottom-end',
+          aria-label='Toggle Chi version'
+          ref='versionDropdown'
+        ) v{{ACTIVE_CHI_VERSION}}
+        .chi-dropdown__menu.-mh--400.-overflow--auto
+          a.chi-dropdown__menu-item(
+            :class="version === ACTIVE_CHI_VERSION ? '-active' : ''"
+            v-for="(version, index) in CHI_VISIBLE_VERSION"
+            :key="index"
+            :href="`https://lib.lumen.com/chi/${version}`"
+          )
+            | v{{version}}
+      .chi-dropdown
+        button#support.chi-button.-flat.-px--1.-ml--1.chi-dropdown__trigger.-animate(
+          data-position='bottom-end'
+          ref='supportDropdown'
+        ) Support
+        .chi-dropdown__menu
+          a#contact.chi-dropdown__menu-item(
+            href='mailto:chi.support@centurylink.com'
+          ) Email Support
+          a#teams.chi-dropdown__menu-item(
+            href='https://teams.microsoft.com/l/channel/19%253aa08fdfde482543ea80f81ce484cea21b%2540thread.skype/CHI?groupId=8631295d-43fc-4e40-93e5-dc8c9ab4d127&tenantId=72b17115-9915-42c0-9f1b-4f98e5a4bcd2',
+            target='_blank',
+            rel='noopener'
+          ) Talk #Chi on Microsoft Teams
+      a.chi-button.-icon.-flat.-ml--1(
+        href='https://github.com/CenturyLink/Chi',
+        rel='noopener',
+        aria-label='GitHub',
+        data-tooltip='GitHub',
+        aria-describedby='chi-tooltip-1'
+        ref="githubButtonTooltip"
+      )
+        .chi-button__content
+          i.chi-icon.icon-logo-github(aria-hidden='true')
+
+ClientOnly
+  chi-drawer.-menu.-position--absolute(ref="drawerSidenav", position="left", no-header, backdrop)
+    .-py--2
+      <Navigation />
 </template>
 
 <script lang="ts">
@@ -173,11 +179,13 @@ export default class Header extends Vue {
   versionDropdown: any;
   supportDropdown: any;
   githubButtonTooltip: any;
+  drawerSidenav: any;
 
   mounted() {
     const versionDropdown = this.$refs.versionDropdown as HTMLElement;
     const supportDropdown = this.$refs.supportDropdown as HTMLElement;
     const githubButtonTooltip = this.$refs.githubButtonTooltip as HTMLElement;
+    this.drawerSidenav = this.$refs.drawerSidenav;
 
     if (versionDropdown) {
       this.versionDropdown = chi.dropdown(versionDropdown);
@@ -202,6 +210,10 @@ export default class Header extends Vue {
     if (this.githubButtonTooltip) {
       this.githubButtonTooltip.dispose();
     }
+  }
+
+  toggleSidenavDrawer() {
+    this.$refs.drawerSidenav.toggle();
   }
 }
 </script>
--- src/documentation/components/Navigation.vue
+++ src/documentation/components/Navigation.vue
@@ -1,51 +1,49 @@
 <template>
-  <nav class="docs-sidenav">
-    <div class="-px--2 -pt--2 -pb--1">
-      <ThemeSwitcher />
-    </div>
-    <ul class="chi-tabs -vertical -lg -animated">
-      <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.gettingStarted) }">
-        <div class="chi-collection__title">Getting Started</div>
-        <DocumentationLinkList :links="NAVIGATION_GETTING_STARTED_ITEMS" />
-      </li>
-      <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.foundations) }">
-        <div class="chi-collection__title">Foundations</div>
-        <ul class="chi-tabs__subtabs">
-          <li>
-            <div class="chi-accordion accessibility-accordion" ref="accessibilityAccordion">
-              <div
-                :class="{
-                  'chi-accordion__item -bg--none': true,
-                  '-expanded': $route.fullPath.includes('/accessibility/'),
-                }"
-              >
-                <button class="chi-accordion__trigger">
-                  <div class="chi-accordion__title">Accessibility</div>
-                  <i class="chi-icon icon-chevron-down -xs" aria-hidden="true"></i>
-                </button>
-                <div class="chi-accordion__content -p--0">
-                  <DocumentationLinkList :links="NAVIGATION_FOUNDATIONS_ACCESIBILITY" />
-                </div>
-              </div>
+<div class="-px--2 -pt--2 -pb--1">
+  <ThemeSwitcher />
+</div>
+<ul class="chi-tabs -vertical -lg -animated">
+  <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.gettingStarted) }">
+    <div class="chi-collection__title">Getting Started</div>
+    <DocumentationLinkList :links="NAVIGATION_GETTING_STARTED_ITEMS" />
+  </li>
+  <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.foundations) }">
+    <div class="chi-collection__title">Foundations</div>
+    <ul class="chi-tabs__subtabs">
+      <li>
+        <div class="chi-accordion accessibility-accordion" ref="accessibilityAccordion">
+          <div
+            :class="{
+              'chi-accordion__item -bg--none': true,
+              '-expanded': $route.fullPath.includes('/accessibility/'),
+            }"
+          >
+            <button class="chi-accordion__trigger">
+              <div class="chi-accordion__title">Accessibility</div>
+              <i class="chi-icon icon-chevron-down -xs" aria-hidden="true"></i>
+            </button>
+            <div class="chi-accordion__content -p--0">
+              <DocumentationLinkList :links="NAVIGATION_FOUNDATIONS_ACCESIBILITY" />
             </div>
-          </li>
-          <DocumentationLinkList :links="NAVIGATION_FOUNDATIONS" />
-        </ul>
+          </div>
+        </div>
       </li>
-      <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.components) }">
-        <div class="chi-collection__title">Components</div>
-        <DocumentationLinkList :links="NAVIGATION_COMPONENTS_ITEMS" />
-      </li>
-      <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.utilities) }">
-        <div class="chi-collection__title">Utilities</div>
-        <DocumentationLinkList :links="NAVIGATION_UTILITIES_ITEMS" />
-      </li>
-      <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.templates) }">
-        <div class="chi-collection__title">Templates</div>
-        <DocumentationLinkList :links="NAVIGATION_TEMPLATE_ITEMS" />
-      </li>
+      <DocumentationLinkList :links="NAVIGATION_FOUNDATIONS" />
     </ul>
-  </nav>
+  </li>
+  <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.components) }">
+    <div class="chi-collection__title">Components</div>
+    <DocumentationLinkList :links="NAVIGATION_COMPONENTS_ITEMS" />
+  </li>
+  <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.utilities) }">
+    <div class="chi-collection__title">Utilities</div>
+    <DocumentationLinkList :links="NAVIGATION_UTILITIES_ITEMS" />
+  </li>
+  <li :class="{ '-active': $route.fullPath.includes(SECTION_URLS.templates) }">
+    <div class="chi-collection__title">Templates</div>
+    <DocumentationLinkList :links="NAVIGATION_TEMPLATE_ITEMS" />
+  </li>
+</ul>
 </template>
 
 <script lang="ts">
