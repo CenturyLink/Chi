e19aa6f6b0ab1fe91400081e63d5d016981de0c6
Jose Luis Lopez Ruiz <jose.luis@ctl.io>
2024-07-28 14:53:06 +0200

    [DPEDE-2324] Prepare Chi 6.0.0 release

--- src/chi-vue/src/build/plugin/constants.ts
+++ src/chi-vue/src/build/plugin/constants.ts
@@ -1,2 +1,2 @@
-export const CHI_VERSION = '5.78.0';
+export const CHI_VERSION = '6.0.0';
 export const CHI_LIB_LUMEN = 'https://lib.lumen.com/chi';
--- src/website/assets/scripts/globalConfigs.js
+++ src/website/assets/scripts/globalConfigs.js
@@ -1,1 +1,1 @@
-window.chiCurrentVersion="5.78.0";
+window.chiCurrentVersion="6.0.0";
