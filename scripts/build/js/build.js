992ef65e3889e8c5e1f880797f96171210f3ecb7
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2024-11-06 19:25:29 +0100

    [DPEDE-2789] Test pipeline 2

--- scripts/build/js/build.js
+++ scripts/build/js/build.js
@@ -1,13 +1,11 @@
 import ora from 'ora';
 import { execSync } from 'child_process';
 
-const isWindows = process.platform === 'win32';
-
 async function buildJS() {
   const spinnerIIFE = ora(`[CHI]: Building JavaScript (IIFE)`).start();
 
   try {
-    execSync(`${isWindows ? 'cross-env' : ''} JS=iife vite build --c vite-js.config.ts`, { stdio: 'ignore' });
+    execSync(`cross-env JS=iife vite build --c vite-js.config.ts`, { stdio: 'ignore' });
     spinnerIIFE.succeed(`[CHI]: JavaScript (IIFE) build completed successfully`);
   } catch (error) {
     spinnerIIFE.fail(`[CHI]: Error during JavaScript (IIFE) build: ${error.message}`);
@@ -19,7 +17,7 @@ async function buildJS() {
   const spinnerAMD = ora(`[CHI]: Building JavaScript (AMD)`).start();
 
   try {
-    execSync(`${isWindows ? 'cross-env' : ''} JS=amd vite build --c vite-js.config.ts`, { stdio: 'ignore' });
+    execSync(`cross-env JS=amd vite build --c vite-js.config.ts`, { stdio: 'ignore' });
     spinnerAMD.succeed(`[CHI]: JavaScript (AMD) build completed successfully`);
   } catch (error) {
     spinnerAMD.fail(`[CHI]: Error during JavaScript (AMD) build: ${error.message}`);
