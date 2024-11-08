28c185fe672b330ee1ad98ba9f4583ccd99ce992
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2024-11-08 13:13:20 +0100

    [DPEDE-2789] Visual tests fixed

--- scripts/tests/approve.js
+++ scripts/tests/approve.js
@@ -1,9 +1,33 @@
 import backstop from 'backstopjs';
+import fs from 'fs';
+import path from 'path';
 
 (async () => {
   try {
+    const directories = [
+      {
+        source: 'reports/html_report/responsive/bitmaps_reference',
+        destination: 'backstop_data/bitmaps_reference/responsive'
+      },
+      {
+        source: 'reports/html_report/non_responsive/bitmaps_reference',
+        destination: 'backstop_data/bitmaps_reference/non_responsive'
+      }
+    ];
+
     await backstop('approve', { config: 'backstop-responsive.json' });
     await backstop('approve', { config: 'backstop-non-responsive.json' });
+
+    const moveFiles = (sourceDir, destDir) => {
+      fs.readdirSync(sourceDir).forEach(file => {
+        const sourcePath = path.join(sourceDir, file);
+        const destPath = path.join(destDir, file);
+  
+        fs.copyFileSync(sourcePath, destPath);
+      });
+    };
+
+    directories.forEach(({ source, destination }) => moveFiles(source, destination));
   } catch (error) {
     console.error('[CHI]: Error approving tests: ', error);
     process.exit(1);
--- scripts/tests/visualTests.js
+++ scripts/tests/visualTests.js
@@ -1,10 +1,12 @@
 import backstop from 'backstopjs';
 
+const local = !process.env.JENKINS_HOME;
+
 (async () => {
   try {
-    await backstop('test', { config: 'backstop-responsive.json' });
-    await backstop('test', { config: 'backstop-non-responsive.json' });
-    await backstop('test', { config: 'backstop-non-responsive-ce.json' });
+    await backstop('test', { docker: local, config: 'backstop-responsive.json' });
+    await backstop('test', { docker: local, config: 'backstop-non-responsive.json' });
+    await backstop('test', { docker: local, config: 'backstop-non-responsive-ce.json' });
   } catch (error) {
     console.error('[CHI]: Error executing tests: ', error);
     process.exit(1);
