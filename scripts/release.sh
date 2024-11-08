d62a67ae86798c9a49c6b827265939fa48fd4e9a
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2024-11-08 17:20:21 +0100

    [DPEDE-2789] Test release 6.12.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ NEW_VERSION=$1
 
 export LC_ALL=C
 
-grep -rl "$CURRENT_VERSION" --exclude-dir={lib,src,dist,node_modules,tests,backstop_data,reports} --exclude={package-lock.json,README.md} . | while read -r file; do
+grep -rl "$CURRENT_VERSION" --exclude-dir={lib,src,dist,node_modules,tests,backstop_data,reports} --exclude={package-lock.json,CHANGELOG.md} . | while read -r file; do
   sed -i "" "s|$CURRENT_VERSION|$NEW_VERSION|g" "$file"
 done
 
