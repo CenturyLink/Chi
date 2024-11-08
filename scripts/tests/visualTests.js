50302148500d6e2b8105457907c5838de60c2ffc
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2024-11-08 13:30:00 +0100

    [DPEDE-2789] Visual tests fixed 2

--- scripts/tests/visualTests.js
+++ scripts/tests/visualTests.js
@@ -1,12 +1,33 @@
 import backstop from 'backstopjs';
+import fs from 'fs';
 
 const local = !process.env.JENKINS_HOME;
+const baseUrl = local ? 'http://host.docker.internal:8000' : 'http://localhost:8000';
+
+const modifyBackstopConfig = (configFile) => {
+  const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+
+  config.scenarios.forEach(scenario => {
+    scenario.url = scenario.url.replace('http://host.docker.internal:8000', baseUrl);
+  });
+
+  const modifiedConfigFile = `modified-${configFile}`;
+
+  fs.writeFileSync(modifiedConfigFile, JSON.stringify(config, null, 2));
+
+  return modifiedConfigFile;
+}
 
 (async () => {
   try {
-    await backstop('test', { docker: local, config: 'backstop-responsive.json' });
-    await backstop('test', { docker: local, config: 'backstop-non-responsive.json' });
-    await backstop('test', { docker: local, config: 'backstop-non-responsive-ce.json' });
+    const configFiles = ['backstop-responsive.json', 'backstop-non-responsive.json', 'backstop-non-responsive-ce.json'];
+    
+    for (const configFile of configFiles) {
+      const modifiedConfigFile = modifyBackstopConfig(configFile);
+      
+      await backstop('test', { docker: local, config: modifiedConfigFile });
+      fs.unlinkSync(modifiedConfigFile);
+    }
   } catch (error) {
     console.error('[CHI]: Error executing tests: ', error);
     process.exit(1);
