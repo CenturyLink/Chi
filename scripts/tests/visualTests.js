401d2d3677c7efcae6cc1ea35c4c435a343ada13
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2025-02-17 18:07:13 +0100

    [DPEDE-3381] Added CE tests

--- scripts/tests/visualTests.js
+++ scripts/tests/visualTests.js
@@ -2,10 +2,10 @@ import backstop from 'backstopjs';
 
 (async () => {
   try {
-    const configFiles = ['backstop-responsive.json', 'backstop-non-responsive.json'];
+    const configFiles = ['backstop-responsive.json', 'backstop-non-responsive.json', 'backstop-non-responsive-ce.json'];
     
     for (const configFile of configFiles) {
-      await backstop('test', {  docker: true, config: configFile });
+      await backstop('test', { docker: true, config: configFile });
     }
   } catch (error) {
     console.error('[CHI]: Error executing tests: ', error);
