ebd9b5033871aec591691d3e097a651215a67c18
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2025-03-06 14:05:30 +0100

    [DPEDE-3646] Consume Chi documentation npm package in Chi

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -3,6 +3,10 @@ set -e
 
 SECONDS=0
 
+CHI_DOCUMENTATION="./node_modules/@centurylink/chi-documentation/.output/public"
+CHI_CE="./node_modules/@centurylink/chi-custom-elements"
+CHI_VUE_UMD="./node_modules/@centurylink/chi-vue/umd/index.umd.js"
+
 # Clean dist folder
 if [ -d "dist" ]; then
     rm -rf "dist"
@@ -12,6 +16,11 @@ fi
 if [ -f "sri.json" ]; then
     rm -f "sri.json"
 fi
+
+# TODO: 1) Adjust chi-documentation pipeline to generate just necessary files
+# TODO: 2) Adjust JS script to don't remove chi.js and index.umd.js from chi-vue (revert changes)
+# TODO: 3) Remove this line and uncomment copy documentation at the end
+bash ./scripts/build/utils/copyFiles.sh $CHI_DOCUMENTATION dist
 
 # Build CSS
 node ./scripts/build/css/build.js
@@ -25,11 +34,6 @@ node ./scripts/build/utils/buildIcons.js
 node ./scripts/build/utils/buildSprites.js
 bash ./scripts/build/utils/copyFiles.sh ./assets dist/assets
 
-# Copy lib
-bash ./scripts/build/utils/copyFiles.sh ./lib/chi-custom-elements dist/js/ce
-bash ./scripts/build/utils/copyFiles.sh ./lib/chi-documentation dist
-bash ./scripts/build/utils/copyFile.sh ./lib/chi-vue/index.umd.js dist/chi-vue/umd
-
 # Build SRI
 node ./scripts/build/utils/buildSri.js
 bash ./scripts/build/utils/copyFile.sh sri.json dist/sri.json
@@ -38,9 +42,13 @@ bash ./scripts/build/utils/copyFile.sh sri.json dist/sri.json
 bash ./scripts/build/utils/buildBoilerplates.sh
 
 # Build tests
-
 node ./scripts/build/utils/buildTests.js
 bash ./scripts/build/utils/copyFiles.sh ./tests/styles dist/tests
+
+# Copy assets from dependencies: chi-documentation, chi-vue, chi-custom-elements
+# bash ./scripts/build/utils/copyFiles.sh $CHI_DOCUMENTATION dist
+bash ./scripts/build/utils/copyFile.sh $CHI_VUE_UMD dist/chi-vue/umd
+bash ./scripts/build/utils/copyFiles.sh ./lib/chi-custom-elements dist/js/ce
 
 minutes=$((SECONDS / 60))
 seconds=$((SECONDS % 60))
