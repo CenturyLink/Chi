eb99d858f6e9f0058b419582899746ee9b9848c7
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2025-04-11 12:58:13 +0200

    [DPEDE-3847] Improve SHAs generation in Chi repository

--- server.cjs
+++ server.cjs
@@ -9,21 +9,21 @@ const basePath = `/chi/${packageJson.version}`;
 
 app.use(basePath, express.static(path.join(__dirname, 'dist')));
 
-app.get(`${basePath}/*`, (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.get(`${basePath}/tests/*`, (req, res) => {
+
+app.get(new RegExp(`${basePath}/tests/(.*)`), (req, res) => {
   const requestedPath = req.path.replace(`${basePath}/tests`, '');
   const testPath = path.join(__dirname, 'dist', 'tests', requestedPath);
-
+  
   if (fs.existsSync(testPath) && fs.lstatSync(testPath).isFile()) {
     res.sendFile(testPath);
   } else {
     res.sendFile(path.join(__dirname, 'dist', 'tests', 'index.html'));
   }
 });
-
 
 app.listen(port, () => {
   console.log(`[CHI]: Started on http://localhost:${port}${basePath}`);
