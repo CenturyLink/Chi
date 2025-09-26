5873182ee0047086b3c326a036f80086dca66f42
Alejandro Sanchez <Alejandro.Pineda@lumen.com>
2025-09-26 10:14:25 +0200

    [DPEDE-4843] Set themes to test to lumen, add report to jenkins

--- scripts/tests/backstopConfig.sh
+++ scripts/tests/backstopConfig.sh
@@ -1,6 +1,7 @@
 # to add new theme, execute the command backstop('reference')
+# needs to add the theme report also in jenkinsfile
 # export THEMES_TO_TEST="lumen portal centurylink colt brightspeed test"
-export THEMES_TO_TEST="portal"
+export THEMES_TO_TEST="lumen"
 
 set_backstop_config () {
   find . -maxdepth 1 -name 'backstop*.json' -not -name '*_*' | while read -r file
