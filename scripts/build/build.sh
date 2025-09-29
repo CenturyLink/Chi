a1990d94c5064a6ee79d272c00ef66403ca0d3ff
ASanchLumen <148064753+ASanchLumen@users.noreply.github.com>
2025-09-29 16:13:22 +0200

    [DPEDE-4874] Remplace changelog for documentation changelog (#1752)
    * [DPEDE-4874] Reemplace changelog for documentation changelog
    
    * [DPEDE-4874] Ignore changelog in chi repo

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -3,7 +3,7 @@ set -e
 
 SECONDS=0
 
-CHI_DOCUMENTATION="./node_modules/@centurylink/chi-documentation/.output/public"
+CHI_DOCUMENTATION="./node_modules/@centurylink/chi-documentation"
 CHI_CE="./node_modules/@centurylink/chi-custom-elements"
 CHI_VUE_UMD="./node_modules/@centurylink/chi-vue/umd/index.umd.js"
 
@@ -39,7 +39,8 @@ node ./scripts/build/utils/buildTests.js
 bash ./scripts/build/utils/copyFiles.sh ./tests/styles dist/tests
 
 # Copy assets from dependencies: chi-documentation, chi-vue, chi-custom-elements
-bash ./scripts/build/utils/copyFiles.sh $CHI_DOCUMENTATION dist
+bash ./scripts/build/utils/copyFiles.sh $CHI_DOCUMENTATION/.output/public dist
+bash ./scripts/build/utils/copyFile.sh $CHI_DOCUMENTATION/CHANGELOG.md dist
 bash ./scripts/build/utils/copyFile.sh $CHI_VUE_UMD dist/chi-vue/umd
 bash ./scripts/build/utils/copyFiles.sh "$CHI_CE/dist" dist/js/ce
 
