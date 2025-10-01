2c35f51ded3681dd0dc9540bdb3245500d72c2e5
Alejandro Sanchez <Alejandro.Pineda@lumen.com>
2025-10-01 10:15:05 +0200

    [DPEDE-4843] Run tests from bash to have control over which fails

--- scripts/tests/visualTests.js
+++ scripts/tests/visualTests.js
@@ -1,18 +1,10 @@
 import backstop from 'backstopjs';
 
-(async (theme) => {
-  const configFiles = [
-    `backstop-responsive_${theme}.json`, 
-    `backstop-non-responsive_${theme}.json`, 
-    `backstop-non-responsive-ce_${theme}.json`
-  ];
-  
+(async (theme, configFile) => {
   try {
-    for (const configFile of configFiles) {
-      await backstop('test', { config: configFile });
-    }
+    await backstop('test', { config: configFile });
   } catch (error) {
     console.error('[CHI]: Error executing tests: ', error);
     process.exit(1);
   }
-})(process.argv[2]);
+})(process.argv[2], process.argv[3]);
