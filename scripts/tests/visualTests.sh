e6447befcabbdf9552467e5133cf469e5014e95f
Alejandro Sanchez <Alejandro.Pineda@lumen.com>
2026-01-22 14:07:10 +0100

    [DPEDE-6195] Test with old package-lock to run tests

--- scripts/tests/visualTests.sh
+++ scripts/tests/visualTests.sh
@@ -12,8 +12,10 @@ fi
 echo "[CHI]: Installing dependencies..."
 
 # backstop runs on node 20 not 22, npm ci will give conflicts with package-lock
+# TO BE REMOVED ONCE MIGRATION TO NODE22 IS COMPLETE
 sed -i.bak 's/"@centurylink\/chi-documentation":[[:space:]]*"[^"]*"/"@centurylink\/chi-documentation": "1.57.0"/' package.json && rm package.json.bak
-rm package-lock.json
+mv package-lock-tests.json package-lock.json
+
 npm i
 
 # npm ci
