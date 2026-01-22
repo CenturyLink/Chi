38b0a7840b750c9118bcc019fff5fd783630d6e7
Alejandro Sanchez <Alejandro.Pineda@lumen.com>
2026-01-22 12:11:01 +0100

    [DPEDE-6195] Use old package and package-lock to fix doc version to 1.57.0 for visual tests

--- scripts/tests/visualTests.sh
+++ scripts/tests/visualTests.sh
@@ -11,6 +11,11 @@ fi
 
 echo "[CHI]: Installing dependencies..."
 
+# backstop runs on node 20 not 22
+# Documentation needs node22, so it's fixed in 1.57.0 with node 20
+mv package.ci.tests.json package.json
+mv package-lock.ci.tests.json package-lock.json
+
 npm ci
 npx playwright install
 
