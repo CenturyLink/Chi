07a206179fa39a3b643a0105164044ac3f13d8a1
NachoCasaleLinde <casale.job.ig@gmail.com>
2026-07-01 11:18:14 +0200

    [DPEDE-7401] Prepare chi 7.15.0 release

--- scripts/build/css/vars/build.ts
+++ scripts/build/css/vars/build.ts
@@ -452,7 +452,7 @@ function generateTokenMetadata(
 
   return {
     theme,
-    version: '7.14.0',
+    version: '7.15.0',
     generatedAt: new Date().toISOString(),
     tokenCount: {
       foundation: generatedOutput.foundationCount,
